192b5fafda381e2ed3a99f2efe0680576e79d4c1
Benjamin Canac <canacb1@gmail.com>
2021-11-17 18:18:47 +0100

    chore(example): normal config

--- example/nuxt.config.ts
+++ example/nuxt.config.ts
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   buildModules: [
-    ['../src', { primary: 'blue', prefix: 'nu' }]
-  ]
+    '../src'
+  ],
+  ui: {
+    primary: 'blue',
+    prefix: 'u'
+  }
 })
