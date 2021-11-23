8242d72595447e262918cd3552575a52059199d8
Benjamin Canac <canacb1@gmail.com>
2021-11-23 17:51:58 +0100

    chore(build): include runtime dir

--- build.config.ts
+++ build.config.ts
@@ -2,6 +2,7 @@ export default {
   entries: [
     './src/index',
     { input: './src/components/', outDir: 'dist/components', ext: 'js' },
+    { input: './src/runtime/', outDir: 'dist/runtime', ext: 'js' },
     { input: './src/utils/', outDir: 'dist/utils', ext: 'js' }
   ],
   declaration: true,
