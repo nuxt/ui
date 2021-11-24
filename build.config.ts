b2690d9d3f4a4ba29f191d5a8c3208e7c243f995
Benjamin Canac <canacb1@gmail.com>
2021-11-24 16:13:41 +0100

    Revert "chore: revert to unbuild"
    This reverts commit 39aa61708a66beae84fbfbed1050a89853ad5b86.

--- build.config.ts
+++ build.config.ts
@@ -1,6 +1,9 @@
 export default {
   entries: [
-    './src/index'
+    './src/index',
+    { input: './src/components/', outDir: 'dist/components', ext: 'js' },
+    { input: './src/runtime/', outDir: 'dist/runtime', ext: 'js' },
+    { input: './src/utils/', outDir: 'dist/utils', ext: 'js' }
   ],
   declaration: true,
   externals: [
