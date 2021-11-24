39aa61708a66beae84fbfbed1050a89853ad5b86
Benjamin Canac <canacb1@gmail.com>
2021-11-24 16:11:33 +0100

    chore: revert to unbuild

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,6 @@
 export default {
   entries: [
-    './src/index',
-    { input: './src/components/', outDir: 'dist/components', ext: 'js' },
-    { input: './src/runtime/', outDir: 'dist/runtime', ext: 'js' },
-    { input: './src/utils/', outDir: 'dist/utils', ext: 'js' }
+    './src/index'
   ],
   declaration: true,
   externals: [
