b3e7bc53ad82f6bd273eea2936738bf16060dc7a
Benjamin Canac <canacb1@gmail.com>
2021-11-26 16:27:59 +0100

    chore(build): remove ext js

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,9 @@
 export default {
   entries: [
     './src/index',
-    { input: './src/components/', outDir: 'dist/components', ext: 'js' },
-    { input: './src/presets/', outDir: 'dist/presets', ext: 'js' },
-    { input: './src/utils/', outDir: 'dist/utils', ext: 'js' }
+    { input: './src/components/', outDir: 'dist/components' },
+    { input: './src/presets/', outDir: 'dist/presets' },
+    { input: './src/utils/', outDir: 'dist/utils' }
   ],
   declaration: true,
   externals: [
