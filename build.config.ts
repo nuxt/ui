3d2a5fe0bbc968a6bd7ee2eb3b203cbd95dbf8a3
Benjamin Canac <canacb1@gmail.com>
2021-12-10 10:21:06 +0100

    fix(build): add plugins dir

--- build.config.ts
+++ build.config.ts
@@ -3,6 +3,7 @@ export default {
     './src/index',
     { input: './src/components/', outDir: 'dist/components' },
     { input: './src/presets/', outDir: 'dist/presets' },
+    { input: './src/plugins/', outDir: 'dist/plugins' },
     { input: './src/utils/', outDir: 'dist/utils' },
     { input: './src/css/', outDir: 'dist/css' }
   ],
