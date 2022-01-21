36cb63b7e63ab1bd2be1c89e07818b1d3fe3fa42
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-01-21 12:11:30 +0100

    feat(Modal): increase z-index

--- src/runtime/components/overlays/Modal.vue
+++ src/runtime/components/overlays/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <TransitionRoot :show="isOpen" as="template">
     <Dialog @close="setIsOpen">
-      <div class="fixed z-10 inset-0 overflow-y-auto">
+      <div class="fixed z-20 inset-0 overflow-y-auto">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <TransitionChild
             as="template"
