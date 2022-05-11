1bc39571afc568f2681ebd9e9f58473e5e4cd016
Benjamin Canac <canacb1@gmail.com>
2022-05-11 18:33:47 +0200

    up

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -4,14 +4,17 @@ import module from '../src/module'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   extends: ['../node_modules/@docus/base'],
-  meta: {
-    title: '@nuxthq/ui',
-    meta: [
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
-    ],
-    link: [
-      { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' }
-    ]
+  app: {
+    head: {
+      title: '@nuxthq/ui',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
+        },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com' }
+      ]
+    }
   },
   modules: [
     module
