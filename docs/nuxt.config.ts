a260970d80cff1cac378601900de1d6df41ee7be
Benjamin Canac <canacb1@gmail.com>
2022-05-11 21:37:30 +0200

    up

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
       theme: {
         extend: {
           fontFamily: {
-            sans: ['"Inter var", sans-serif']
+            sans: ['Inter, sans-serif']
           }
         }
       }
