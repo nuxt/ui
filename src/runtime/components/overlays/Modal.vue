f98253c255bb9ee33c9a090fde08896766847443
Benjamin Canac <canacb1@gmail.com>
2022-06-30 13:52:57 +0200

    chore(Modal): increase `z-index`

--- src/runtime/components/overlays/Modal.vue
+++ src/runtime/components/overlays/Modal.vue
@@ -1,6 +1,6 @@
 <template>
   <TransitionRoot :appear="appear" :show="isOpen" as="template">
-    <Dialog class="relative z-20" @close="close">
+    <Dialog class="relative z-50" @close="close">
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <TransitionChild
           as="template"
