5722a3ae62706229179b75d9291babd1c2039244
Benjamin Canac <canacb1@gmail.com>
2022-07-23 23:39:22 +0200

    fix(CommandPalette): icon inactive opacity on dark mode

--- src/runtime/components/navigation/CommandPaletteGroup.vue
+++ src/runtime/components/navigation/CommandPaletteGroup.vue
@@ -15,7 +15,7 @@
       >
         <li :class="['flex justify-between select-none items-center rounded-md px-3 py-2 gap-3 relative', active && 'bg-gray-100 dark:bg-gray-800 u-text-gray-900', command.disabled ? 'cursor-not-allowed' : 'cursor-pointer']">
           <div class="flex items-center gap-3 min-w-0">
-            <Icon v-if="command.icon" :name="command.icon" :class="['h-5 w-5 flex-shrink-0 text-opacity-40', active && 'text-opacity-100', command.iconClass || 'text-gray-900 dark:text-gray-50']" aria-hidden="true" />
+            <Icon v-if="command.icon" :name="command.icon" :class="['h-5 w-5 flex-shrink-0', active ? 'text-opacity-100' : 'text-opacity-40', command.iconClass || 'text-gray-900 dark:text-gray-50']" aria-hidden="true" />
             <Avatar
               v-else-if="command.avatar"
               v-bind="{ size: 'xxs', ...command.avatar }"
