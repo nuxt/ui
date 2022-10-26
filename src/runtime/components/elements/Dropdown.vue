ee69146c0a4ffadda37a8d7c013e9e874a849589
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2022-10-26 11:29:28 +0200

    fix: remove `MenuItem` `as` prop

--- src/runtime/components/elements/Dropdown.vue
+++ src/runtime/components/elements/Dropdown.vue
@@ -10,7 +10,7 @@
       <transition appear v-bind="transitionClass">
         <MenuItems :class="baseClass" static>
           <div v-for="(subItems, index) of items" :key="index" :class="groupClass">
-            <MenuItem v-for="(item, subIndex) of subItems" :key="subIndex" v-slot="{ active, disabled }" :disabled="item.disabled" as="div">
+            <MenuItem v-for="(item, subIndex) of subItems" :key="subIndex" v-slot="{ active, disabled }" :disabled="item.disabled">
               <Component v-bind="item" :is="(item.to && NuxtLink) || (item.click && 'button') || 'div'" :class="resolveItemClass({ active, disabled })" @click="e => onItemClick(e, item)">
                 <slot :name="item.slot" :item="item">
                   <Icon v-if="item.icon" :name="item.icon" :class="[itemIconClass, item.iconClass]" />
@@ -151,8 +151,6 @@ function onItemClick (e, item: any) {
 
   if (item.click) {
     item.click(e)
-  } else if (item.to) {
-    menuApi.value.closeMenu && menuApi.value.closeMenu()
   }
 }
 
