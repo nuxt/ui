28586c35a558d9e925094f86e07acdb928d54ad7
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-11-02 16:04:50 +0100

    fix(types): add missing field in command palette type (#111)

--- src/runtime/types/command-palette.d.ts
+++ src/runtime/types/command-palette.d.ts
@@ -14,6 +14,7 @@ export interface Command {
   group?: string
   score?: number
   matches?: (FuseSortFunctionMatch | FuseSortFunctionMatchList)[]
+  [key: string]: any
 }
 
 export interface Group {
@@ -22,4 +23,5 @@ export interface Group {
   inactive?: string
   commands: Command[]
   options?: Partial<UseFuseOptions<Command>>
+  [key: string]: any
 }
