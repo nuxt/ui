d5d250b8cfa5671254ad29bc0d3b8b8c1432b9d8
Benjamin Canac <canacb1@gmail.com>
2023-05-09 14:27:20 +0200

    chore(Badge): default size to `sm` and add `lg`

--- src/runtime/app.config.ts
+++ src/runtime/app.config.ts
@@ -57,15 +57,16 @@ const badge = {
   rounded: 'rounded-md',
   font: 'font-medium',
   size: {
-    sm: 'text-xs px-1.5 py-0.5',
-    md: 'text-xs px-2 py-1',
-    lg: 'text-xs px-2.5 py-1.5'
+    xs: 'text-xs px-1.5 py-0.5',
+    sm: 'text-xs px-2 py-1',
+    md: 'text-sm px-2 py-1',
+    lg: 'text-sm px-2.5 py-1.5'
   },
   variant: {
     solid: 'bg-{color}-50 dark:bg-{color}-400 dark:bg-opacity-10 text-{color}-500 dark:text-{color}-400 ring-1 ring-inset ring-{color}-500 dark:ring-{color}-400 ring-opacity-10 dark:ring-opacity-20'
   },
   default: {
-    size: 'md',
+    size: 'sm',
     variant: 'solid',
     color: 'primary'
   }
