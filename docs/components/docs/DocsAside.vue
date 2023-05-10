f48ead6faf6fd14deeff84ca7b25d6bb7fae6f12
Benjamin Canac <canacb1@gmail.com>
2023-05-10 11:59:30 +0200

    fix(docs): sticky search button `z-index`

--- docs/components/docs/DocsAside.vue
+++ docs/components/docs/DocsAside.vue
@@ -3,7 +3,7 @@
     class="hidden pb-8 overflow-y-auto lg:block lg:self-start lg:top-16 lg:max-h-[calc(100vh-64px)] lg:sticky lg:pr-8 lg:pl-[2px]"
   >
     <div class="relative">
-      <div class="sticky top-0 pointer-events-none">
+      <div class="sticky top-0 pointer-events-none z-[1]">
         <div class="h-8 bg-white dark:bg-gray-900" />
         <div class="bg-white dark:bg-gray-900 relative pointer-events-auto">
           <UButton
