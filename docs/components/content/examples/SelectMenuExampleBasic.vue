24434dc5611faf80524a9f2c784fec24955aac0b
Benjamin Canac <canacb1@gmail.com>
2023-05-11 14:17:28 +0200

    docs: default value in select menu basic example

--- docs/components/content/examples/SelectMenuExampleBasic.vue
+++ docs/components/content/examples/SelectMenuExampleBasic.vue
@@ -1,7 +1,7 @@
 <script setup>
 const people = ['Wade Cooper', 'Arlene Mccoy', 'Devon Webb', 'Tom Cook', 'Tanya Fox', 'Hellen Schmidt', 'Caroline Schultz', 'Mason Heaney', 'Claudie Smitham', 'Emil Schaefer']
 
-const selected = ref()
+const selected = ref(people[0])
 </script>
 
 <template>
