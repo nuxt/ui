37f1a1b5adc39e3f09b861ed593159016113b13e
Benjamin Canac <canacb1@gmail.com>
2023-05-13 23:22:07 +0200

    docs: lots of improvements

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>> {
-  darkMode: 'class',
   content: [
     'docs/content/**/*.md'
   ],
