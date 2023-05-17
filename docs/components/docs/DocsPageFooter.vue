c834f401cd4ab34c08e0503f25066d49e556c91b
Benjamin Canac <canacb1@gmail.com>
2023-05-17 15:03:48 +0200

    docs: lint on page footer button

--- docs/components/docs/DocsPageFooter.vue
+++ docs/components/docs/DocsPageFooter.vue
@@ -1,6 +1,13 @@
 <template>
   <footer class="flex items-center justify-between gap-1.5">
-    <UButton :to="`https://github.com/nuxtlabs/ui/edit/dev/docs/content/${page._file}`" label="Edit on GitHub" color="gray" variant="link" :padded="false" trailing-icon="i-heroicons-arrow-top-right-on-square-20-solid" />
+    <UButton
+      :to="`https://github.com/nuxtlabs/ui/edit/dev/docs/content/${page._file}`"
+      label="Edit on GitHub"
+      color="gray"
+      variant="link"
+      :padded="false"
+      trailing-icon="i-heroicons-arrow-top-right-on-square-20-solid"
+    />
 
     <div class="flex items-baseline gap-1.5 text-sm text-center text-gray-500 dark:text-gray-400">
       Made by
