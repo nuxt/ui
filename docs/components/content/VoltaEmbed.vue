1bfcf31964f7a13ae0058f154542aa97f54eb57e
Benjamin Canac <canacb1@gmail.com>
2023-05-17 12:16:52 +0200

    docs: add roadmap page with volta board

---
+++ docs/components/content/VoltaEmbed.vue
@@ -0,0 +1,14 @@
+<template>
+  <iframe :src="src" class="w-full min-h-[calc(100vh/1.5)] border border-gray-200 dark:border-gray-800 rounded-md" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  token: {
+    type: String,
+    required: true
+  }
+})
+
+const src = computed(() => `https://volta.net/embed/${props.token}`)
+</script>
