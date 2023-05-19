dfab900562c8d37ece3f0817ecb718e3a4183daa
Benjamin Canac <canacb1@gmail.com>
2023-05-19 14:51:31 +0200

    docs: add badge in VerticalNavigation example

--- docs/components/content/examples/VerticalNavigationExample.vue
+++ docs/components/content/examples/VerticalNavigationExample.vue
@@ -3,7 +3,8 @@ const links = [{
   label: 'Profile',
   avatar: {
     src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-  }
+  },
+  badge: 100
 }, {
   label: 'Installation',
   icon: 'i-heroicons-home',
