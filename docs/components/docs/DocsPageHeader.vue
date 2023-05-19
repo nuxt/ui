e358183165e6633345bc8ba0142653b254a11590
Benjamin Canac <canacb1@gmail.com>
2023-05-19 13:01:34 +0200

    docs: getting started title on index

--- docs/components/docs/DocsPageHeader.vue
+++ docs/components/docs/DocsPageHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header v-if="page" class="relative border-b border-gray-200 dark:border-gray-800 pb-8 mb-12">
     <p class="mb-4 text-sm leading-6 font-semibold text-primary-500 dark:text-primary-400 capitalize">
-      {{ useLowerCase(page._dir) }}
+      {{ page._dir?.title ? page._dir.title : useLowerCase(page._dir) }}
     </p>
     <div class="flex flex-col lg:flex-row lg:items-center lg:justify-between">
       <h1 class="text-3xl sm:text-4xl font-extrabold text-gray-900 tracking-tight dark:text-white">
