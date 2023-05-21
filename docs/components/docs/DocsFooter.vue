4add124bc1defdd44defe68058b7903feb42da5b
Harry Yep <github@hode.co.uk>
2023-05-21 16:46:37 +0100

    fix: LogoLabs not shown

--- docs/components/docs/DocsFooter.vue
+++ docs/components/docs/DocsFooter.vue
@@ -3,7 +3,7 @@
     <div class="flex items-baseline gap-1.5 text-sm text-center text-gray-500 dark:text-gray-400">
       Made by
       <NuxtLink to="https://nuxtlabs.com" aria-label="NuxtLabs">
-        <LogoLabs class="text-white w-14 h-auto" />
+        <LogoLabs class="text-primary-500 w-14 h-auto dark:text-primary-400" />
       </NuxtLink>
     </div>
   </footer>
