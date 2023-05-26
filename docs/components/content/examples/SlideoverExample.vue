2d6ce654f4161242256da88c7c2548ffb8222c23
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2023-05-27 01:50:34 +0530

    docs: add close button to Slideover example (#211)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

--- docs/components/content/examples/SlideoverExample.vue
+++ docs/components/content/examples/SlideoverExample.vue
@@ -7,8 +7,23 @@ const isOpen = ref(false)
     <UButton label="Open" @click="isOpen = true" />
 
     <USlideover v-model="isOpen">
-      <div class="p-4 h-full">
-        <Placeholder class="w-full h-full" />
+      <div class="p-4 sm:p-6 flex flex-col flex-1 gap-4 sm:gap-6">
+        <div class="flex items-center justify-between">
+          <h2 class="font-semibold text-gray-900 dark:text-white">
+            Title
+          </h2>
+
+          <UButton
+            icon="i-heroicons-x-mark-20-solid"
+            color="gray"
+            variant="link"
+            size="md"
+            :padded="false"
+            @click="isOpen = false"
+          />
+        </div>
+
+        <Placeholder class="flex-1 w-full" />
       </div>
     </USlideover>
   </div>
