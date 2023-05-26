5a3df51e00ae013bb1870bb1d76709ed1098ae81
Benjamin Canac <canacb1@gmail.com>
2023-05-26 22:20:03 +0200

    up

--- docs/components/content/examples/SlideoverExample.vue
+++ docs/components/content/examples/SlideoverExample.vue
@@ -7,9 +7,23 @@ const isOpen = ref(false)
     <UButton label="Open" @click="isOpen = true" />
 
     <USlideover v-model="isOpen">
-      <div class="p-4 h-full relative">
-        <UButton class="absolute top-8 right-8 z-50" leading-icon="i-heroicons-x-mark-solid" @click="isOpen = false" />
-        <Placeholder class="w-full h-full" />
+      <div class="p-4 sm:p-6 flex flex-col flex-1 gap-4 sm:gap-6">
+        <div class="flex items-center justify-between">
+          <h2 class="font-semibold text-gray-900 dark:text-white">
+            Title
+          </h2>
+
+          <UButton
+            icon="i-heroicons-x-mark-20-solid"
+            color="gray"
+            variant="link"
+            size="md"
+            :padded="false"
+            @click="isOpen = false"
+          />
+        </div>
+
+        <Placeholder class="flex-1 w-full" />
       </div>
     </USlideover>
   </div>
