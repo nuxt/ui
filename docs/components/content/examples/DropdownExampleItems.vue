c75455e7f7f98651f67bc142702e37eb347aa1ba
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2023-05-30 18:53:48 +0200

    docs(dropdown): improve props documentation

---
+++ docs/components/content/examples/DropdownExampleItems.vue
@@ -0,0 +1,19 @@
+<script setup>
+const items = [
+  [{
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+    }
+  }], [{
+    label: 'Logout',
+    icon: 'i-heroicons-arrow-left-on-rectangle-20-solid'
+  }]
+]
+</script>
+
+<template>
+  <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+    <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+  </UDropdown>
+</template>
