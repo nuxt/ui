1c851b4d405af068393e58afaa3becb44b5ae178
Haytham Salama <haythamasalama@gmail.com>
2023-06-07 02:51:04 +0300

    feat(docs): add docs for table loading state

---
+++ docs/components/content/examples/TableExampleLoadingSlot.vue
@@ -0,0 +1,31 @@
+<script setup>
+const columns = [{
+  key: 'name',
+  label: 'Name'
+}, {
+  key: 'title',
+  label: 'Title'
+}, {
+  key: 'email',
+  label: 'Email'
+}, {
+  key: 'role',
+  label: 'Role'
+}, {
+  key: 'actions'
+}]
+
+const people = []
+
+const isLoading = ref(true);
+</script>
+
+<template>
+  <UTable :rows="people" :columns="columns" :is-loading="isLoading">
+    <template #loading-state>
+      <div class="flex items-center justify-center">
+        <span class="py-10">Loading...</span>
+      </div>
+    </template>
+  </UTable>
+</template>
