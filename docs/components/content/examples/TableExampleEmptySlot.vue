1184cb12dd9ad381a1e3f68b06268dcb7443e0c3
Haytham Salama <haythamasalama@gmail.com>
2023-06-07 03:12:32 +0300

    feat(docs): add docs for empty slot

---
+++ docs/components/content/examples/TableExampleEmptySlot.vue
@@ -0,0 +1,29 @@
+<script setup>
+const columns = [{
+  key: 'name',
+  label: 'Name'
+}, {
+  key: 'title',
+  label: 'Title'
+}, {
+  key: 'email',
+  label: 'Email'
+}, {
+  key: 'role',
+  label: 'Role'
+}, {
+  key: 'actions'
+}]
+
+const people = []
+</script>
+
+<template>
+  <UTable :rows="people" :columns="columns">
+    <template #empty-state>
+      <div class="flex items-center justify-center">
+          <span class="py-10">No Data.</span>
+      </div>
+    </template>
+  </UTable>
+</template>
