912b817e0b531785be9b6bf323e6b45468083cae
Haytham Salama <haythamasalama@gmail.com>
2023-06-09 13:48:34 +0300

    refactor(docs): update examples and content

---
+++ docs/components/content/examples/PaginationExampleSlots.vue
@@ -0,0 +1,33 @@
+<script setup>
+const page = ref(1);
+const items = ref(Array(55));
+</script>
+
+<template>
+  <UPagination
+    v-model="page"
+    :page-count="5"
+    class="flex justify-center items-center shadow-none"
+    :total="items.length"
+  >
+    <template #prev="{ onClick }">
+      <UTooltip text="previous" @click="onClick">
+        <UButton
+          icon="i-heroicons-arrow-left-20-solid"
+          color="gray"
+          class="mr-2 rounded-none"
+        />
+      </UTooltip>
+    </template>
+
+    <template #next="{ onClick }">
+      <UTooltip text="next" @click="onClick">
+        <UButton
+          icon="i-heroicons-arrow-right-20-solid"
+          color="gray"
+          class="ml-2 rounded-none"
+        />
+      </UTooltip>
+    </template>
+  </UPagination>
+</template>
