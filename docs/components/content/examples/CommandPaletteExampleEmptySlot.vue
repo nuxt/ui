ce852e2d15755901b2e9ba4b22d5a2ebea2f53a7
Benjamin Canac <canacb1@gmail.com>
2023-06-12 14:48:54 +0200

    up

--- docs/components/content/examples/CommandPaletteExampleEmptySlot.vue
+++ docs/components/content/examples/CommandPaletteExampleEmptySlot.vue
@@ -1,13 +1,9 @@
 <template>
-  <UCommandPalette :groups="[]">
+  <UCommandPalette>
     <template #empty-state>
-      <div class="flex flex-col items-center justify-center gap-3">
-        <UIcon name="i-heroicons-magnifying-glass-20-solid" class="w-6 h-6 text-gray-400" />
-        <span class="text-sm text-center text-gray-900 dark:text-white">We couldn't find any items!</span>
-        <div>
-          <UButton label="Add item" />
-          <UButton label="Clear Search" class="ml-2" color="primary" variant="outline"/>
-        </div>
+      <div class="flex flex-col items-center justify-center py-6 gap-3">
+        <span class="italic text-sm">Nothing here!</span>
+        <UButton label="Add item" />
       </div>
     </template>
   </UCommandPalette>
