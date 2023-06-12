bc81d45b2b2cd3aa44c438c3642e2775d5bf22d7
Benjamin Canac <canacb1@gmail.com>
2023-06-12 14:34:12 +0200

    docs: improve forms usage with examples

---
+++ docs/components/content/examples/RadioExample.vue
@@ -0,0 +1,23 @@
+<script setup>
+const methods = [{
+  name: 'email',
+  value: 'email',
+  label: 'Email'
+}, {
+  name: 'sms',
+  value: 'sms',
+  label: 'Phone (SMS)'
+}, {
+  name: 'push',
+  value: 'push',
+  label: 'Push notification'
+}]
+
+const selected = ref('sms')
+</script>
+
+<template>
+  <div class="space-y-1">
+    <URadio v-for="method of methods" :key="method.name" v-model="selected" v-bind="method" />
+  </div>
+</template>
