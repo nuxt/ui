7b17cc6744a1301621857541228ebcb0cfec645d
Tom Smith <tom.smith2711@gmail.com>
2023-06-15 23:14:29 +0100

    remove style tag

--- src/runtime/components/forms/Range.vue
+++ src/runtime/components/forms/Range.vue
@@ -24,99 +24,97 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from "vue";
-import type { PropType } from "vue";
-import { defu } from "defu";
-import { classNames } from "../../utils";
-import { useAppConfig } from "#imports";
+import { computed, defineComponent } from 'vue'
+import type { PropType } from 'vue'
+import { defu } from 'defu'
+import { classNames } from '../../utils'
+import { useAppConfig } from '#imports'
 // TODO: Remove
 // @ts-expect-error
-import appConfig from "#build/app.config";
+import appConfig from '#build/app.config'
 
 export default defineComponent({
   props: {
     modelValue: {
       type: Number,
       required: true,
-      default: 50,
+      default: 50
     },
     name: {
       type: String,
-      default: null,
+      default: null
     },
     disabled: {
       type: Boolean,
-      default: false,
+      default: false
     },
     autofocus: {
       type: Boolean,
-      default: false,
+      default: false
     },
     min: {
       type: Number,
-      default: 0,
+      default: 0
     },
     max: {
       type: Number,
-      default: 100,
+      default: 100
     },
     step: {
       type: Number,
-      default: 1,
+      default: 1
     },
     size: {
       type: String,
       default: () => appConfig.ui.range.default.size,
       validator(value: string) {
-        return Object.keys(appConfig.ui.range.size).includes(value);
-      },
+        return Object.keys(appConfig.ui.range.size).includes(value)
+      }
     },
     color: {
       type: String,
       default: () => appConfig.ui.range.default.color,
       validator(value: string) {
-        return appConfig.ui.colors.includes(value);
-      },
+        return appConfig.ui.colors.includes(value)
+      }
     },
     ui: {
       type: Object as PropType<Partial<typeof appConfig.ui.range>>,
-      default: () => appConfig.ui.range,
-    },
+      default: () => appConfig.ui.range
+    }
   },
-  emits: ["update:modelValue", "focus", "blur"],
+  emits: ['update:modelValue', 'focus', 'blur'],
   setup(props, { emit, slots }) {
     // TODO: Remove
-    const appConfig = useAppConfig();
+    const appConfig = useAppConfig()
 
     const ui = computed<Partial<typeof appConfig.ui.range>>(() =>
       defu({}, props.ui, appConfig.ui.range)
-    );
+    )
 
     const onInput = (event: InputEvent) => {
-      emit("update:modelValue", (event.target as any).value);
-    };
+      emit('update:modelValue', (event.target as any).value)
+    }
 
     const inputClass = computed(() => {
       return classNames(
         ui.value.base,
-        ui.value.thumb.base.replaceAll("{color}", props.color),
+        ui.value.thumb.base.replaceAll('{color}', props.color),
         ui.value.thumb.size[props.size],
         ui.value.size[props.size]
-      );
-    });
+      )
+    })
 
     const wrapperClass = computed(() => {
       return classNames(
-        ui.value.wrapper.replaceAll("{color}", props.color),
+        ui.value.wrapper.replaceAll('{color}', props.color),
         ui.value.size[props.size],
-        props.disabled ? ui.value.disabled : ""
-      );
-    });
+        props.disabled ? ui.value.disabled : ''
+      )
+    })
 
     // eslint-disable-next-line vue/no-dupe-keys
-    return { ui, onInput, inputClass, wrapperClass };
-  },
-});
+    return { ui, onInput, inputClass, wrapperClass }
+  }
+})
 </script>
-
-<style scoped></style>
