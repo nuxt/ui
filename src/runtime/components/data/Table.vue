1ff11ac1a3eff537a4ee854a049668f312f1d415
Benjamin Canac <canacb1@gmail.com>
2023-06-27 15:32:19 +0200

    feat(Table): reset sort on third click
    Resolves #300

--- src/runtime/components/data/Table.vue
+++ src/runtime/components/data/Table.vue
@@ -188,7 +188,13 @@ export default defineComponent({
 
     function onSort (column) {
       if (sort.value.column === column.key) {
-        sort.value.direction = sort.value.direction === 'asc' ? 'desc' : 'asc'
+        const direction = !column.direction || column.direction === 'asc' ? 'desc' : 'asc'
+
+        if (sort.value.direction === direction) {
+          sort.value = defu({}, props.sort, { column: null, direction: 'asc' })
+        } else {
+          sort.value.direction = sort.value.direction === 'asc' ? 'desc' : 'asc'
+        }
       } else {
         sort.value = { column: column.key, direction: column.direction || 'asc' }
       }
