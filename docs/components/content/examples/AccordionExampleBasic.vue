47533ca03dcbbfe4083b3d1e301ff43928f0c185
Benjamin Canac <canacb1@gmail.com>
2023-06-27 12:45:01 +0200

    up

--- docs/components/content/examples/AccordionExampleBasic.vue
+++ docs/components/content/examples/AccordionExampleBasic.vue
@@ -7,6 +7,7 @@ const items = [{
 }, {
   label: 'Installation',
   icon: 'i-heroicons-arrow-down-tray',
+  disabled: true,
   content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.'
 }, {
   label: 'Theming',
