05b8a22eec074710a1030ac73b7fc9d9ca19ab3b
Benjamin Canac <canacb1@gmail.com>
2023-07-12 18:44:09 +0200

    chore(Dropdown): add `Item` type

---
+++ src/runtime/types/dropdown.d.ts
@@ -0,0 +1,15 @@
+import type { RouteLocationRaw } from 'vue-router'
+import type { Avatar } from './avatar'
+
+export interface Item {
+  to?: string | RouteLocationRaw
+  exact?: boolean
+  label: string
+  slot?: string
+  icon?: string
+  iconClass?: string
+  avatar?: Partial<Avatar>
+  shortcuts?: string[]
+  disabled?: boolean
+  click?: Function
+}
