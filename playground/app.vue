403899f11a5c3a23c456c21396c4caac6a60f20a
Benjamin Canac <canacb1@gmail.com>
2023-07-19 13:21:39 +0200

    chore: add playground

---
+++ playground/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <UContainer class="min-h-screen flex items-center">
+    <UCard class="flex-1 text-center">
+      Welcome to the playground!
+    </UCard>
+  </UContainer>
+</template>
