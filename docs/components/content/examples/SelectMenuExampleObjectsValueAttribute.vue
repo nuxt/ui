07bb75318b2aae6af787cea3feddc7e29e90da30
Marc-Olivier Castagnetti <marcolivier.castagnetti@gmail.com>
2023-07-19 15:39:02 +0200

    Add an example in the docs for valueAttribute usage

---
+++ docs/components/content/examples/SelectMenuExampleObjectsValueAttribute.vue
@@ -0,0 +1,31 @@
+<script setup>
+const people = [{
+  id: 1,
+  name: 'Wade Cooper'
+},
+{
+  id: 2,
+  name: 'Arlene Mccoy'
+},
+{
+  id: 3,
+  name: 'Devon Webb'
+},
+{
+  id: 4,
+  name: 'Tom Cook'
+}]
+
+const selected = ref([people[0].id])
+</script>
+
+<template>
+  <USelectMenu
+    v-model="selected"
+    :options="people"
+    placeholder="Select people"
+    value-attribute="id"
+    option-attribute="name"
+    multiple
+  />
+</template>
