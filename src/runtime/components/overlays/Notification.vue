df455db3caeb689ac1f24f224606183d4f5135ea
Benjamin Canac <canacb1@gmail.com>
2023-07-20 11:58:43 +0200

    feat(Notification): support html with `title` and `description` slots (#431)

--- src/runtime/components/overlays/Notification.vue
+++ src/runtime/components/overlays/Notification.vue
@@ -9,10 +9,14 @@
 
             <div class="w-0 flex-1">
               <p :class="ui.title">
-                {{ title }}
+                <slot name="title" :title="title">
+                  {{ title }}
+                </slot>
               </p>
               <p v-if="description" :class="ui.description">
-                {{ description }}
+                <slot name="description" :description="description">
+                  {{ description }}
+                </slot>
               </p>
 
               <div v-if="description && actions.length" class="mt-3 flex items-center gap-2">
