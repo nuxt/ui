08349b5a5d96d5e509d8b0e4c15e2810ac20bc93
Benjamin Canac <canacb1@gmail.com>
2023-07-26 18:19:34 +0200

    feat(Alert): new component

---
+++ docs/components/content/examples/AlertExampleHtml.vue
@@ -0,0 +1,11 @@
+<template>
+  <UAlert title="Heads <i>up</i>!" icon="i-heroicons-command-line">
+    <template #title="{ title }">
+      <span v-html="title" />
+    </template>
+
+    <template #description>
+      You can add <b>components</b> to your app using the <u>cli</u>.
+    </template>
+  </UAlert>
+</template>
