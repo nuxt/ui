788236ea699ec822ad4d949d1778c16fc1d9f76c
Romain Hamel <rom.hml@gmail.com>
2023-07-26 20:28:57 +0200

    feat(Form): add support for Joi

---
+++ docs/components/content/examples/FormExampleJoi.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Joi from 'joi'
+import { Form } from '#ui'
+
+const schema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string()
+    .min(8)
+    .required()
+})
+
+const state = ref({
+  email: undefined,
+  password: undefined
+})
+
+const form = ref()
+
+async function submit () {
+  await form.value!.validate()
+  // Do something with state.value
+}
+</script>
+
+<template>
+  <UForm
+    ref="form"
+    :schema="schema"
+    :state="state"
+    class="space-y-2"
+    @submit.prevent="submit"
+  >
+    <UFormGroup label="Email" path="email">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UFormGroup label="Password" path="password">
+      <UInput v-model="state.password" type="password" />
+    </UFormGroup>
+
+    <UButton block type="submit">
+      Submit
+    </UButton>
+  </UForm>
+</template>
--- src/runtime/components/forms/Form.ts
+++ src/runtime/components/forms/Form.ts
@@ -2,24 +2,100 @@ import { provide, ref, PropType, h } from 'vue'
 import { FormError, FormEvent } from '../../types'
 import { useEventBus } from '@vueuse/core'
 
+import type { ValidationError as JoiError, Schema as JoiSchema } from 'joi'
 import type { ZodSchema, ZodError } from 'zod'
-import type { ObjectSchema, ValidationError } from 'yup'
+import type {
+  ObjectSchema as YupObjectSchema,
+  ValidationError as YupError
+} from 'yup'
 
-function isYupSchema (schema: any): schema is ObjectSchema<any> {
-  return (
-    schema.validate !== undefined
-  )
+export default defineComponent({
+  props: {
+    schema: {
+      type: Object as
+        | PropType<ZodSchema>
+        | PropType<YupObjectSchema<any>>
+        | PropType<JoiSchema>,
+      default: undefined
+    },
+
+    state: {
+      type: Object,
+      required: true
+    },
+
+    validate: {
+      type: Function as PropType<(state: any) => Promise<FormError[]>>,
+      default: () => []
+    }
+  },
+
+  setup (props, { slots, expose }) {
+    const seed = Math.random().toString(36).substring(7)
+    const bus = useEventBus<FormEvent>(`form-${seed}`)
+
+    bus.on(async (event) => {
+      if (event.type === 'blur') {
+        const otherErrors = errors.value.filter(
+          (error) => error.path !== event.path
+        )
+        const pathErrors = (await getErrors()).filter(
+          (error) => error.path === event.path
+        )
+        errors.value = otherErrors.concat(pathErrors)
+      }
+    })
+
+    const errors = ref<FormError[]>([])
+    provide('form-errors', errors)
+    provide('form-events', bus)
+
+    async function getErrors (): Promise<FormError[]> {
+      let errs = await props.validate(props.state)
+
+      if (props.schema) {
+        if (isZodSchema(props.schema)) {
+          errs = errs.concat(await getZodErrors(props.state, props.schema))
+        } else if (isYupSchema(props.schema)) {
+          errs = errs.concat(await getYupErrors(props.state, props.schema))
+        } else if (isJoiSchema(props.schema)) {
+          errs = errs.concat(await getJoiErrors(props.state, props.schema))
+        } else {
+          throw new Error('Form validation failed: Unsupported form schema')
+        }
+      }
+
+      return errs
+    }
+
+    async function validate () {
+      errors.value = await getErrors()
+      if (errors.value.length > 0) {
+        throw new Error(
+          `Form validation failed: ${JSON.stringify(errors.value, null, 2)}`
+        )
+      }
+    }
+
+    expose({
+      validate
+    })
+
+    return () => h('form', slots.default?.())
+  }
+})
+
+function isYupSchema (schema: any): schema is YupObjectSchema<any> {
+  return schema.validate && schema.__isYupSchema__
 }
 
-function isYupError (error: any): error is ValidationError {
-  return (
-    error.inner !== undefined
-  )
+function isYupError (error: any): error is YupError {
+  return error.inner !== undefined
 }
 
 async function getYupErrors (
   state: any,
-  schema: ObjectSchema<any>
+  schema: YupObjectSchema<any>
 ): Promise<FormError[]> {
   try {
     await schema.validate(state, { abortEarly: false })
@@ -63,73 +139,29 @@ async function getZodErrors (
   }
 }
 
-export default defineComponent({
-  props: {
-    schema: {
-      type: Object as PropType<ZodSchema> | PropType<ObjectSchema<any>>,
-      default: undefined
-    },
+function isJoiSchema (schema: any): schema is JoiSchema {
+  return schema.validateAsync !== undefined && schema.id !== undefined
+}
 
-    state: {
-      type: Object,
-      required: true
-    },
+function isJoiError (error: any): error is JoiError {
+  return error.isJoi === true
+}
 
-    validate: {
-      type: Function as PropType<(state: any) => Promise<FormError[]>>,
-      default: () => []
+async function getJoiErrors (
+  state: any,
+  schema: JoiSchema
+): Promise<FormError[]> {
+  try {
+    await schema.validateAsync(state, { abortEarly: false })
+    return []
+  } catch (error) {
+    if (isJoiError(error)) {
+      return error.details.map((detail) => ({
+        path: detail.path.join('.'),
+        message: detail.message
+      }))
+    } else {
+      throw error
     }
-  },
-
-  setup (props, { slots, expose }) {
-    const seed = Math.random().toString(36).substring(7)
-    const bus = useEventBus<FormEvent>(`form-${seed}`)
-
-    bus.on(async (event) => {
-      if (event.type === 'blur') {
-        const otherErrors = errors.value.filter(
-          (error) => error.path !== event.path
-        )
-        const pathErrors = (await getErrors()).filter(
-          (error) => error.path === event.path
-        )
-        errors.value = otherErrors.concat(pathErrors)
-      }
-    })
-
-    const errors = ref<FormError[]>([])
-    provide('form-errors', errors)
-    provide('form-events', bus)
-
-    async function getErrors (): Promise<FormError[]> {
-      let errs = await props.validate(props.state)
-
-      if (props.schema) {
-        if (isZodSchema(props.schema)) {
-          errs = errs.concat(await getZodErrors(props.state, props.schema))
-        } else if (isYupSchema(props.schema)) {
-          errs = errs.concat(await getYupErrors(props.state, props.schema))
-        } else {
-          throw new Error('Form validation failed: Unsupported form schema')
-        }
-      }
-
-      return errs
-    }
-
-    async function validate () {
-      errors.value = await getErrors()
-      if (errors.value.length > 0) {
-        throw new Error(
-          `Form validation failed: ${JSON.stringify(errors.value, null, 2)}`
-        )
-      }
-    }
-
-    expose({
-      validate
-    })
-
-    return () => h('form', slots.default?.())
   }
-})
+}
