2cd620899f3e997357f6274cc7a0bfc79a8277b6
Benjamin Canac <canacb1@gmail.com>
2023-07-27 18:28:25 +0200

    fix(module): safelist all colors for `toast.add`
    Resolves #375, resolves #440

--- src/module.ts
+++ src/module.ts
@@ -123,7 +123,7 @@ export default defineNuxtModule<ModuleOptions>({
       }
 
       tailwindConfig.safelist = tailwindConfig.safelist || []
-      tailwindConfig.safelist.push(...generateSafelist(options.safelistColors))
+      tailwindConfig.safelist.push(...generateSafelist(options.safelistColors, colors))
 
       tailwindConfig.plugins = tailwindConfig.plugins || []
       tailwindConfig.plugins.push(iconsPlugin({ collections: getIconCollections(options.icons as any[]) }))
