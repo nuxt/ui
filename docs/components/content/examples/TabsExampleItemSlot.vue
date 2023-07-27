923813a7d6cce74867fa6247346e424555f7d278
Benjamin Canac <canacb1@gmail.com>
2023-07-27 16:22:49 +0200

    feat(Tabs): new component (#450)

---
+++ docs/components/content/examples/TabsExampleItemSlot.vue
@@ -0,0 +1,58 @@
+<script setup>
+const items = [{
+  key: 'account',
+  label: 'Account',
+  description: 'Make changes to your account here. Click save when you\'re done.'
+}, {
+  key: 'password',
+  label: 'Password',
+  description: 'Change your password here. After saving, you\'ll be logged out.'
+}]
+
+const accountForm = reactive({ name: 'Benjamin', username: 'benjamincanac'  })
+const passwordForm = reactive({ currentPassword: '', newPassword: '' })
+
+function onSubmit (form) {
+  console.log('Submitted form:', form)
+}
+</script>
+
+<template>
+  <UTabs :items="items" class="w-full">
+    <template #item="{ item }">
+      <UCard @submit.prevent="() => onSubmit(item.key === 'account' ? accountForm : passwordForm)">
+        <template #header>
+          <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+            {{ item.label }}
+          </h3>
+          <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">
+            {{ item.description }}
+          </p>
+        </template>
+
+        <div v-if="item.key === 'account'" class="space-y-3">
+          <UFormGroup label="Name" name="name">
+            <UInput v-model="accountForm.name" />
+          </UFormGroup>
+          <UFormGroup label="Username" name="username">
+            <UInput v-model="accountForm.username" />
+          </UFormGroup>
+        </div>
+        <div v-else-if="item.key === 'password'" class="space-y-3">
+          <UFormGroup label="Current Password" name="current" required>
+            <UInput v-model="passwordForm.currentPassword" type="password" required />
+          </UFormGroup>
+          <UFormGroup label="New Password" name="new" required>
+            <UInput v-model="passwordForm.newPassword" type="password" required />
+          </UFormGroup>
+        </div>
+
+        <template #footer>
+          <UButton type="submit" color="black">
+            Save {{ item.key === 'account' ? 'account' : 'password' }}
+          </UButton>
+        </template>
+      </UCard>
+    </template>
+  </UTabs>
+</template>
