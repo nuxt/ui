aa97de7390323a641fbb5e1c1a7b4c5da1db66a0
Benjamin Canac <canacb1@gmail.com>
2023-07-27 16:50:04 +0200

    feat(Alert): add `subtle` variant

--- src/runtime/app.config.ts
+++ src/runtime/app.config.ts
@@ -313,7 +313,8 @@ const alert = {
   variant: {
     solid: 'bg-{color}-500 dark:bg-{color}-400 text-white dark:text-gray-900',
     outline: 'text-{color}-500 dark:text-{color}-400 ring-1 ring-inset ring-{color}-500 dark:ring-{color}-400',
-    soft: 'bg-{color}-50 dark:bg-{color}-400 dark:bg-opacity-10 text-{color}-500 dark:text-{color}-400'
+    soft: 'bg-{color}-50 dark:bg-{color}-400 dark:bg-opacity-10 text-{color}-500 dark:text-{color}-400',
+    subtle: 'bg-{color}-50 dark:bg-{color}-400 dark:bg-opacity-10 text-{color}-500 dark:text-{color}-400 ring-1 ring-inset ring-{color}-500 dark:ring-{color}-400 ring-opacity-25 dark:ring-opacity-25'
   },
   default: {
     color: 'white',
