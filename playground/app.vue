3d7f6820a0e14a523b5bb32de71b9b880ead85e5
Benjamin Canac <canacb1@gmail.com>
2023-07-28 23:03:34 +0200

    playground: button group with dropdown

--- playground/app.vue
+++ playground/app.vue
@@ -5,15 +5,47 @@
         Welcome to the playground!
       </template>
 
-      <p class="text-gray-500 dark:text-gray-400">
-        Try your components here!
-      </p>
+      <UButtonGroup size="sm">
+        <UButton label="Action" color="white" />
+        <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+          <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
+        </UDropdown>
+      </UButtonGroup>
     </UCard>
   </UContainer>
 </template>
 
 <script setup>
-
+const items = [
+  [{
+    label: 'Profile',
+    avatar: {
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
+    }
+  }], [{
+    label: 'Edit',
+    icon: 'i-heroicons-pencil-square-20-solid',
+    shortcuts: ['E'],
+    click: () => {
+      console.log('Edit')
+    }
+  }, {
+    label: 'Duplicate',
+    icon: 'i-heroicons-document-duplicate-20-solid',
+    shortcuts: ['D'],
+    disabled: true
+  }], [{
+    label: 'Archive',
+    icon: 'i-heroicons-archive-box-20-solid'
+  }, {
+    label: 'Move',
+    icon: 'i-heroicons-arrow-right-circle-20-solid'
+  }], [{
+    label: 'Delete',
+    icon: 'i-heroicons-trash-20-solid',
+    shortcuts: ['⌘', 'D']
+  }]
+]
 </script>
 
 <style>
