dce1ac199b457c124b82ad12811f446f8f70db6b
Haytham Salama <haythamasalama@gmail.com>
2023-07-30 01:42:28 +0300

    style: foramt code

--- docs/components/content/examples/SelectMenuExampleObjects.vue
+++ docs/components/content/examples/SelectMenuExampleObjects.vue
@@ -1,30 +1,27 @@
 <script setup>
 const people = [{
-                  id: 'benjamincanac',
-                  label: 'benjamincanac',
-                  href: 'https://github.com/benjamincanac',
-                  target: '_blank',
-                  avatar: { src: 'https://avatars.githubusercontent.com/u/739984?v=4' }
-                },
-                {
-                  id: 'Atinux',
-                  label: 'Atinux',
-                  href: 'https://github.com/Atinux',
-                  target: '_blank',
-                  avatar: { src: 'https://avatars.githubusercontent.com/u/904724?v=4' }
-                },
-                {
-                  id: 'smarroufin',
-                  label: 'smarroufin',
-                  href: 'https://github.com/smarroufin',
-                  target: '_blank',
-                  avatar: { src: 'https://avatars.githubusercontent.com/u/7547335?v=4' }
-                },
-                {
-                  id: 'nobody',
-                  label: 'Nobody',
-                  icon: 'i-heroicons-user-circle'
-                }]
+  id: 'benjamincanac',
+  label: 'benjamincanac',
+  href: 'https://github.com/benjamincanac',
+  target: '_blank',
+  avatar: { src: 'https://avatars.githubusercontent.com/u/739984?v=4' }
+}, {
+  id: 'Atinux',
+  label: 'Atinux',
+  href: 'https://github.com/Atinux',
+  target: '_blank',
+  avatar: { src: 'https://avatars.githubusercontent.com/u/904724?v=4' }
+}, {
+  id: 'smarroufin',
+  label: 'smarroufin',
+  href: 'https://github.com/smarroufin',
+  target: '_blank',
+  avatar: { src: 'https://avatars.githubusercontent.com/u/7547335?v=4' }
+}, {
+  id: 'nobody',
+  label: 'Nobody',
+  icon: 'i-heroicons-user-circle'
+}]
 
 const selected = ref(people[0])
 </script>
