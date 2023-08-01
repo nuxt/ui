871d3b3a8553dedc4b65ad8598d72a9b7416cf99
Benjamin Canac <canacb1@gmail.com>
2023-08-01 14:52:34 +0200

    docs(tailwind.config): override `green` with nuxt one

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -6,6 +6,20 @@ export default <Partial<Config>>{
     extend: {
       fontFamily: {
         sans: ['Inter', 'Inter fallback', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        green: {
+          50: '#d6ffee',
+          100: '#acffdd',
+          200: '#83ffcc',
+          300: '#30ffaa',
+          400: '#00dc82',
+          500: '#00bd6f',
+          600: '#009d5d',
+          700: '#007e4a',
+          800: '#005e38',
+          900: '#003f25'
+        }
       }
     }
   }
