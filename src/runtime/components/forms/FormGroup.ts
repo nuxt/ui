nuxt/ui

commit c59595f2c6cf414bf04bb5995ba029a59ef8035b

fix(FormGroup): set `size` default to null
This prevents passing a `size` prop when not specified, especially when having a Checkbox, Radio, etc. underneath that don't support this prop.

src/runtime/components/forms/FormGroup.ts
@@ -19,7 +19,7 @@ export default defineComponent({
     },
     size: {
       type: String,
-      default: () => appConfig.ui.input.default.size,
+      default: null,
       validator (value: string) {
         return Object.keys(appConfig.ui.formGroup.size).includes(value)
       }

commit 83c42cd1ccf81f2aaf64a7fc20bbe3a9cf25b8d5

fix(FormGroup): `size` were invalid since default has been removed
Bug introduced in https://github.com/nuxtlabs/ui/commit/c59595f2c6cf414bf04bb5995ba029a59ef8035b

src/runtime/components/forms/FormGroup.ts
@@ -90,16 +90,18 @@ export default defineComponent({
       return cloneVNode(node, vProps)
     }))
 
+    const size = computed(() => ui.value.size[props.size ?? appConfig.ui.input.default.size])
+
     return () => h('div', { class: [ui.value.wrapper] }, [
-      props.label && h('div', { class: [ui.value.label.wrapper, ui.value.size[props.size]] }, [
+      props.label && h('div', { class: [ui.value.label.wrapper, size.value] }, [
         h('label', { for: props.name, class: [ui.value.label.base, props.required && ui.value.label.required] }, props.label),
         props.hint && h('span', { class: [ui.value.hint] }, props.hint)
       ]),
-      props.description && h('p', { class: [ui.value.description, ui.value.size[props.size]
+      props.description && h('p', { class: [ui.value.description, size.value
       ] }, props.description),
       h('div', { class: [!!props.label && ui.value.container] }, [
         ...clones.value,
-        errorMessage.value ? h('p', { class: [ui.value.error, ui.value.size[props.size]] }, errorMessage.value) : props.help ? h('p', { class: [ui.value.help, ui.value.size[props.size]] }, props.help) : null
+        errorMessage.value ? h('p', { class: [ui.value.error, size.value] }, errorMessage.value) : props.help ? h('p', { class: [ui.value.help, size.value] }, props.help) : null
       ])
     ])
   }