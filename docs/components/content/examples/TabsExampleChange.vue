409f20a16da6fb09dc062a1ceb0d82cb07994dfe
Benjamin Canac <canacb1@gmail.com>
2023-08-04 10:16:36 +0200

    feat(Tabs): control selected index (#490)

---
+++ docs/components/content/examples/TabsExampleChange.vue
@@ -0,0 +1,22 @@
+<script setup>
+const items = [{
+  label: 'Tab1',
+  content: 'This is the content shown for Tab1'
+}, {
+  label: 'Tab2',
+  content: 'And, this is the content for Tab2'
+}, {
+  label: 'Tab3',
+  content: 'Finally, this is the content for Tab3'
+}]
+
+function onChange (index) {
+  const item = items[index]
+
+  alert(`${item.label} was clicked!`)
+}
+</script>
+
+<template>
+  <UTabs :items="items" @change="onChange" />
+</template>
