a8a1c150a00212eeb8cde32ff06ee3b6c45fbedd
171H <40647432+171h@users.noreply.github.com>
2023-08-07 04:23:33 +0800

    fix(Button): add missing prop types (#508)

--- src/runtime/types/button.d.ts
+++ src/runtime/types/button.d.ts
@@ -6,13 +6,16 @@ export interface Button extends Link {
   label?: string
   loading?: boolean
   disabled?: boolean
+  padded?: boolean
   size?: string
   color?: string
   variant?: string
   icon?: string
+  loadingIcon?: string
   leadingIcon?: string
   trailingIcon?: string
   trailing?: boolean
+  leading?: boolean
   to?: string | object
   target?: string
   square?: boolean
