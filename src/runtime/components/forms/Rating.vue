efde94f973c396e400fa9186d328aa95e45d017f
Larra <meixmei0000@gmail.com>
2023-08-09 05:05:44 +0800

    fix: lint

--- src/runtime/components/forms/Rating.vue
+++ src/runtime/components/forms/Rating.vue
@@ -19,99 +19,99 @@
 </template>
 
 <script lang="ts">
-import { computed } from "vue";
-import { defu } from "defu";
-import UIcon from "../elements/Icon.vue";
-import { classNames } from "../../utils";
-import { useFormEvents } from "../../composables/useFormEvents";
-import { useAppConfig } from "#imports";
+import { computed } from 'vue'
+import { defu } from 'defu'
+import UIcon from '../elements/Icon.vue'
+import { classNames } from '../../utils'
+import { useFormEvents } from '../../composables/useFormEvents'
+import { useAppConfig } from '#imports'
 
 // TODO: Remove
 // @ts-expect-error
-import appConfig from "#build/app.config";
+import appConfig from '#build/app.config'
 
 export default defineComponent({
   components: {
-    UIcon,
+    UIcon
   },
   props: {
     modelValue: {
       type: Number,
-      default: 0,
+      default: 0
     },
     icon: {
       type: String,
-      default: () => appConfig.ui.rating.default.icon,
+      default: () => appConfig.ui.rating.default.icon
     },
     max: {
       type: Number,
-      default: () => appConfig.ui.rating.default.max,
+      default: () => appConfig.ui.rating.default.max
     },
     color: {
       type: String,
       default: () => appConfig.ui.rating.default.color,
-      validator(value: string) {
-        return appConfig.ui.colors.includes(value);
-      },
+      validator (value: string) {
+        return appConfig.ui.colors.includes(value)
+      }
     },
     size: {
       type: String,
-      default: () => appConfig.ui.rating.default.size,
+      default: () => appConfig.ui.rating.default.size
     },
     readOnly: {
       type: Boolean,
-      default: false,
+      default: false
     },
     ui: {
       type: Object as PropType<Partial<typeof appConfig.ui.rating>>,
-      default: () => appConfig.ui.rating,
-    },
+      default: () => appConfig.ui.rating
+    }
   },
-  emits: ["update:modelValue"],
-  setup(props, { emit }) {
-    const appConfig = useAppConfig();
+  emits: ['update:modelValue'],
+  setup (props, { emit }) {
+    const appConfig = useAppConfig()
 
     const ui = computed<Partial<typeof appConfig.ui.rating>>(() =>
       defu({}, props.ui, appConfig.ui.rating)
-    );
+    )
 
-    const { emitFormBlur } = useFormEvents();
+    const { emitFormBlur } = useFormEvents()
 
     const rate = computed({
-      get() {
-        return props.modelValue;
+      get () {
+        return props.modelValue
       },
-      set(value) {
-        emit("update:modelValue", value);
-        emitFormBlur();
-      },
-    });
+      set (value) {
+        emit('update:modelValue', value)
+        emitFormBlur()
+      }
+    })
 
     const setRating = (rating: number) => {
-      if (props.readOnly) return;
+      if (props.readOnly) return
 
       if (rate.value == rating) {
-        rate.value = 0;
-        return;
+        rate.value = 0
+        return
       }
 
-      rate.value = rating;
-    };
+      rate.value = rating
+    }
 
     const ratingClass = computed(() => {
       return classNames(
-        ui.value.base.replaceAll("{color}", props.color),
+        ui.value.base.replaceAll('{color}', props.color),
         ui.value.size[props.size]
-      );
-    });
+      )
+    })
 
     return {
       // eslint-disable-next-line vue/no-dupe-keys
       ui,
       rate,
       setRating,
-      ratingClass,
-    };
-  },
-});
+      ratingClass
+    }
+  }
+})
 </script>
