fa1103b4ecd57b913885581f5ab895afa1d67c0b
Benjamin Canac <canacb1@gmail.com>
2023-08-11 17:24:18 +0200

    docs: rebrand to `Nuxt UI`

--- docs/components/Header.vue
+++ docs/components/Header.vue
@@ -2,9 +2,7 @@
   <UHeader>
     <template #left>
       <NuxtLink to="/getting-started" class="flex items-end gap-1.5 font-bold text-xl text-gray-900 dark:text-white">
-        <Logo class="w-8 h-8 text-primary" />
-
-        <span class="hidden sm:block">NuxtLabs</span><span class="sm:text-primary">UI</span>
+        <Logo class="w-auto h-6" />
       </NuxtLink>
     </template>
 
