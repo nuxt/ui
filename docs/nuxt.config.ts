66e0d78a42df67fe1c904ac97b932d7fbed577a6
Sébastien Chopin <seb@nuxt.com>
2023-08-30 18:59:11 +0200

    docs: remove concurrency to 1

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -60,8 +60,6 @@ export default defineNuxtConfig({
   },
   nitro: {
     prerender: {
-      // Waiting for https://github.com/nuxt/nuxt/issues/22763
-      concurrency: 1,
       routes: [
         '/getting-started',
         '/dev/getting-started',
