626a3a2ac7ce55834ed63bcc8c459a5c4356d41f
Benjamin Canac <canacb1@gmail.com>
2023-09-04 13:48:55 +0200

    docs: improve props types (#588)

--- src/runtime/components/forms/Form.vue
+++ src/runtime/components/forms/Form.vue
@@ -10,8 +10,6 @@ import { useEventBus } from '@vueuse/core'
 import type { ZodSchema } from 'zod'
 import type { ValidationError as JoiError, Schema as JoiSchema } from 'joi'
 import type { ObjectSchema as YupObjectSchema, ValidationError as YupError } from 'yup'
-import type { ObjectSchema as ValibotObjectSchema } from 'valibot'
-import { safeParseAsync } from 'valibot'
 import type { FormError, FormEvent, FormEventType, FormSubmitEvent, Form } from '../../types/form'
 
 export default defineComponent({
