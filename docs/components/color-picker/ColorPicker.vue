bc2315b7d96e2eb7a5accd6a613ad41a463291c4
Benjamin Canac <canacb1@gmail.com>
2023-09-11 11:25:23 +0200

    docs: improve accessibility

--- docs/components/color-picker/ColorPicker.vue
+++ docs/components/color-picker/ColorPicker.vue
@@ -1,7 +1,7 @@
 <template>
   <UPopover mode="hover">
     <template #default="{ open }">
-      <UButton color="gray" variant="ghost" square :class="[open && 'bg-gray-50 dark:bg-gray-800']">
+      <UButton color="gray" variant="ghost" square :class="[open && 'bg-gray-50 dark:bg-gray-800']" aria-label="Color picker">
         <UIcon name="i-heroicons-swatch-20-solid" class="w-5 h-5 text-primary-500 dark:text-primary-400" />
       </UButton>
     </template>
