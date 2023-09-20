f983c974c40bc04ee77f816979488872998980bb
Benjamin Canac <canacb1@gmail.com>
2023-09-20 18:51:29 +0200

    chore(scripts): remove `pnpm install`

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,9 +3,6 @@
 # Restore all git changes
 git restore -s@ -SW  -- .
 
-# Resolve pnpm
-pnpm install
-
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
