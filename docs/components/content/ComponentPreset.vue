d379c579c08903a93606eae6d03bf181d3ee3e22
Benjamin Canac <canacb1@gmail.com>
2023-09-21 11:12:03 +0200

    docs: fix preset display

--- docs/components/content/ComponentPreset.vue
+++ docs/components/content/ComponentPreset.vue
@@ -6,6 +6,7 @@
 // @ts-expect-error
 import { transformContent } from '@nuxt/content/transformers'
 import { upperFirst, camelCase } from 'scule'
+import * as config from '#ui/ui.config'
 
 const props = defineProps({
   slug: {
@@ -14,17 +15,16 @@ const props = defineProps({
   }
 })
 
-const appConfig = useAppConfig()
 const route = useRoute()
 // eslint-disable-next-line vue/no-dupe-keys
 const slug = props.slug || route.params.slug[route.params.slug.length - 1]
 const camelName = camelCase(slug)
 const name = `U${upperFirst(camelName)}`
 
-const preset = appConfig.ui[camelName]
+const preset = config[camelName]
 
 const { data: ast } = await useAsyncData(`${name}-preset`, () => transformContent('content:_markdown.md', `
-\`\`\`json [appConfig.ui.${camelName}]
+\`\`\`json
 ${JSON.stringify(preset, null, 2)}
 \`\`\`\
 `, {
