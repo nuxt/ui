13bd4de60054cd5aafc3cae8fb88e5f391359524
Haytham Salama <haythamasalama@gmail.com>
2023-09-22 23:21:53 +0300

    fix(Pagination): correct `displayedPages()`

--- src/runtime/components/navigation/Pagination.vue
+++ src/runtime/components/navigation/Pagination.vue
@@ -127,53 +127,42 @@ export default defineComponent({
     const pages = computed(() => Array.from({ length: Math.ceil(props.total / props.pageCount) }, (_, i) => i + 1))
 
     const displayedPages = computed(() => {
-      if (!props.max || pages.value.length <= 5) {
-        return pages.value
-      } else {
-        const current = currentPage.value
-        const max = pages.value.length
-        const r = Math.floor((Math.min(props.max, max) - 5) / 2)
-        const r1 = current - r
-        const r2 = current + r
-        const beforeWrapped = r1 - 1 > 1
-        const afterWrapped = r2 + 1 < max
-        const items: Array<number | string> = [1]
+      const totalItems = props.total
+      const current = currentPage.value
+      const perPage = props.pageCount
+      const lastPage = Math.ceil(totalItems / perPage)
 
-        if (beforeWrapped) items.push(props.divider)
+      const maxDisplayedPages = Math.min(props.max, perPage)
+      const halfDisplayedPages = Math.floor(maxDisplayedPages / 2)
+      const startDisplayedPage = Math.max(2, current - halfDisplayedPages)
+      const endDisplayedPage = Math.min(startDisplayedPage + maxDisplayedPages - 2, Math.ceil(totalItems / perPage))
 
-        if (!afterWrapped) {
-          const addedItems = (current + r + 2) - max
-          for (let i = current - r - addedItems; i <= current - r - 1; i++) {
-            items.push(i)
-          }
-        }
+      const items = []
 
-        for (let i = r1 > 2 ? (r1) : 2; i <= Math.min(max, r2); i++) {
+      if (totalItems <= maxDisplayedPages) {
+        for (let i = 1; i <= totalItems; i++) {
           items.push(i)
         }
-
-        if (!beforeWrapped) {
-          const addedItems = 1 - (current - r - 2)
-          for (let i = current + r + 1; i <= current + r + addedItems; i++) {
-            items.push(i)
-          }
-        }
-
-        if (afterWrapped) items.push(props.divider)
-
-        if (r2 < max) items.push(max)
-
-        // Replace divider by number on start edge case [1, '…', 3, ...]
-        if (items.length >= 3 && items[1] === props.divider && items[2] === 3) {
-          items[1] = 2
-        }
-        // Replace divider by number on end edge case [..., 48, '…', 50]
-        if (items.length >= 3 && items[items.length - 2] === props.divider && items[items.length - 1] === items.length) {
-          items[items.length - 2] = items.length - 1
-        }
-
         return items
       }
+
+      items.push(1)
+
+      if (current > halfDisplayedPages + 1) {
+        items.push(props.divider)
+      }
+
+      for (let i = startDisplayedPage; i < endDisplayedPage; i++) {
+        items.push(i)
+      }
+
+      if (current < lastPage - halfDisplayedPages) {
+        items.push(props.divider)
+      }
+
+      items.push(lastPage)
+
+      return items
     })
 
     const canGoPrev = computed(() => currentPage.value > 1)
