a7adcd9f729a650bb06efc17e63b81e6a4b2194d
Benjamin Canac <canacb1@gmail.com>
2023-09-28 18:23:49 +0200

    up

--- docs/components/content/examples/FormGroupErrorSlotExample.vue
+++ docs/components/content/examples/FormGroupErrorSlotExample.vue
@@ -3,8 +3,10 @@
     <template #default="{ error }">
       <UInput v-model="email" type="email" placeholder="Enter email" :trailing-icon="error ? 'i-heroicons-exclamation-triangle-20-solid' : undefined" />
     </template>
+
     <template #error="{ error }">
-      <UAlert icon="i-heroicons-exclamation-triangle-20-solid" :description="error" color="red" />
+      <UAlert v-if="error" icon="i-heroicons-exclamation-triangle-20-solid" :title="error" color="red" />
+      <UAlert v-else icon="i-heroicons-check-circle-20-solid" title="Your email is valid" color="green" />
     </template>
   </UFormGroup>
 </template>
