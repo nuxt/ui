7d48380fb07103ded3182ab4df045a1f5dc8a2aa
Pooya Parsa <pooya@pi0.io>
2023-09-28 22:34:42 +0200

    try linking config only

--- docs/modules/vercel.ts
+++ docs/modules/vercel.ts
@@ -5,12 +5,12 @@ import { resolve, dirname } from 'node:path'
 export default defineNuxtModule({
   setup (_, nuxt) {
     nuxt.hook('nitro:build:public-assets', () => {
-      const vercelDir = resolve(nuxt.options.rootDir, '.vercel/output')
-      const workspaceVercelDir = resolve(nuxt.options.workspaceDir, '.vercel/output')
+      const vercelConfig = resolve(nuxt.options.rootDir, '.vercel/output/config.json')
+      const workspaceVercelConfig = resolve(nuxt.options.workspaceDir, '.vercel/output/config.json')
       try {
-        mkdirSync(dirname(workspaceVercelDir), { recursive: true })
-        mkdirSync(dirname(vercelDir), { recursive: true })
-        symlinkSync(vercelDir, workspaceVercelDir, 'junction')
+        mkdirSync(dirname(vercelConfig), { recursive: true })
+        mkdirSync(dirname(workspaceVercelConfig), { recursive: true })
+        symlinkSync(vercelConfig, workspaceVercelConfig)
       } catch (err) {
         console.error(err)
       }
