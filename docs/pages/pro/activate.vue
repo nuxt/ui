c6cd2731418871cff4e71ae4228f5f1134f00361
Benjamin Canac <canacb1@gmail.com>
2023-09-29 14:49:39 +0200

    up

--- docs/pages/pro/activate.vue
+++ docs/pages/pro/activate.vue
@@ -18,30 +18,34 @@ async function submit (event: FormSubmitEvent<any>) {
 </script>
 
 <template>
-  <UContainer>
-    <UPage>
-      <UPageHero title="Activate your license" description="Enable Nuxt UI Pro in your production projects by activating your license key received by email and get invited to the GitHub private repository." />
-      <UCard class="lg:w-1/2">
-        <UForm
-          :validate="validate"
-          :state="state"
-          @submit="submit"
-        >
-          <UFormGroup label="License Key" name="license">
-            <UInput v-model="state.license" />
-          </UFormGroup>
-          <UFormGroup label="GitHub Username" name="username">
-            <UInput v-model="state.username" />
-          </UFormGroup>
-          <UButton type="submit">
-            Submit
-          </UButton>
-        </UForm>
-      </UCard>
-      <p class="text-sm text-gray-600 dark:text-gray-400 mt-4">
-        If you purchased the team license, activate the license for each team member.
-      </p>
-      <div class="h-20" />
-    </UPage>
-  </UContainer>
+  <UMain>
+    <UContainer>
+      <UPageHero title="Activate your license" description="Enable Nuxt UI Pro in your production projects by activating your license key received by email and get invited to the GitHub private repository." align="center" />
+
+      <UPage>
+        <UCard>
+          <UForm
+            :validate="validate"
+            :state="state"
+            class="space-y-4"
+            @submit="submit"
+          >
+            <UFormGroup label="License Key" name="license">
+              <UInput v-model="state.license" />
+            </UFormGroup>
+
+            <UFormGroup label="GitHub Username" name="username">
+              <UInput v-model="state.username" />
+            </UFormGroup>
+            <UButton type="submit">
+              Submit
+            </UButton>
+          </UForm>
+        </UCard>
+        <p class="text-sm text-gray-600 dark:text-gray-400 mt-4">
+          If you purchased the team license, activate the license for each team member.
+        </p>
+      </UPage>
+    </UContainer>
+  </UMain>
 </template>
