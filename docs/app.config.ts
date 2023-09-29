65504ca3932306a176cc92c6cd15b246f05e21e6
Haytham Salama <haythamasalama@gmail.com>
2023-09-30 02:30:18 +0300

    chore: update background color when white for `select` ,`selectMenu`,`input`

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -1,6 +1,30 @@
 export default defineAppConfig({
   ui: {
     primary: 'green',
-    gray: 'slate'
+    gray: 'slate',
+
+    input: {
+      color: {
+        white: {
+          'background': ''
+        }
+      }
+    },
+
+    select: {
+      color: {
+        white: {
+          'background': ''
+        }
+      }
+    },
+
+    selectMenu: {
+      color: {
+        white: {
+          'background': ''
+        }
+      }
+    }
   }
 })
