00f47d859ece979f56dcc5677a67af09d5d7f67b
Florent Delerue <florentdelerue@hotmail.com>
2023-10-03 17:45:27 +0200

    feat: new resizable component

---
+++ docs/components/pro/ProResizable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="panels" class="h-auto min-h-auto aspect-video w-full grid grid-cols-[auto,_1fr] overflow-hidden dark:ring-gray-800 rounded-xl">
+    <div class="relative">
+      <div
+        class="resizer hidden min-[371px]:block mr-2 overflow-auto z-0 w-[11px] h-full relative resize-x
+        before:content-[''] before:w-[105%] before:h-[105%] before:bg-transparent before:pointer-events-none before:block before:absolute
+        [&::-webkit-resizer]:bg-transparent [&::-webkit-scrollbar]:w-[10px] [&::-webkit-scrollbar]:h-[500px] [&::-webkit-scrollbar-corner]:bg-none"
+      />
+      <div class="bg-gray-300 dark:bg-gray-700 rounded-full h-[42px] w-[10px] absolute right-0 top-[45%] -z-[1] mr-2" />
+    </div>
+    <div class="relative min-w-[280px] border border-gray-300 dark:border-gray-700 rounded-xl">
+      <div class="overflow-auto m-0">
+        <div class="m-4 h-full">
+          <slot />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const panels = ref<HTMLElement>()
+const panelsWidth = ref('')
+
+onMounted(() => panelsWidth.value = `${panels.value.offsetWidth}px`)
+
+</script>
+
+<style scoped lang="postcss">
+@media (min-width: 371px) {
+  .resizer {
+    max-width: calc(v-bind(panelsWidth) - 330px);
+  }
+}
+</style>
