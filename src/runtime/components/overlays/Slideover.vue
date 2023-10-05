a1fb4da7277e4803aaf90358e08331b454c42389
Haytham Salama <haythamasalama@gmail.com>
2023-10-05 17:47:31 +0300

    up

--- src/runtime/components/overlays/Slideover.vue
+++ src/runtime/components/overlays/Slideover.vue
@@ -45,7 +45,7 @@ export default defineComponent({
       default: false
     },
     side: {
-      type: String,
+      type: String as PropType<'left' | 'right'>,
       default: 'right',
       validator: (value: string) => ['left', 'right'].includes(value)
     },
