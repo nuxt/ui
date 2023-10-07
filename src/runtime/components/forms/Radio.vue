5d45f876693755c00a90f623b9fe001acf0d39d3
Romain Hamel <romain@boilr.io>
2023-10-07 12:45:42 +0200

    fix(Radio): missing imports

--- src/runtime/components/forms/Radio.vue
+++ src/runtime/components/forms/Radio.vue
@@ -27,7 +27,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, inject, inject } from 'vue'
+import { computed, defineComponent, inject, toRef } from 'vue'
 import type { PropType } from 'vue'
 import { twMerge, twJoin } from 'tailwind-merge'
 import { useUI } from '../../composables/useUI'
