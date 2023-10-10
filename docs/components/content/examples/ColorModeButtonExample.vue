f2cff0c5c930db76c9ff7ab76d70ba71f76d83ac
Haytham Salama <haythamasalama@gmail.com>
2023-10-10 16:42:17 +0300

    refactor: show the code instead of the link

---
+++ docs/components/content/examples/ColorModeButtonExample.vue
@@ -0,0 +1,28 @@
+<script setup>
+const colorMode = useColorMode()
+
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+</script>
+
+<template>
+  <ClientOnly>
+    <UButton
+      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+      color="gray"
+      variant="ghost"
+      aria-label="Theme"
+      @click="isDark = !isDark"
+    />
+
+    <template #fallback>
+      <div class="w-8 h-8" />
+    </template>
+  </ClientOnly>
+</template>
