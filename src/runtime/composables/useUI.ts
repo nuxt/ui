0bd12fdf9236883094ecf7b2b79f3dcce4fd23aa
Benjamin Canac <canacb1@gmail.com>
2023-10-12 15:27:22 +0200

    chore(useUI): add option to force `appConfig` merge

--- src/runtime/composables/useUI.ts
+++ src/runtime/composables/useUI.ts
@@ -4,7 +4,7 @@ import { useAppConfig } from '#imports'
 import { mergeConfig, omit, get } from '../utils'
 import { Strategy } from '../types'
 
-export const useUI = <T>(key, $ui: Ref<Partial<T & { strategy: Strategy }> | undefined>, $config?: Ref<T> | T, $wrapperClass?: Ref<string>) => {
+export const useUI = <T>(key, $ui: Ref<Partial<T & { strategy: Strategy }> | undefined>, $config?: Ref<T> | T, $wrapperClass?: Ref<string>, withAppConfig: boolean = false) => {
   const $attrs = useAttrs()
   const appConfig = useAppConfig()
 
@@ -12,11 +12,12 @@ export const useUI = <T>(key, $ui: Ref<Partial<T & { strategy: Strategy }> | und
     const _ui = toValue($ui)
     const _config = toValue($config)
     const _wrapperClass = toValue($wrapperClass)
+
     return mergeConfig<T>(
       _ui?.strategy || (appConfig.ui?.strategy as Strategy),
       _wrapperClass ? { wrapper: _wrapperClass } : {},
       _ui || {},
-      process.dev ? get(appConfig.ui, key, {}) : {},
+      (process.dev || withAppConfig) ? get(appConfig.ui, key, {}) : {},
       _config || {}
     )
   })
