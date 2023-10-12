92b86186e7b8a987eec1da9cf45a0ec378d421cf
Nils Olsson <nilso@enosis.net>
2023-10-12 03:14:40 -0700

    feat(Popover): manual mode & horizontal offset (#781)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>
    Co-authored-by: Lnunu <62177121+Lnunu@users.noreply.github.com>

---
+++ docs/components/content/examples/PopoverExampleOpen.vue
@@ -0,0 +1,18 @@
+<script setup>
+const open = ref(false)
+</script>
+
+<template>
+  <div class="flex gap-4 items-center">
+    <UToggle v-model="open" />
+    <UPopover :open="open">
+      <UButton color="white" label="Open" trailing-icon="i-heroicons-chevron-down-20-solid" />
+
+      <template #panel>
+        <div class="p-4">
+          <Placeholder class="h-20 w-48" />
+        </div>
+      </template>
+    </UPopover>
+  </div>
+</template>
