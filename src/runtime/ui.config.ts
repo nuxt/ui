ceaa6ff1b36ab2b4e5f4cc9f117b40987f71a0d1
Conner Blanton <connerblanton@gmail.com>
2023-10-26 18:02:23 -0500

    changing default popper arrow background color

--- src/runtime/ui.config.ts
+++ src/runtime/ui.config.ts
@@ -4,7 +4,7 @@ const _popperArrowPresets = {
   base: 'before:w-2 before:h-2',
   ring: 'before:ring-1 before:ring-gray-200 dark:before:ring-gray-800',
   rounded: 'before:rounded-sm',
-  background: 'before:bg-white dark:before:bg-gray-900',
+  background: 'before:bg-gray-200 dark:before:bg-gray-800',
   shadow: 'before:shadow',
   placement: 'group-data-[popper-placement*="right"]:-left-1 group-data-[popper-placement*="left"]:-right-1 group-data-[popper-placement*="top"]:-bottom-1 group-data-[popper-placement*="bottom"]:-top-1'
 }
