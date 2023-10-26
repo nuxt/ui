d3a0e8041da4029bee5bd9bf8ee3c8e925f409f8
Benjamin Canac <canacb1@gmail.com>
2023-10-26 11:50:53 +0200

    feat(CommandPalette): handle `filter` attribute in groups

--- docs/components/content/examples/CommandPaletteExampleAsync.vue
+++ docs/components/content/examples/CommandPaletteExampleAsync.vue
@@ -1,19 +1,17 @@
 <script setup>
-const groups = computed(() => {
-  return [{
-    key: 'users',
-    label: q => q && `Users matching “${q}”...`,
-    search: async (q) => {
-      if (!q) {
-        return []
-      }
-
-      const users = await $fetch('https://jsonplaceholder.typicode.com/users', { params: { q } })
-
-      return users.map(user => ({ id: user.id, label: user.name, suffix: user.email }))
+const groups = [{
+  key: 'users',
+  label: q => q && `Users matching “${q}”...`,
+  search: async (q) => {
+    if (!q) {
+      return []
     }
-  }].filter(Boolean)
-})
+
+    const users = await $fetch('https://jsonplaceholder.typicode.com/users', { params: { q } })
+
+    return users.map(user => ({ id: user.id, label: user.name, suffix: user.email }))
+  }
+}]
 </script>
 
 <template>
