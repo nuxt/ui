18e8d28272efbf89abb5eaedbffcd6b7705b22c1
Benjamin Canac <canacb1@gmail.com>
2023-10-27 22:08:29 +0200

    chore(MeterGroup): consistency when multiple `useUI`

--- src/runtime/ui.config.ts
+++ src/runtime/ui.config.ts
@@ -537,8 +537,8 @@ export const meter = {
 }
 
 export const meterGroup = {
-  base: 'flex flex-col gap-2 w-full',
-  wrapper: 'flex flex-row flex-nowrap flex-shrink overflow-hidden',
+  wrapper: 'flex flex-col gap-2 w-full',
+  base: 'flex flex-row flex-nowrap flex-shrink overflow-hidden',
   background: 'bg-gray-200 dark:bg-gray-700',
   transition: 'transition-all',
   rounded: 'rounded-full',
