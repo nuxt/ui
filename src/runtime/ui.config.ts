a08fce086fc7047c0436a47c51708e05e0d487a4
Conner Blanton <connerblanton@gmail.com>
2023-10-28 16:55:57 -0500

    updating chip sizes

--- src/runtime/ui.config.ts
+++ src/runtime/ui.config.ts
@@ -278,15 +278,15 @@ export const chip = {
     'bottom-left': 'bottom-0 left-0 translate-y-1/2 -translate-x-1/2 transform'
   },
   size: {
-    '3xs': 'h-[4px] min-w-[4px] text-[4px] p-px',
-    '2xs': 'h-[5px] min-w-[5px] text-[5px] p-px',
-    xs: 'h-1.5 min-w-[0.375rem] text-[6px] p-px',
-    sm: 'h-2 min-w-[0.5rem] text-[7px] p-0.5',
-    md: 'h-2.5 min-w-[0.625rem] text-[8px] p-0.5',
-    lg: 'h-3 min-w-[0.75rem] text-[10px] p-0.5',
-    xl: 'h-3.5 min-w-[0.875rem] text-[11px] p-1',
-    '2xl': 'h-4 min-w-[1rem] text-[12px] p-1',
-    '3xl': 'h-5 min-w-[1.25rem] text-[14px] p-1'
+    '3xs': 'h-[4px] min-w-[4px] text-[4px]',
+    '2xs': 'h-[5px] min-w-[5px] text-[5px]',
+    xs: 'h-1.5 min-w-[0.375rem] text-[6px]',
+    sm: 'h-2 min-w-[0.5rem] text-[6px]',
+    md: 'h-2.5 min-w-[0.625rem] text-[8px]',
+    lg: 'h-3 min-w-[0.75rem] text-[10px]',
+    xl: 'h-3.5 min-w-[0.875rem] text-[10px]',
+    '2xl': 'h-4 min-w-[1rem] text-[12px]',
+    '3xl': 'h-5 min-w-[1.25rem] text-[14px]'
   },
   default: {
     size: 'sm',
