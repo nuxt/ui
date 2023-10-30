6cb0673252ed3cd5f50da4cfcccf281d1a42ab94
Juho Rutila <juho.rutila@gmail.com>
2023-10-30 23:05:26 +0200

    test: add first tailwind config test

--- test/basic.spec.ts
+++ test/basic.spec.ts
@@ -2,28 +2,26 @@ import { describe, expect, it } from 'vitest'
 import module from '../src/module'
 import { loadNuxt } from '@nuxt/kit'
 import { join } from 'path'
+import { getIconCollections, iconsPlugin } from '@egoist/tailwindcss-icons'
 
-async function getTailwindCSSConfig (): Promise<[any, any]> {
+// TODO: fix these anys
+async function getTailwindCSSConfig (overrides: any = {}): Promise<[any, any]> {
+  overrides.modules = [ module ]
+  overrides.ssr = overrides.ssr ?? false
+  overrides.hooks = overrides.hooks ?? {}
   return new Promise((resolve, reject) => {
+    overrides.hooks['tailwindcss:resolvedConfig'] = async (config) => {
+        resolve([config, nuxt])
+      }
     const nuxt = loadNuxt({
       cwd: join(process.cwd(), 'fixtures', 'empty'),
       dev: false,
-      overrides:
-      {
-        ssr: false,
-        modules: [ module ],
-        hooks: {
-          'tailwindcss:resolvedConfig': async (config) => {
-            console.log('resolvedConfig in test')
-            resolve([config, nuxt])
-          }
-        }
-      }
+      overrides
     })
   })
 }
 
-describe('basic', () => {
+describe('nuxt', () => {
   it('should add plugins and modules to nuxt', async () => {
     const [_config, lnuxt] = await getTailwindCSSConfig()
     await lnuxt.then((nuxt) => {
@@ -37,6 +35,26 @@ describe('basic', () => {
         '@nuxtjs/color-mode': true,
         '@nuxtjs/tailwindcss': true
       })
+      // default values in appConfig
+      expect(nuxt.options.appConfig.ui).toContain({
+        primary: 'green',
+        gray: 'cool'
+      })
+    })
+  })
+})
+
+describe.only('tailwind', () => {
+  it.each([
+    [ ['myColor'], '', /bg-(myColor|primary)-50/ ]
+  ])('smart safelist %s should %s generate safelist %s', async (safelistColors, not, safelistPattern) => {
+    const [config, _nuxt] = await getTailwindCSSConfig({
+      ui: {
+        safelistColors
+      }
+    })
+    expect(config.safelist).toContainEqual({
+      pattern: safelistPattern
     })
   })
 })
