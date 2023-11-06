a41e00f21c9b2a8a74898d149bc9297b5388a099
Juho Rutila <juho.rutila@gmail.com>
2023-11-06 20:51:11 +0200

    test: more snapshot based button tests

--- test/components/Button.spec.ts
+++ test/components/Button.spec.ts
@@ -3,12 +3,30 @@ import { describe, it, expect } from 'vitest'
 import { mountSuspended } from 'nuxt-vitest/utils'
 import Button from '../../src/runtime/components/elements/Button.vue'
 
+type ButtonOptions = TypeOf<typeof Button.props>
+
 describe.only('Button', () => {
   it.each([
-    [ 'basic case', { slots: { default: () => 'hello' } } ],
-    [ 'leading icon', { props: { leading: true, icon: 'heroicons-check' }, slots: { default: () => 'leading icon' } } ],
-  ])('renders %s correctly', async (_, options) => {
-    const component = await mountSuspended(Button, options)
-    expect(component.html()).toMatchSnapshot()
+    [ 'basic case', { } ],
+    [ 'leading icon', { props: { leading: true, icon: 'heroicons-check' } } ],
+    [ 'black solid', { props: { color: 'black', variant: 'solid' } } ],
+    [ 'rounded full', { props: { ui: { rounded: 'rounded-full' } } } ],
+    [ '<UButton icon="i-heroicons-pencil-square" size="sm" color="primary" square variant="solid" />' ]
+  ])('renders %s correctly', async (nameOrHtml: string, options: ButtonOptions) => {
+    let html
+    if (options === undefined) {
+      const app = {
+        template: nameOrHtml,
+        components: { UButton: Button }
+      }
+      const result = await mountSuspended(app)
+      html = result.html()
+    } else {
+      options.slots = options.slots || { default: () => 'label' }
+      options.slots.default = options.slots.default || (() => 'label')
+      const component = await mountSuspended(Button, options)
+      html = component.html()
+    }
+    expect(html).toMatchSnapshot()
   })
 })
