6fb1c91c3a8492e14c9d4d4567a23ae1624cc7ac
Juho Rutila <juho.rutila@gmail.com>
2023-11-06 21:09:29 +0200

    test: generalize the renderer

--- test/components/Button.spec.ts
+++ test/components/Button.spec.ts
@@ -1,7 +1,8 @@
 // @vitest-environment nuxt
 import { describe, it, expect } from 'vitest'
-import { mountSuspended } from 'nuxt-vitest/utils'
 import Button from '../../src/runtime/components/elements/Button.vue'
+import type { TypeOf } from 'zod'
+import ComponentRender from './component-render'
 
 type ButtonOptions = TypeOf<typeof Button.props>
 
@@ -13,20 +14,7 @@ describe.only('Button', () => {
     [ 'rounded full', { props: { ui: { rounded: 'rounded-full' } } } ],
     [ '<UButton icon="i-heroicons-pencil-square" size="sm" color="primary" square variant="solid" />' ]
   ])('renders %s correctly', async (nameOrHtml: string, options: ButtonOptions) => {
-    let html
-    if (options === undefined) {
-      const app = {
-        template: nameOrHtml,
-        components: { UButton: Button }
-      }
-      const result = await mountSuspended(app)
-      html = result.html()
-    } else {
-      options.slots = options.slots || { default: () => 'label' }
-      options.slots.default = options.slots.default || (() => 'label')
-      const component = await mountSuspended(Button, options)
-      html = component.html()
-    }
+    const html = await ComponentRender(nameOrHtml, options, Button)
     expect(html).toMatchSnapshot()
   })
 })
---
+++ test/components/component-render.ts
@@ -0,0 +1,21 @@
+import { mountSuspended } from 'nuxt-vitest/utils'
+import path from 'path'
+
+export default async function (nameOrHtml: string, options: any, component: any) {
+  let html
+  const name = path.parse(component.__file).name
+  if (options === undefined) {
+    const app = {
+      template: nameOrHtml,
+      components: { [`U${name}`]: component }
+    }
+    const result = await mountSuspended(app)
+    html = result.html()
+  } else {
+    options.slots = options.slots || { default: () => 'label' }
+    options.slots.default = options.slots.default || (() => 'label')
+    const cResult = await mountSuspended(component, options)
+    html = cResult.html()
+  }
+  return html
+}
