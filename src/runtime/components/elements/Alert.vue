9a1a1b8caf1c040c458230458b9fa9cbfb32a1bb
Benjamin Canac <canacb1@gmail.com>
2023-11-14 15:55:39 +0100

    fix(Alert): prevent `gap` when no close button or action
    Fixes #831

--- src/runtime/components/elements/Alert.vue
+++ src/runtime/components/elements/Alert.vue
@@ -20,7 +20,7 @@
           <UButton v-for="(action, index) of actions" :key="index" v-bind="{ ...ui.default.actionButton, ...action }" @click.stop="action.click" />
         </div>
       </div>
-      <div class="flex-shrink-0 flex items-center gap-3">
+      <div v-if="closeButton || (!description && !$slots.description && actions.length)" class="flex-shrink-0 flex items-center gap-3">
         <div v-if="!description && !$slots.description && actions.length" class="flex items-center gap-2">
           <UButton v-for="(action, index) of actions" :key="index" v-bind="{ ...ui.default.actionButton, ...action }" @click.stop="action.click" />
         </div>
