5e0e2090b0e20968050ac881866c4b0300eeb19c
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2023-11-16 01:40:18 -0300

    Minor style fix [skip ci]

--- docs/components/content/examples/LadderExampleStepObjects.vue
+++ docs/components/content/examples/LadderExampleStepObjects.vue
@@ -7,7 +7,7 @@ const steps = [
   { label: 'Address', icon: 'i-heroicons-home' },
   { label: 'Shipping', icon: 'i-heroicons-truck' },
   { label: 'Trade-in', icon: 'i-heroicons-arrow-path', disabled: true },
-  { label: 'Payment', icon: 'i-heroicons-banknotes', click: () => alert('You clicked Payment!')},
+  { label: 'Payment', icon: 'i-heroicons-banknotes', click: () => alert('You clicked Payment!') },
   { label: 'Checkout', icon: 'i-heroicons-check' }
 ]
 </script>
