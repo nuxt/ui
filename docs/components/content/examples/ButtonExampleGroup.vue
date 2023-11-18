73d5642a9cd0538ef8275bc2d194115792674943
Conner Blanton <connerblanton@gmail.com>
2023-11-18 12:03:50 -0600

    updated docs

---
+++ docs/components/content/examples/ButtonExampleGroup.vue
@@ -0,0 +1,11 @@
+<template>
+  <UButtonGroup>
+    <USelect :options="['Email', 'Text']" placeholder="Delivery" />
+    <USelectMenu :options="['Benjamin Canac', 'Anthony Fu', 'Daniel Roe']" placeholder="Send To" />
+    <UInput placeholder="Message" />
+    <UButton icon="i-heroicons-clipboard-document" color="gray" />
+    <UDropdown :items="[[{ label: 'Now' }, { label: 'Later' }]]">
+      <UButton icon="i-heroicons-paper-airplane" color="gray" />
+    </UDropdown>
+  </UButtonGroup>
+</template>
