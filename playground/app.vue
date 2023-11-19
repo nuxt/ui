56b4b313d656a399142625020f76e64504865baf
Conner Blanton <connerblanton@gmail.com>
2023-11-19 15:07:05 -0600

    restoring playground app.vue

--- playground/app.vue
+++ playground/app.vue
@@ -1,59 +1,19 @@
 <template>
   <UContainer class="min-h-screen flex items-center">
-        <UButtonGroup size="lg" orientation="horizontal">
-          <UButton label="Action" color="white" />
-          <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-            <UButton class="w-full" color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
-          </UDropdown>
-          <UInput placeholder="Search" />
-          <UBadge label="New" color="blue" />
-          <USelect :options="['All', 'Active', 'Completed']" placeholder="Select" />
-          <USelectMenu :options="['All', 'Active', 'Completed']" placeholder="Select Menu" />
-        </UButtonGroup>
-        <!-- <div class="flex gap-2">
-          <UButton label="Action" color="white" />
-          <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-            <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" />
-          </UDropdown>
-          <UBadge label="New" color="blue" />
-          <UInput placeholder="Search" />
-          <USelect :options="['All', 'Active', 'Completed']" placeholder="Select" />
-          <USelectMenu :options="['All', 'Active', 'Completed']" placeholder="Select Menu" />
-        </div> -->
+    <UCard class="flex-1" :ui="{ background: 'bg-gray-50 dark:bg-gray-800/50', ring: 'ring-1 ring-gray-300 dark:ring-gray-700', divide: 'divide-y divide-gray-300 dark:divide-gray-700', header: { base: 'font-bold' } }">
+      <template #header>
+        Welcome to the playground!
+      </template>
+
+      <p class="text-gray-500 dark:text-gray-400">
+        Try your components here!
+      </p>
+    </UCard>
   </UContainer>
 </template>
 
 <script setup>
-const items = [
-  [{
-    label: 'Profile',
-    avatar: {
-      src: 'https://avatars.githubusercontent.com/u/739984?v=4'
-    }
-  }], [{
-    label: 'Edit',
-    icon: 'i-heroicons-pencil-square-20-solid',
-    shortcuts: ['E'],
-    click: () => {
-      console.log('Edit')
-    }
-  }, {
-    label: 'Duplicate',
-    icon: 'i-heroicons-document-duplicate-20-solid',
-    shortcuts: ['D'],
-    disabled: true
-  }], [{
-    label: 'Archive',
-    icon: 'i-heroicons-archive-box-20-solid'
-  }, {
-    label: 'Move',
-    icon: 'i-heroicons-arrow-right-circle-20-solid'
-  }], [{
-    label: 'Delete',
-    icon: 'i-heroicons-trash-20-solid',
-    shortcuts: ['⌘', 'D']
-  }]
-]
+
 </script>
 
 <style>
