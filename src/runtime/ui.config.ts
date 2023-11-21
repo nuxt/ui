11ccbbb24ef61e6bd3bb703f950955dd21d6a3eb
Italo <DarkGhostHunter@Gmail.com>
2023-11-21 14:59:42 -0300

    feat(SelectMenu): allows to clear search query on close (#968)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

--- src/runtime/ui.config.ts
+++ src/runtime/ui.config.ts
@@ -790,7 +790,8 @@ export const selectMenu = {
     placement: 'bottom-end'
   },
   default: {
-    selectedIcon: 'i-heroicons-check-20-solid'
+    selectedIcon: 'i-heroicons-check-20-solid',
+    clearOnClose: false
   },
   arrow: {
     ..._popperArrow,
