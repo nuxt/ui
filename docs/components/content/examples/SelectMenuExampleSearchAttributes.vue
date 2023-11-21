d57efc8763906650078c9fe5b80e2a53d56bdc9f
Benjamin Canac <canacb1@gmail.com>
2023-11-21 22:16:51 +0100

    up

--- docs/components/content/examples/SelectMenuExampleSearchAttributes.vue
+++ docs/components/content/examples/SelectMenuExampleSearchAttributes.vue
@@ -2,9 +2,10 @@
 const options = [
   { id: 1, name: 'Wade Cooper', favoriteColors: ['red', 'yellow'] },
   { id: 2, name: 'Arlene Mccoy', favoriteColors: ['blue', 'yellow'] },
-  { id: 3, name: 'Devon Webb', favoriteColors: ['black', 'blue'] },
-  { id: 4, name: 'Tom Cook', favoriteColors: ['yellow', 'blue'] },
-  { id: 5, name: 'Tanya Fox', favoriteColors: ['green', 'red'] }
+  { id: 3, name: 'Devon Webb', favoriteColors: ['green', 'blue'] },
+  { id: 4, name: 'Tom Cook', favoriteColors: ['blue', 'red'] },
+  { id: 5, name: 'Tanya Fox', favoriteColors: ['green', 'red'] },
+  { id: 5, name: 'Hellen Schmidt', favoriteColors: ['green', 'yellow'] }
 ]
 
 const selected = ref(options[1])
@@ -23,7 +24,7 @@ const selected = ref(options[1])
     :search-attributes="['name', 'favoriteColors']"
   >
     <template #option="{ option: person }">
-      <span v-for="color in person.favoriteColors" :key="color.id" class="h-2 w-2 rounded-full mr-2" :class="`bg-${color}-500`" />
+      <span v-for="color in person.favoriteColors" :key="color.id" class="h-2 w-2 rounded-full" :class="`bg-${color}-500 dark:bg-${color}-400`" />
       <span class="truncate">{{ person.name }}</span>
     </template>
   </USelectMenu>
