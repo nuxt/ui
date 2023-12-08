363669e1d9e7ae978bd073d80efcd00784c644bf
Inesh Bose <dev@inesh.xyz>
2023-12-08 19:39:17 +0000

    chore: finish

--- src/runtime/components/forms/SelectMenu.vue
+++ src/runtime/components/forms/SelectMenu.vue
@@ -67,9 +67,6 @@
               :class="uiMenu.input"
               @change="query = $event.target.value"
             />
-
-            <component :is="createOptionVNode" v-if="creatable === 'always' && showCreateOption" as="template" />
-
             <component
               :is="searchable ? 'HComboboxOption' : 'HListboxOption'"
               v-for="(option, index) in filteredOptions"
@@ -101,7 +98,7 @@
               </li>
             </component>
 
-            <component :is="createOptionVNode" v-if="creatable === true && showCreateOption" as="template" />
+            <component :is="createOptionVNode" v-if="showCreateOption" :value="queryOption" />
             <p v-else-if="searchable && query && !filteredOptions.length" :class="uiMenu.option.empty">
               <slot name="option-empty" :query="query">
                 No results for "{{ query }}".
@@ -406,6 +403,22 @@ export default defineComponent({
 
     const debouncedSearch = typeof props.searchable === 'function' ? useDebounceFn(props.searchable, props.debounce) : undefined
 
+    const matchOption = (searchFrom: any, searchFor: string, method: 'search' | 'exact') =>
+      method === 'exact' ? String(searchFrom) === searchFor : String(searchFrom).search(new RegExp(searchFor, 'i')) !== -1
+
+    const _search = (searchFrom: any[], searchFor: string, method: 'search' | 'exact' = 'search') =>
+      (searchFrom).filter((option: any) => {
+        return (props.searchAttributes?.length ? props.searchAttributes : [props.optionAttribute]).some((searchAttribute: any) => {
+          if (['string', 'number'].includes(typeof option)) {
+            return matchOption(option, searchFor, method)
+          }
+
+          const child = get(option, searchAttribute)
+
+          return child !== null && child !== undefined && matchOption(child, searchFor, method)
+        })
+      })
+
     const filteredOptions = computedAsync(async () => {
       if (props.searchable && debouncedSearch) {
         return await debouncedSearch(query.value)
@@ -415,17 +428,7 @@ export default defineComponent({
         return props.options
       }
 
-      return (props.options as any[]).filter((option: any) => {
-        return (props.searchAttributes?.length ? props.searchAttributes : [props.optionAttribute]).some((searchAttribute: any) => {
-          if (['string', 'number'].includes(typeof option)) {
-            return String(option).search(new RegExp(query.value, 'i')) !== -1
-          }
-
-          const child = get(option, searchAttribute)
-
-          return child !== null && child !== undefined && String(child).search(new RegExp(query.value, 'i')) !== -1
-        })
-      })
+      return _search(props.options, query.value)
     })
 
     const queryOption = computed(() => {
@@ -438,28 +441,13 @@ export default defineComponent({
       && (
         props.creatable !== 'always'
           ? !filteredOptions.value.length
-          : (
-            filteredOptions.value.length === 1
-              ? query.value !== (['string', 'number'].includes(typeof filteredOptions.value[0])
-                ? String(filteredOptions.value[0])
-                : (
-                  props.searchAttributes?.length
-                    ? props.searchAttributes
-                    : [props.optionAttribute]
-                ).some((searchAttribute: any) => {
-                  const child = get(filteredOptions.value[0], searchAttribute)
-
-                  return child !== null && child !== undefined && String(child) !== query.value
-                })
-              )
-              : true
-          )
+          : !_search(filteredOptions.value, query.value, 'exact').length
       )
     )
 
     const createOptionVNode = h(
-      props.searchable ? 'HComboboxOption' : 'HListboxOption',
-      { value: queryOption },
+      props.searchable ? HComboboxOption : HListboxOption,
+      { as: 'template' },
       {
         default: ({ active, selected }) => h(
           'li',
@@ -478,7 +466,7 @@ export default defineComponent({
             { class: uiMenu.value.option.container },
             slots['option-create']
               ? slots['option-create']({ option: queryOption.value, active, selected })
-              : h('span', { class: uiMenu.value.option.create }, [`Create ${queryOption.value[props.optionAttribute]}`])
+              : h('span', { class: uiMenu.value.option.create }, [`Create "${queryOption.value[props.optionAttribute]}"`])
           )
         )
       }
