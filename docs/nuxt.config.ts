9d8f3581396adccdf5b0f829274d95c80e591ee3
Benjamin Canac <canacb1@gmail.com>
2023-12-21 11:18:14 +0100

    docs: pull `nuxt/ui-pro` docs from `main` branch

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -61,7 +61,7 @@ export default defineNuxtConfig({
         prefix: '/pro',
         driver: 'github',
         repo: 'nuxt/ui-pro',
-        branch: 'dev',
+        branch: 'main',
         dir: '.docs/content/pro',
         token: process.env.NUXT_GITHUB_TOKEN || ''
       } : undefined
