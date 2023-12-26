29e64ca963eeed1e82640957860f43391d8683ed
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-26 07:00:33 -0500

    feat(VerticalNavigation): improve accessibility (#948)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

--- src/runtime/components/navigation/VerticalNavigation.vue
+++ src/runtime/components/navigation/VerticalNavigation.vue
@@ -1,39 +1,46 @@
 <template>
   <nav :class="ui.wrapper" v-bind="attrs">
-    <ULink
-      v-for="(link, index) of links"
-      v-slot="{ isActive }"
-      :key="index"
-      v-bind="omit(link, ['label', 'labelClass', 'icon', 'iconClass', 'avatar', 'badge', 'click'])"
-      :class="[ui.base, ui.padding, ui.width, ui.ring, ui.rounded, ui.font, ui.size]"
-      :active-class="ui.active"
-      :inactive-class="ui.inactive"
-      @click="link.click"
-      @keyup.enter="$event.target.blur()"
-    >
-      <slot name="avatar" :link="link" :is-active="isActive">
-        <UAvatar
-          v-if="link.avatar"
-          v-bind="{ size: ui.avatar.size, ...link.avatar }"
-          :class="[ui.avatar.base]"
-        />
-      </slot>
-      <slot name="icon" :link="link" :is-active="isActive">
-        <UIcon
-          v-if="link.icon"
-          :name="link.icon"
-          :class="twMerge(twJoin(ui.icon.base, isActive ? ui.icon.active : ui.icon.inactive), link.iconClass)"
-        />
-      </slot>
-      <slot :link="link" :is-active="isActive">
-        <span v-if="link.label" :class="twMerge(ui.label, link.labelClass)">{{ link.label }}</span>
-      </slot>
-      <slot name="badge" :link="link" :is-active="isActive">
-        <span v-if="link.badge" :class="[ui.badge.base, isActive ? ui.badge.active : ui.badge.inactive]">
-          {{ link.badge }}
-        </span>
-      </slot>
-    </ULink>
+    <ul>
+      <li v-for="(link, index) of links" :key="index">
+        <ULink
+          v-slot="{ isActive }"
+          v-bind="omit(link, ['label', 'labelClass', 'icon', 'iconClass', 'avatar', 'badge', 'click'])"
+          :class="[ui.base, ui.padding, ui.width, ui.ring, ui.rounded, ui.font, ui.size]"
+          :active-class="ui.active"
+          :inactive-class="ui.inactive"
+          @click="link.click"
+          @keyup.enter="$event.target.blur()"
+        >
+          <slot name="avatar" :link="link" :is-active="isActive">
+            <UAvatar
+              v-if="link.avatar"
+              v-bind="{ size: ui.avatar.size, ...link.avatar }"
+              :class="[ui.avatar.base]"
+            />
+          </slot>
+          <slot name="icon" :link="link" :is-active="isActive">
+            <UIcon
+              v-if="link.icon"
+              :name="link.icon"
+              :class="twMerge(twJoin(ui.icon.base, isActive ? ui.icon.active : ui.icon.inactive), link.iconClass)"
+            />
+          </slot>
+          <slot :link="link" :is-active="isActive">
+            <span v-if="link.label" :class="twMerge(ui.label, link.labelClass)">
+              <span v-if="isActive" class="sr-only">
+                Current page:
+              </span>
+              {{ link.label }}
+            </span>
+          </slot>
+          <slot name="badge" :link="link" :is-active="isActive">
+            <span v-if="link.badge" :class="[ui.badge.base, isActive ? ui.badge.active : ui.badge.inactive]">
+              {{ link.badge }}
+            </span>
+          </slot>
+        </ULink>
+      </li>
+    </ul>
   </nav>
 </template>
 
