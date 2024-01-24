2cb41db111da79f4f3f8ccd825071820050ba885
Benjamin Canac <canacb1@gmail.com>
2024-01-24 11:55:05 +0100

    fix(Select): consistent placeholder with input and textarea
    Resolves #1276

--- src/runtime/components/forms/SelectMenu.vue
+++ src/runtime/components/forms/SelectMenu.vue
@@ -384,7 +384,7 @@ export default defineComponent({
         variant?.replaceAll('{color}', color.value),
         (isLeading.value || slots.leading) && ui.value.leading.padding[size.value],
         (isTrailing.value || slots.trailing) && ui.value.trailing.padding[size.value]
-      ), props.selectClass)
+      ), props.placeholder && !props.modelValue && ui.value.placeholder, props.selectClass)
     })
 
     const isLeading = computed(() => {
