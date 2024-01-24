fc91e90958f4a26658e1795bc811704aea0c278f
Benjamin Canac <canacb1@gmail.com>
2024-01-24 17:04:18 +0100

    chore(VerticalNavigation): harmonize config with buttons

--- src/runtime/ui.config/navigation/verticalNavigation.ts
+++ src/runtime/ui.config/navigation/verticalNavigation.ts
@@ -1,8 +1,8 @@
 export default {
   wrapper: 'relative',
-  base: 'group relative flex items-center gap-2 focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:before:ring-inset focus-visible:before:ring-1 focus-visible:before:ring-primary-500 dark:focus-visible:before:ring-primary-400 before:absolute before:inset-px before:rounded-md disabled:cursor-not-allowed disabled:opacity-75',
+  base: 'group relative flex items-center gap-1.5 focus:outline-none focus-visible:outline-none dark:focus-visible:outline-none focus-visible:before:ring-inset focus-visible:before:ring-1 focus-visible:before:ring-primary-500 dark:focus-visible:before:ring-primary-400 before:absolute before:inset-px before:rounded-md disabled:cursor-not-allowed disabled:opacity-75',
   ring: 'focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400',
-  padding: 'px-3 py-1.5',
+  padding: 'px-2.5 py-1.5',
   width: 'w-full',
   rounded: 'rounded-md',
   font: 'font-medium',
@@ -11,13 +11,13 @@ export default {
   inactive: 'text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white hover:before:bg-gray-50 dark:hover:before:bg-gray-800/50',
   label: 'truncate relative',
   icon: {
-    base: 'flex-shrink-0 w-4 h-4',
+    base: 'flex-shrink-0 w-5 h-5',
     active: 'text-gray-700 dark:text-gray-200',
     inactive: 'text-gray-400 dark:text-gray-500 group-hover:text-gray-700 dark:group-hover:text-gray-200'
   },
   avatar: {
     base: 'flex-shrink-0',
-    size: '3xs' as const
+    size: '2xs' as const
   },
   badge: {
     base: 'flex-shrink-0 ml-auto relative rounded',
