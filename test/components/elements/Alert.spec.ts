217df53ac76773641a9a9724281bcf060ea0dd92
Florent Delerue <florentdelerue@hotmail.com>
2024-02-13 16:21:39 +0100

    feat: update alert test

--- test/components/elements/Alert.spec.ts
+++ test/components/elements/Alert.spec.ts
@@ -6,14 +6,16 @@ import ComponentRender from '../component-render'
 describe('Alert', () => {
   it.each([
     ['basic case', {}],
-    ['renders with default values correctly', { props: { title: 'Alert Title' } }],
-    ['renders with description', { props: { title: 'Alert Title', description: 'This is an alert description.' } }],
-    // ['renders with icon', { props: { title: 'Alert Title', icon: 'heroicons-information-circle' } }],
-    // ['renders with avatar', { props: { title: 'Alert Title', avatar: { src: 'avatar.jpg', alt: 'User Avatar' } } }],
-    // ['renders with close button', { props: { title: 'Alert Title', closeButton: { label: 'Close' } } }],
-    //['renders with actions', { props: { title: 'Alert Title', actions: [() => { }] } }],
-    ['renders with custom color', { props: { title: 'Alert Title', color: 'red' } }],
-    ['renders with custom variant', { props: { title: 'Alert Title', variant: 'outline' } }],
+    ['with tile', { props: { title: 'Alert Title' } }],
+    ['with title slot', { slots: { title: 'Alert Title slot' } }],
+    ['with description', { props: { title: 'Alert Title', description: 'This is an alert description.' } }],
+    ['with description slot', { slots: { title: 'Alert Title', description: 'This is an alert description slot' } }],
+    ['with icon', { props: { title: 'Alert Title', icon: 'heroicons-information-circle', ui: { icon: { base: 'w-5 h-5' } } } }],
+    ['with avatar', { props: { title: 'Alert Title', avatar: { src: 'avatar.jpg', alt: 'User Avatar' }, ui: { avatar: { base: 'flex-shrink-0 self-center' } } } }],
+    ['with close button', { props: { title: 'Alert Title', closeButton: { label: 'Close' }, ui: { default: { closeButton: '' } } } }],
+    ['with actions', { props: { title: 'Alert Title', actions: [() => { }], ui: { default: { actionButton: { size: 'sm', color: 'red', variant: 'outline' } } } } }],
+    ['with custom color', { props: { title: 'Alert Title', color: 'red' } }],
+    ['with custom variant', { props: { title: 'Alert Title', variant: 'outline' } }],
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Alert.props>) => {
     if (options !== undefined) {
