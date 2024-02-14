8ecf8fea2a577611484e64d592e611a73cbc1389
Florent Delerue <florentdelerue@hotmail.com>
2024-02-14 15:29:37 +0100

    feat: improve form test

--- test/components/forms/Form.spec.ts
+++ test/components/forms/Form.spec.ts
@@ -2,21 +2,13 @@ import { describe, it, expect } from 'vitest'
 import { UForm } from '#components'
 import type { TypeOf } from 'zod'
 import ComponentRender from '../component-render'
-import { ZodSchema } from 'zod'
 
 describe('Form', () => {
   it.each([
     ['basic case', {}],
-    ['with custom state', { props: { state: { username: 'john_doe', password: 'password123' } } }],
-    ['with custom validate function', { props: { validate: () => [] } }],
-    ['with custom validateOn array', { props: { validateOn: ['submit'] } }],
-    ['with custom validateOn array including other events', { props: { validateOn: ['blur', 'input', 'change', 'submit'] } }],
+    ['with default slot', { slots: { default: 'Form slot' } }]
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Form.props>) => {
-    if (options !== undefined) {
-      options.slots = options.slots || { default: () => 'label' }
-      options.slots.default = options.slots.default || (() => 'label')
-    }
     const html = await ComponentRender(nameOrHtml, options, UForm)
     expect(html).toMatchSnapshot()
   })
