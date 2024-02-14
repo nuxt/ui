f0ba4a0c7abc034d9f2c14c66666d198096d99b5
Florent Delerue <florentdelerue@hotmail.com>
2024-02-14 17:29:14 +0100

    feat: improve radiogroup test

--- test/components/forms/RadioGroup.spec.ts
+++ test/components/forms/RadioGroup.spec.ts
@@ -6,13 +6,12 @@ import ComponentRender from '../component-render'
 describe('RadioGroup', () => {
   it.each([
     ['basic case', {}],
-    ['with name and legend', { props: { name: 'radioGroupName', legend: 'Radio Group Legend' } }],
-    ['with options and legend', { props: { options: [{ label: 'Option 1', value: 'option1' }, { label: 'Option 2', value: 'option2' }], legend: 'Radio Group Legend' } }],
-    ['with optionAttribute and legend', { props: { optionAttribute: 'name', legend: 'Radio Group Legend' } }],
-    ['with valueAttribute and legend', { props: { valueAttribute: 'id', legend: 'Radio Group Legend' } }],
-    ['with disabled and legend', { props: { disabled: true, legend: 'Radio Group Legend' } }],
-    ['with custom color and legend', { props: { color: 'blue', legend: 'Radio Group Legend' } }],
-    ['with custom class and legend', { props: { class: 'custom-class', legend: 'Radio Group Legend' } }],
+    ['with legend', { props: { legend: 'Radio Group Legend' } }],
+    ['with slot legend', { slots: { legend: 'Radio Group Slot Legend' } }],
+    ['with options and legend', { props: { options: [{ label: 'Option 1', value: 'option1' }, { label: 'Option 2', value: 'option2' }] } }],
+    ['with disabled', { props: { disabled: true, options: [{ label: 'Option 1', value: 'option1' }, { label: 'Option 2', value: 'option2' }] } }],
+    ['with custom color', { props: { color: 'red', options: [{ label: 'Option 1', value: 'option1' }, { label: 'Option 2', value: 'option2' }] } }],
+    ['with label', { props: { label: 'label' } }],
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof RadioGroup.props>) => {
     if (options !== undefined) {
