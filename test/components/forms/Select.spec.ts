eff419c5b41c802e62c6699fa5a98d5eed47d624
Florent Delerue <florentdelerue@hotmail.com>
2024-02-14 18:05:01 +0100

    feat: improve select test

--- test/components/forms/Select.spec.ts
+++ test/components/forms/Select.spec.ts
@@ -6,22 +6,27 @@ import ComponentRender from '../component-render'
 describe('Select', () => {
   it.each([
     ['basic case', {}],
-    ['with custom id', { props: { id: 'mySelect' }, options: {} }],
-    ['with custom name', { props: { name: 'mySelectInput' }, options: {} }],
-    ['with placeholder', { props: { placeholder: 'Select an option' }, options: {} }],
-    ['with disabled', { props: { disabled: true }, options: {} }],
-    ['with icon', { props: { icon: 'i-heroicons-academic-cap' }, options: {} }],
-    ['with loading icon', { props: { loadingIcon: 'i-heroicons-adjustments-horizontal' }, options: {} }],
-    ['with leading icon', { props: { leadingIcon: 'i-heroicons-adjustments-vertical' }, options: {} }],
-    ['with trailing icon', { props: { trailingIcon: 'i-heroicons-archive-box' }, options: {} }],
-    ['with trailing', { props: { trailing: true }, options: {} }],
-    ['with leading', { props: { leading: true }, options: {} }],
-    ['with loading', { props: { loading: true }, options: {} }],
-    ['with padded', { props: { padded: false }, options: {} }],
-    ['with custom size', { props: { size: 'lg' }, options: {} }],
-    ['with custom color', { props: { color: 'blue' }, options: {} }],
-    ['with custom variant', { props: { variant: 'outlined' }, options: {} }],
-    ['with custom options', { props: { options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }, options: {} }]
+    ['with custom id', { props: { id: 'mySelect' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with custom name', { props: { name: 'mySelectInput' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with placeholder', { props: { placeholder: 'Select an option' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with disabled', { props: { disabled: true }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with icon', { props: { icon: 'i-heroicons-academic-cap' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with loading', { props: { loading: true }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with loadingIcon', { props: { loading: true, loadingIcon: 'i-heroicons-adjustments-horizontal' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with leadingIcon', { props: { leadingIcon: 'i-heroicons-adjustments-vertical' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with trailing icon', { props: { trailingIcon: 'i-heroicons-archive-box' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with trailing', { props: { trailing: true, icon: '' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with leading + icon', { props: { leading: true, icon: 'i-heroicons-adjustments-vertical', options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with trailing + icon', { props: { trailing: true, icon: 'i-heroicons-archive-box', options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with leading slot', { slots: { leading: 'leading slot' } ,props: { options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with leading slot + iconName', { slots: { leading: 'leading slot' } ,props: { iconName: 'i-heroicons-cloud', options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with training slot', { slots: { leading: 'trailing slot' } ,props: { options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with training slot + iconName', { slots: { leading: 'trailing slot' } ,props: { iconName: 'i-heroicons-bookmark-solid', options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with padded', { props: { padded: false, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with custom size', { props: { size: 'lg' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with custom color', { props: { color: 'red', options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }],
+    ['with custom variant', { props: { variant: 'outlined' }, options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] }],
+    ['with custom options', { props: { options: [{ value: 'opt1', label: 'Option 1' }, { value: 'opt2', label: 'Option 2' }] } }]
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Select.props>) => {
     if (options !== undefined) {
