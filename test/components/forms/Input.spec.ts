c358aa06c6ef3def673824058821db8d8c4e7f24
Florent Delerue <florentdelerue@hotmail.com>
2024-02-14 16:09:15 +0100

    feat: improve input test

--- test/components/forms/Input.spec.ts
+++ test/components/forms/Input.spec.ts
@@ -6,21 +6,29 @@ import ComponentRender from '../component-render'
 describe('Input', () => {
   it.each([
     ['basic case', {}],
-    ['with custom name and placeholder', { props: { name: 'username', placeholder: 'Enter your username' } }],
-    ['with value', { props: { value: 'JohnDoe' } }],
+    ['with name', { props: { name: 'username' } }],
+    ['with placeholder', { props: { placeholder: 'Enter your username' } }],
     ['with disabled', { props: { disabled: true } }],
     ['with readonly', { props: { readonly: true } }],
-    ['with autofocus', { props: { autofocus: true } }],
     ['with required', { props: { required: true } }],
-    ['with custom class', { props: { class: 'custom-class' } }],
-    ['with custom style', { props: { style: { color: 'red', fontSize: '16px' } } }],
-    ['with custom type', { props: { type: 'password' } }]
+    ['with custom type', { props: { type: 'password' } }],
+    ['with icon', { props: { icon: 'i-heroicons-battery-50-solid' } }],
+    ['with leading and icon', { props: { leading: true, icon: 'i-heroicons-battery-50-solid' } }],
+    ['with leadingIcon', { props: { leadingIcon: 'i-heroicons-battery-50-solid' } }],
+    ['with loading icon', { props: { loading: true } }],
+    ['with leading slots', { slots: { leading: 'leading slot' } }],
+    ['with trailing and icon', { props: { trailing: true, icon: 'i-heroicons-battery-50-solid' } }],
+    ['with trailingIcon', { props: { trailingIcon: 'i-heroicons-battery-50-solid' } }],
+    ['with trailing slots', { slots: { leading: 'trailing slot' } }],
+    ['without padded', { props: { padded: false } }],
+    ['with size', { props: { size: 'xs' } }],
+    ['with color', { props: { color: 'red' } }],
+    ['with variant', { props: { variant: 'outline' } }],
+    ['with input class', { props: { inputClass: 'w-full' } }],
+
+
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Input.props>) => {
-    if (options !== undefined) {
-      options.slots = options.slots || { default: () => 'label' }
-      options.slots.default = options.slots.default || (() => 'label')
-    }
     const html = await ComponentRender(nameOrHtml, options, UInput)
     expect(html).toMatchSnapshot()
   })
