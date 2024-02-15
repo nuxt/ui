4e87aada1e5b58e24d48c9bf9039c5c2b6e06f0f
Florent Delerue <florentdelerue@hotmail.com>
2024-02-15 11:28:24 +0100

    feat: improve container test

--- test/components/layout/Container.spec.ts
+++ test/components/layout/Container.spec.ts
@@ -6,13 +6,10 @@ import ComponentRender from '../component-render'
 describe('Container', () => {
   it.each([
     ['basic case', {}],
-    ['with as', { as: 'article' }],
+    ['with as', { props: { as: 'article' } }],
+    ['with default slot', { slots: { default: 'default slot' } }],
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Container.props>) => {
-    if (options !== undefined) {
-      options.slots = options.slots || { default: () => 'label' }
-      options.slots.default = options.slots.default || (() => 'label')
-    }
     const html = await ComponentRender(nameOrHtml, options, UContainer)
     expect(html).toMatchSnapshot()
   })
