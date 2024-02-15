fad3724845a2b5138c24a396951c7055ad605c64
Florent Delerue <florentdelerue@hotmail.com>
2024-02-15 11:46:25 +0100

    feat: improve divider test

--- test/components/layout/Divider.spec.ts
+++ test/components/layout/Divider.spec.ts
@@ -5,13 +5,15 @@ import ComponentRender from '../component-render'
 
 describe('Divider', () => {
   it.each([
-    ['basic case', {}]
+    ['basic case', {}],
+    ['with label', { props: { label: 'Divider label' } }],
+    ['with icon', { props: { icon: 'i-heroicons-academic-cap', ui: { icon: { base: 'flex-shrink-0 w-5 h-5' } } } }],
+    ['with avatar', { props: { avatar: { src: 'https://repository-images.githubusercontent.com/428329515/43fec891-9030-4601-8233-5d45ba5c6013', alt: 'nuxt-ui' }, ui: { avatar: { size: 'md' }} } }],
+    ['with orientation', { props: { orientation: 'vertical' } }],
+    ['with type', { props: { type: 'dotted' } }],
+    ['with default slot', { slots: { default: 'Default slot' } }],
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Divider.props>) => {
-    if (options !== undefined) {
-      options.slots = options.slots || { default: () => 'label' }
-      options.slots.default = options.slots.default || (() => 'label')
-    }
     const html = await ComponentRender(nameOrHtml, options, UDivider)
     expect(html).toMatchSnapshot()
   })
