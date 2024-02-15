9d239bceb42fe7618f34ef38fbf9b6baff7b11eb
Florent Delerue <florentdelerue@hotmail.com>
2024-02-15 12:01:23 +0100

    feat: improve breadcrumb test

--- test/components/navigation/Breadcrumb.spec.ts
+++ test/components/navigation/Breadcrumb.spec.ts
@@ -6,8 +6,12 @@ import ComponentRender from '../component-render'
 describe('Breadcrumb', () => {
   it.each([
     ['basic case', {}],
-    ['with links', { links: [{ label: 'Home', to: '/' }, { label: 'About', to: '/about' }] }],
-    ['with divider', { divider: '>' }],
+    ['with links', { props: { links: [{ label: 'Home', to: '/' }, { label: 'About', to: '/about' }], ui: { divider: { base: 'flex-shrink-0 w-5 h-5' }} } }],
+    ['with divider', { props: { divider: '|', links: [{ label: 'Home', to: '/' }, { label: 'About', to: '/about' }], ui: { divider: { base: 'flex-shrink-0 w-5 h-5' } } } }],
+    ['with icon', { props: { links: [{ label: 'Home', to: '/', icon: 'i-heroicons-academic-cap' }, { label: 'About', to: '/about', icon: 'i-heroicons-code-bracket-square' }], ui: { divider: { base: 'flex-shrink-0 w-5 h-5' }, icon: { base: 'flex-shrink-0 w-5 h-5' } } } }],
+    ['with icon slot', { slots: { icon: 'icon slot' }, props: { links: [{ label: 'Home', to: '/', icon: 'i-heroicons-academic-cap' }, { label: 'About', to: '/about', icon: 'i-heroicons-code-bracket-square' }], ui: { divider: { base: 'flex-shrink-0 w-5 h-5' } } } }],
+    ['with divider slot', { slots: { divider: 'divider slot' }, props: { links: [{ label: 'Home', to: '/' }, { label: 'About', to: '/about' }], ui: { divider: { base: 'flex-shrink-0 w-5 h-5' } } } }],
+    ['with default slot', { slots: { default: 'default slot' }, props: { links: [{ label: 'Home', to: '/' }, { label: 'About', to: '/about' }], ui: { divider: { base: 'flex-shrink-0 w-5 h-5' } } } }],
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Breadcrumb.props>) => {
     if (options !== undefined) {
