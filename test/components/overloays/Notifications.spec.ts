c764e6b22ae2cb12a8507b2cd160484a31fca88c
Florent Delerue <florentdelerue@hotmail.com>
2024-02-15 15:31:25 +0100

    feat: improve notifications test

--- test/components/overloays/Notifications.spec.ts
+++ test/components/overloays/Notifications.spec.ts
@@ -1,18 +1,19 @@
 import { describe, it, expect } from 'vitest'
-import { UNotification } from '#components'
+import { UNotifications } from '#components'
 import type { TypeOf } from 'zod'
 import ComponentRender from '../component-render'
+import ui from '../../../src/runtime/ui.config/overlays/notifications'
 
-describe('Notification', () => {
+describe('Notifications', () => {
   it.each([
-    ['basic case', {}],
+    ['basic case', { }],
     // // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Notification.props>) => {
     if (options !== undefined) {
       options.slots = options.slots || { default: () => 'label' }
       options.slots.default = options.slots.default || (() => 'label')
     }
-    const html = await ComponentRender(nameOrHtml, options, UNotification)
+    const html = await ComponentRender(nameOrHtml, options, UNotifications)
     expect(html).toMatchSnapshot()
   })
 })
