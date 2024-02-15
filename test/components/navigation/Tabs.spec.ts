c91b9b51c595d724fc50730883da9b2598a8a794
Florent Delerue <florentdelerue@hotmail.com>
2024-02-15 15:05:46 +0100

    feat: tabs test

--- test/components/navigation/Tabs.spec.ts
+++ test/components/navigation/Tabs.spec.ts
@@ -2,20 +2,16 @@ import { describe, it, expect } from 'vitest'
 import { UTabs } from '#components'
 import type { TypeOf } from 'zod'
 import ComponentRender from '../component-render'
+import ui from '../../../src/runtime/ui.config/navigation/tabs'
 
 describe('Tabs', () => {
   it.each([
-    ['basic case', {}],
-    // ['with orientation', { orientation: 'vertical' }],
-    // ['with defaultIndex', { defaultIndex: 2 }],
-    // ['with items', { items: [{ label: 'Item 1' }, { label: 'Item 2' }] }],
-    // ['with unmount', { unmount: true }]
+    ['basic case', { props: { ui: { list: { marker: { base: ui.list.marker.base } } } } }],
+    ['with orientation', { props: { orientation: 'vertical', ui: { list: { marker: { base: ui.list.marker.base } } } } }],
+    ['with items', { props: { items: [{ label: 'Item 1' }, { label: 'Item 2' }], ui: { list: { tab: { base: ui.list.tab.base }, marker: { base: ui.list.marker.base } } } } }],
+    ['with tabs slot', { slots: { default: 'tab slot' }, props: { items: [{ label: 'Item 1' }, { label: 'Item 2' }], ui: { list: { tab: { base: ui.list.tab.base }, marker: { base: ui.list.marker.base } } } } }],
     // @ts-ignore
   ])('renders %s correctly', async (nameOrHtml: string, options: TypeOf<typeof Tabs.props>) => {
-    if (options !== undefined) {
-      options.slots = options.slots || { default: () => 'label' }
-      options.slots.default = options.slots.default || (() => 'label')
-    }
     const html = await ComponentRender(nameOrHtml, options, UTabs)
     expect(html).toMatchSnapshot()
   })
