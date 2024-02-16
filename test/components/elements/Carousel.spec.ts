5915fa8e350796ffecddcfdfffc2fca9840c7f19
Florent Delerue <florentdelerue@hotmail.com>
2024-02-16 15:44:44 +0100

    feat: up carousel test and  snap

--- test/components/elements/Carousel.spec.ts
+++ test/components/elements/Carousel.spec.ts
@@ -10,7 +10,7 @@ describe('Carousel', () => {
     ['with arrows and indicators', { props: { arrows: true, indicators: true, ui: uiCarousel } }],
     ['with arrows only', { props: { arrows: true, indicators: false, ui: uiCarousel } }],
     ['with indicators only', { props: { arrows: false, indicators: true, ui: uiCarousel } }],
-    ['with custom previous button', { props: { arrows: true, prevButton: uiCarousel } }],
+    ['with custom previous button', { props: { arrows: true, ui: uiCarousel } }],
     ['with custom next button', { props: { nextButton: { label: 'Custom next button', arrows: true }, ui: uiCarousel } }],
     ['with previous slot', { slots: { prev: 'prev slot' }, props: { arrows: true, ui: uiCarousel } }],
     ['with next slot', { slots: { next: 'next slot' }, props: { arrows: true, ui: uiCarousel } }]
