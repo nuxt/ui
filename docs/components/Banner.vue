18449ac3869f872286033cd89cf5faa521f90811
Benjamin Canac <canacb1@gmail.com>
2024-02-21 12:07:49 +0100

    up

--- docs/components/Banner.vue
+++ docs/components/Banner.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import { isAfter } from 'date-fns'
+
 const id = 'nuxt-ui-banner-1'
 const to = '/pro/pricing'
+const date = new Date('2024-02-25T20:00:00Z')
 
-const timeAgo = useTimeAgo(new Date('2024-02-25T22:00:00'))
+const timeAgo = useTimeAgo(date)
 
 const hideBanner = () => {
   localStorage.setItem(id, 'true')
@@ -22,30 +25,44 @@ if (process.server) {
     }]
   })
 }
+
+onMounted(() => {
+  if (isAfter(new Date(), date)) {
+    hideBanner()
+    return
+  }
+
+  const interval = setInterval(() => {
+    if (isAfter(new Date(), date)) {
+      hideBanner()
+      clearInterval(interval)
+    }
+  }, 1000)
+})
 </script>
 
 <template>
-  <div class="relative bg-background/75 backdrop-blur z-50 border-b border-gray-200 dark:border-gray-800 app-banner">
-    <UContainer>
+  <div class="relative bg-primary hover:bg-primary/90 transition-[background] backdrop-blur z-50 app-banner">
+    <UContainer class="py-2">
       <NuxtLink v-if="to" :to="to" class="focus:outline-none" tabindex="-1">
         <span class="absolute inset-0 " aria-hidden="true" />
       </NuxtLink>
 
-      <div class="flex items-center justify-between h-12">
-        <div class="lg:flex-1 flex items-center" />
+      <div class="flex items-center justify-between gap-2">
+        <div class="lg:flex-1 hidden lg:flex items-center" />
 
-        <p class="text-sm font-medium flex items-center gap-1 text-gray-900 dark:text-white">
-          <UIcon name="i-heroicons-gift" class=" w-5 h-5" />
-          <span class="font-semibold">Nuxt UI Pro</span> <UBadge label="v1.0" size="xs" class="rounded" color="white" /> is out with Dashboard components! Discounted prices end {{ timeAgo }}.
+        <p class="text-sm font-medium text-white dark:text-gray-900">
+          <UIcon name="i-heroicons-gift" class="w-5 h-5 align-top flex-shrink-0 pointer-events-none mr-2" />
+          <span class="font-semibold">Nuxt UI Pro v1.0</span> is out with dashboard components! Discounted prices <span class="font-semibold">end {{ timeAgo }}</span>.
         </p>
 
         <div class="flex items-center justify-end lg:flex-1">
-          <UButton
-            color="gray"
-            variant="ghost"
-            icon="i-heroicons-x-mark-20-solid"
+          <button
+            class="p-1.5 rounded-md inline-flex hover:bg-primary/90"
             @click.prevent="hideBanner"
-          />
+          >
+            <UIcon name="i-heroicons-x-mark-20-solid" class="w-5 h-5 text-white dark:text-gray-900" />
+          </button>
         </div>
       </div>
     </UContainer>
