806b89e8438f3173a4bfee7787db23e48065c269
Alexander Thorwaldson <alex@zoobz.io>
2024-02-22 12:57:10 -0800

    docs(UAlert): document new `UAlert` slots

---
+++ docs/components/content/examples/AlertExampleAvatar.vue
@@ -0,0 +1,21 @@
+<template>
+  <UAlert 
+    title="Customize Alert Avatar" 
+    description="Insert custom content into the avatar slot!" 
+    :avatar="{ 
+      src: 'https://avatars.githubusercontent.com/u/739984?v=4', 
+      alt: 'Avatar' 
+    }"
+  >
+    <template #avatar="{ avatar, ui }">
+      <UAvatar 
+        v-bind="{ size: ui.size, ...avatar }" 
+        :class="ui.base" 
+        chip-color="primary"
+        chip-text=""
+        chip-position="top-right"
+      />
+    </template>
+  </UAlert>
+</template>
+  
---
+++ docs/components/content/examples/AlertExampleIcon.vue
@@ -0,0 +1,10 @@
+<template>
+  <UAlert title="Customize Alert Icon" description="Insert custom content into the icon slot!" icon="i-heroicons-command-line">
+    <template #icon="{ icon, ui }">
+      <UBadge title="3" size="sm">
+        <UIcon :name="icon" :class="ui.base" />
+      </UBadge>
+    </template>
+  </UAlert>
+</template>
+  
