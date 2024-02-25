df1ce0d33b50f3f7babe2273ca0883f0665438c6
Benjamin Canac <canacb1@gmail.com>
2024-02-25 14:52:34 +0100

    docs: update banner

--- docs/components/Banner.vue
+++ docs/components/Banner.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
-import { isAfter } from 'date-fns'
-
 const id = 'nuxt-ui-banner-1'
 const to = '/pro/pricing'
-const date = new Date('2024-02-25T20:00:00Z')
-
-const timeAgo = useTimeAgo(date)
 
 const hideBanner = () => {
   localStorage.setItem(id, 'true')
@@ -25,20 +20,6 @@ if (process.server) {
     }]
   })
 }
-
-onMounted(() => {
-  if (isAfter(new Date(), date)) {
-    hideBanner()
-    return
-  }
-
-  const interval = setInterval(() => {
-    if (isAfter(new Date(), date)) {
-      hideBanner()
-      clearInterval(interval)
-    }
-  }, 1000)
-})
 </script>
 
 <template>
@@ -52,8 +33,8 @@ onMounted(() => {
         <div class="lg:flex-1 hidden lg:flex items-center" />
 
         <p class="text-sm font-medium text-white dark:text-gray-900">
-          <UIcon name="i-heroicons-gift" class="w-5 h-5 align-top flex-shrink-0 pointer-events-none mr-2" />
-          <span class="font-semibold">Nuxt UI Pro v1.0</span> is out with dashboard components! Discount ends <span class="font-semibold">{{ timeAgo }}</span>.
+          <UIcon name="i-heroicons-rocket-launch" class="w-5 h-5 align-top flex-shrink-0 pointer-events-none mr-2" />
+          <span class="font-semibold">Nuxt UI Pro v1.0</span> is out with dashboard components!
         </p>
 
         <div class="flex items-center justify-end lg:flex-1">
