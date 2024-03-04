40b914cfeb822e861d4e56a6e560e2c1f8b7f513
Eugen Istoc <eugenistoc@gmail.com>
2024-03-04 11:15:53 -0500

    fix: slideover component layout

--- docs/components/content/examples/SlideoverExampleComponent.vue
+++ docs/components/content/examples/SlideoverExampleComponent.vue
@@ -10,16 +10,18 @@ const emits = defineEmits<{
 </script>
 
 <template>
-  <UCard class="flex flex-col flex-1" :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
-    <template #header>
-      <div class="flex items-center justify-between">
-        <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-          This slideover was opened programmatically: {{ props.count }} times
-        </h3>
-        <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="emits('close')" />
-      </div>
-    </template>
-
-    <Placeholder class="h-full" />
-  </UCard>
+  <USlideover>
+    <UCard class="flex flex-col flex-1" :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+      <template #header>
+        <div class="flex items-center justify-between">
+          <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+            Opened programmatically: {{ props.count }} times
+          </h3>
+          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="emits('close')" />
+        </div>
+      </template>
+  
+      <Placeholder class="h-full" />
+    </UCard>
+  </USlideover>
 </template>
