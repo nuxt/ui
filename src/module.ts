0d1f8528c8e1ef0400098493d7fef86cb048473c
Benjamin Canac <canacb1@gmail.com>
2024-03-06 20:08:58 +0100

    chore(module): dynamic colors

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,5 @@
 import { defu } from 'defu'
-import { createResolver, defineNuxtModule, addComponentsDir, addImportsDir, addVitePlugin, installModule } from '@nuxt/kit'
+import { createResolver, defineNuxtModule, addComponentsDir, addImportsDir, addVitePlugin, addPlugin, addTemplate, installModule } from '@nuxt/kit'
 import tailwindcss from '@tailwindcss/vite'
 import type { DeepPartial } from './runtime/types'
 import * as theme from './runtime/theme'
@@ -34,19 +34,42 @@ export default defineNuxtModule({
 
     nuxt.options.alias['#ui'] = resolver.resolve('./runtime')
 
-    nuxt.options.css.push(resolver.resolve('./runtime/main.css'))
-
     nuxt.options.appConfig.ui = defu(nuxt.options.appConfig.ui || {}, {
       primary: 'green',
       gray: 'cool',
       icons: {
-        loading: 'i-heroicons-arrow-path-20-solid'
+        search: 'i-heroicons-magnifying-glass-20-solid',
+        close: 'i-heroicons-x-mark-20-solid',
+        check: 'i-heroicons-check-20-solid',
+        loading: 'i-heroicons-arrow-path-20-solid',
+        empty: 'i-heroicons-circle-stack-20-solid'
       }
     })
 
     await installModule('nuxt-icon')
 
     addVitePlugin(tailwindcss)
+
+    addPlugin({
+      src: resolver.resolve('./runtime/plugins/index')
+    })
+
+    const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
+
+    const template = addTemplate({
+      filename: 'main.css',
+      write: true,
+      getContents: () => `
+      @import "tailwindcss";
+
+      @theme {
+        ${shades.map(shade => `--color-primary-${shade}: var(--color-primary-${shade});`).join('\n')}
+        ${shades.map(shade => `--color-gray-${shade}: var(--color-gray-${shade});`).join('\n')}
+      }
+      `
+    })
+
+    nuxt.options.css.push(template.dst)
 
     addComponentsDir({
       path: resolver.resolve('./runtime/components'),
