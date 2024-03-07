46e27304e81a029f750ad12ec97c2c9bb27af62c
Benjamin Canac <canacb1@gmail.com>
2024-03-07 16:57:09 +0100

    playground: clean config

--- playground/app.config.ts
+++ playground/app.config.ts
@@ -2,12 +2,7 @@ export default defineAppConfig({
   ui: {
     primary: 'sky',
     button: {
-      base: 'font-semibold',
-      variants: {
-        color: {
-          toto: 'bg-pink-500 text-white'
-        }
-      }
+      base: 'font-semibold'
     },
     container: {
       base: 'max-w-5xl'
