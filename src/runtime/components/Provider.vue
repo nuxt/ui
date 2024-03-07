9711b4db5e44fafdfe9094bf3368cd22bc232d9f
Benjamin Canac <canacb1@gmail.com>
2024-03-07 12:56:09 +0100

    chore(Provider): add

---
+++ src/runtime/components/Provider.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ConfigProvider, ToastProvider, TooltipProvider } from 'radix-vue'
+import { useId } from '#imports'
+
+const useIdFunction = () => useId()
+</script>
+
+<template>
+  <ConfigProvider :use-id="useIdFunction">
+    <TooltipProvider>
+      <ToastProvider>
+        <slot />
+      </ToastProvider>
+    </TooltipProvider>
+  </ConfigProvider>
+</template>
