4f344437e2e44cfb44fc69c1da10f357b8b18121
Benjamin Canac <canacb1@gmail.com>
2024-03-11 15:03:44 +0100

    chore(icons): sort

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -1,7 +1,7 @@
 export default {
-  search: 'i-heroicons-magnifying-glass-20-solid',
-  close: 'i-heroicons-x-mark-20-solid',
   check: 'i-heroicons-check-20-solid',
+  close: 'i-heroicons-x-mark-20-solid',
+  empty: 'i-heroicons-circle-stack-20-solid',
   loading: 'i-heroicons-arrow-path-20-solid',
-  empty: 'i-heroicons-circle-stack-20-solid'
+  search: 'i-heroicons-magnifying-glass-20-solid'
 }
