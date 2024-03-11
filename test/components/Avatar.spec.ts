924551e54654ee04db31c09aff309695ffaa4d52
Benjamin Canac <canacb1@gmail.com>
2024-03-11 15:43:44 +0100

    test(Avatar): update

--- test/components/Avatar.spec.ts
+++ test/components/Avatar.spec.ts
@@ -6,7 +6,9 @@ describe('Avatar', () => {
   it.each([
     ['with src', { props: { src: 'https://avatars.githubusercontent.com/u/739984?v=4' } }],
     ['with alt', { props: { alt: 'Benjamin Canac' } }],
+    ['with class', { props: { class: 'bg-white dark:bg-gray-900' } }],
     ['with fallback', { props: { fallback: '+1' } }],
+    ['with icon', { props: { icon: 'i-heroicons-photo' } }],
     ['with size 3xs', { props: { size: '3xs' as const } }],
     ['with size 2xs', { props: { size: '2xs' as const } }],
     ['with size xs', { props: { size: 'xs' as const } }],
@@ -15,7 +17,8 @@ describe('Avatar', () => {
     ['with size lg', { props: { size: 'lg' as const } }],
     ['with size xl', { props: { size: 'xl' as const } }],
     ['with size 2xl', { props: { size: '2xl' as const } }],
-    ['with size 3xl', { props: { size: '3xl' as const } }]
+    ['with size 3xl', { props: { size: '3xl' as const } }],
+    ['with ui', { props: { ui: { fallback: 'font-bold' } } }]
   ])('renders %s correctly', async (nameOrHtml: string, options: { props?: AvatarProps, slots?: any }) => {
     const html = await ComponentRender(nameOrHtml, options, Avatar)
     expect(html).toMatchSnapshot()
