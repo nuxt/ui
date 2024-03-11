37c14186a0628ba84eb2235ab1b078ba64e869fe
Benjamin Canac <canacb1@gmail.com>
2024-03-11 17:39:40 +0100

    test(Tooltip): add skip

---
+++ test/components/Tooltip.spec.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest'
+import Tooltip, { type TooltipProps } from '../../src/runtime/components/Tooltip.vue'
+import ComponentRender from '../component-render'
+
+describe.skip('Tooltip', () => {
+  it.each([
+    ['with content', { props: { content: 'Tooltip content', defaultOpen: true }, slots: { default: () => 'Hover me' } }]
+  ])('renders %s correctly', async (nameOrHtml: string, options: { props?: TooltipProps, slots?: any }) => {
+    const html = await ComponentRender(nameOrHtml, options, Tooltip)
+    expect(html).toMatchSnapshot()
+  })
+})
