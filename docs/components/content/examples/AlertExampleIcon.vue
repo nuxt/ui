925b18f813cd31c8dbb934d238ff8d0cc3ea3608
Benjamin Canac <canacb1@gmail.com>
2024-03-13 21:49:12 +0100

    Update docs/components/content/examples/AlertExampleIcon.vue

--- docs/components/content/examples/AlertExampleIcon.vue
+++ docs/components/content/examples/AlertExampleIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <UAlert title="Customize Alert Icon" description="Insert custom content into the icon slot!" icon="i-heroicons-command-line">
     <template #icon="{ icon }">
-      <UBadge title="3" size="sm">
+      <UBadge size="sm">
         <UIcon :name="icon" />
       </UBadge>
     </template>
