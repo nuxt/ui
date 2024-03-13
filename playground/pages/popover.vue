ebbd931db110d9f4c0541e749036110e3f7e9fd1
Benjamin Canac <canacb1@gmail.com>
2024-03-13 14:43:07 +0100

    playground(popover): update

--- playground/pages/popover.vue
+++ playground/pages/popover.vue
@@ -1,14 +1,24 @@
 <template>
-  <UPopover v-model:open="open" arrow mode="hover">
-    <UButton label="Hover me" color="white" />
+  <div class="flex gap-2">
+    <UPopover v-model:open="open" arrow>
+      <UButton label="Click me" color="white" />
 
-    <template #content>
-      <div class="flex gap-2 p-4">
-        <UButton label="Close" color="gray" @click="open = false" />
-        <UButton label="Send" color="black" trailing-icon="i-heroicons-paper-airplane" :loading="loading" @click="send" />
-      </div>
-    </template>
-  </UPopover>
+      <template #content>
+        <div class="flex justify-center gap-2 p-4 w-48">
+          <UButton label="Close" color="gray" @click="open = false" />
+          <UButton label="Send" color="black" trailing-icon="i-heroicons-paper-airplane" :loading="loading" @click="send" />
+        </div>
+      </template>
+    </UPopover>
+
+    <UPopover arrow mode="hover">
+      <UButton label="Hover me" color="white" />
+
+      <template #content>
+        <div class="w-48 h-16" />
+      </template>
+    </UPopover>
+  </div>
 </template>
 
 <script setup>
