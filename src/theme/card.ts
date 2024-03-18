cecfb58445affccf0d52f975d0329acbcbd3d9c2
Benjamin Canac <canacb1@gmail.com>
2024-03-18 11:33:24 +0100

    fix(Card): improve body padding

--- src/theme/card.ts
+++ src/theme/card.ts
@@ -2,7 +2,7 @@ export default {
   slots: {
     root: 'bg-white dark:bg-gray-900 ring ring-gray-200 dark:ring-gray-800 rounded-lg shadow',
     header: 'p-4 sm:px-6',
-    body: 'px-4 py-5 sm:p-6',
+    body: 'p-4 sm:p-6',
     footer: 'p-4 sm:px-6'
   }
 }
