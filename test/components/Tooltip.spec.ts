cf91171f6ced9cec68947f70951a057ae90bd2ce
Benjamin Canac <canacb1@gmail.com>
2024-03-19 16:18:11 +0100

    chore(Tooltip): add tests

--- test/components/Tooltip.spec.ts
+++ test/components/Tooltip.spec.ts
@@ -1,12 +1,25 @@
+import { defineComponent } from 'vue'
 import { describe, it, expect } from 'vitest'
+import Provider from '../../src/runtime/components/Provider.vue'
 import Tooltip, { type TooltipProps } from '../../src/runtime/components/Tooltip.vue'
 import ComponentRender from '../component-render'
 
-describe.skip('Tooltip', () => {
+const TooltipWrapper = defineComponent({
+  components: {
+    UProvider: Provider,
+    UTooltip: Tooltip
+  },
+  inheritAttrs: false,
+  template: '<UProvider><UTooltip v-bind="$attrs"><slot /></UTooltip></UProvider>'
+})
+
+describe('Tooltip', () => {
   it.each([
-    ['with text', { props: { text: 'Tooltip', open: true }, slots: { default: () => 'Hover me' } }]
+    ['with text', { props: { text: 'Tooltip', open: true }, slots: { default: () => 'Hover me' } }],
+    ['with arrow', { props: { text: 'Tooltip', arrow: true, open: true }, slots: { default: () => 'Hover me' } }],
+    ['with shortcuts', { props: { text: 'Tooltip', shortcuts: ['⌘', 'K'], open: true }, slots: { default: () => 'Hover me' } }]
   ])('renders %s correctly', async (nameOrHtml: string, options: { props?: TooltipProps, slots?: any }) => {
-    const html = await ComponentRender(nameOrHtml, options, Tooltip)
+    const html = await ComponentRender(nameOrHtml, options, TooltipWrapper)
     expect(html).toMatchSnapshot()
   })
 })
