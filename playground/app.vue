68f3da8816549f2394c6d893a83fff1dc77302d0
Benjamin Canac <canacb1@gmail.com>
2024-03-20 12:34:24 +0100

    playground: add padding bottom

--- playground/app.vue
+++ playground/app.vue
@@ -37,7 +37,7 @@ function upperName (name: string) {
     <UContainer class="min-h-screen flex flex-col gap-4 items-center justify-center overflow-y-auto">
       <UNavigationMenu :links="components.map(component => ({ label: upperName(component), to: `/${component}` }))" class="border-b border-gray-200 dark:border-gray-800" />
 
-      <div class="flex-1 flex flex-col justify-center">
+      <div class="flex-1 flex flex-col justify-center pb-12">
         <NuxtPage />
       </div>
     </UContainer>
