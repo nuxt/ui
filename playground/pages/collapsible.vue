6946c2dfecaf0788c7971cf372f9838b03acfc32
Benjamin Canac <canacb1@gmail.com>
2024-03-21 18:41:28 +0100

    playground(collapsible): handle `trailingIcon` differently

--- playground/pages/collapsible.vue
+++ playground/pages/collapsible.vue
@@ -1,10 +1,13 @@
 <template>
   <UCollapsible class="space-y-2 w-48">
-    <UButton class="group" color="gray" label="Open" block>
-      <template #trailing="trailingProps">
-        <UIcon name="i-heroicons-chevron-right-20-solid" :class="trailingProps.class" class="group-data-[state=open]:rotate-90 transition-transform duration-200" />
-      </template>
-    </UButton>
+    <UButton
+      class="group"
+      trailing-icon="i-heroicons-chevron-right-20-solid"
+      color="gray"
+      label="Open"
+      block
+      :ui="{ trailingIcon: 'group-data-[state=open]:rotate-90 transition-transform duration-200' }"
+    />
 
     <template #content>
       <Placeholder class="h-24 w-full" />
