98920e549ee4c45ec27c238b59b5aacec173e86d
Benjamin Canac <canacb1@gmail.com>
2024-03-21 13:00:34 +0100

    chore(theme): add more icons

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -3,5 +3,8 @@ export default {
   close: 'i-heroicons-x-mark-20-solid',
   empty: 'i-heroicons-circle-stack-20-solid',
   loading: 'i-heroicons-arrow-path-20-solid',
-  search: 'i-heroicons-magnifying-glass-20-solid'
+  search: 'i-heroicons-magnifying-glass-20-solid',
+  chevronDown: 'i-heroicons-chevron-down-20-solid',
+  chevronLeft: 'i-heroicons-chevron-left-20-solid',
+  chevronRight: 'i-heroicons-chevron-right-20-solid'
 }
