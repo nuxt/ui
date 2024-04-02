0b29cf5aa3281a82ff6a91517533b8619de823d3
Benjamin Canac <canacb1@gmail.com>
2024-04-02 16:50:34 +0200

    playground: clean nuxt config

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['../src/module'],
-  ui: {
-    colors: ['primary', 'red']
-  }
+  modules: ['../src/module']
 })
