ed3a3babdb9007bb694caa6b9be01dde43e94b4f
Neil Richter <me@neilrichter.com>
2024-04-15 10:27:39 +0200

    feat(useToast): allow updating an existing notification  (#1668)

--- src/runtime/composables/useToast.ts
+++ src/runtime/composables/useToast.ts
@@ -22,8 +22,17 @@ export function useToast () {
     notifications.value = notifications.value.filter((n: Notification) => n.id !== id)
   }
 
+  function update (id: string, notification: Partial<Notification>) {
+    const index = notifications.value.findIndex((n: Notification) => n.id === id)
+    if (index !== -1) {
+      const previous = notifications.value[index]
+      notifications.value.splice(index, 1, { ...previous, ...notification })
+    }
+  }
+
   return {
     add,
-    remove
+    remove,
+    update
   }
 }
