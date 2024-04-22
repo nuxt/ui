bd3fa8658f84fb7bd96d322968462c5eaa987b86
Eugen Istoc <eugenistoc@gmail.com>
2024-04-22 05:13:34 -0400

    fix(Slideover): export and clean types (#1692)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

--- src/runtime/types/slideover.d.ts
+++ src/runtime/types/slideover.d.ts
@@ -1,17 +1,16 @@
 import type { Component } from 'vue'
 
-interface Slideover {
-    ui?: any;
-    side?: 'right' | 'left';
-    transition?: boolean;
-    appear?: boolean;
-    overlay?: boolean;
-    preventClose?: boolean;
-    modelValue?: boolean;
+export interface Slideover {
+  ui?: any
+  side?: 'right' | 'left'
+  transition?: boolean
+  appear?: boolean
+  overlay?: boolean
+  preventClose?: boolean
+  modelValue?: boolean
 }
 
-interface SlideoverState {
-    component: Component | string;
-    props: Slideover;
+export interface SlideoverState {
+  component: Component | string
+  props: Slideover
 }
-
