f90f7d7b7c0d81cf42b2232e9c12597210cd5791
Benjamin Canac <canacb1@gmail.com>
2024-04-28 17:54:32 +0200

    fix(Popover): use `scale-in` / `scale-out` animations

--- src/theme/popover.ts
+++ src/theme/popover.ts
@@ -1,22 +1,6 @@
 export default {
   slots: {
-    content: 'bg-white dark:bg-gray-900 shadow-lg rounded-md ring ring-gray-200 dark:ring-gray-800',
+    content: 'bg-white dark:bg-gray-900 shadow-lg rounded-md ring ring-gray-200 dark:ring-gray-800 data-[state=open]:animate-[scale-in_100ms_ease-out] data-[state=closed]:animate-[scale-out_100ms_ease-in]',
     arrow: 'fill-gray-200 dark:fill-gray-800'
-  },
-  variants: {
-    side: {
-      top: {
-        content: 'data-[state=open]:animate-[slide-in-from-top-and-fade_200ms_ease-out] data-[state=closed]:animate-[slide-out-to-top-and-fade_200ms_ease-in]'
-      },
-      right: {
-        content: 'data-[state=open]:animate-[slide-in-from-right-and-fade_200ms_ease-out] data-[state=closed]:animate-[slide-out-to-right-and-fade_200ms_ease-in]'
-      },
-      bottom: {
-        content: 'data-[state=open]:animate-[slide-in-from-bottom-and-fade_200ms_ease-out] data-[state=closed]:animate-[slide-out-to-bottom-and-fade_200ms_ease-in]'
-      },
-      left: {
-        content: 'data-[state=open]:animate-[slide-in-from-left-and-fade_200ms_ease-out] data-[state=closed]:animate-[slide-out-to-left-and-fade_200ms_ease-in]'
-      }
-    }
   }
 }
