0450f6b4a91d0af38ff6094fb590915b7164d9e0
Benjamin Canac <canacb1@gmail.com>
2024-04-29 11:20:45 +0200

    fix(Tooltip): use `scale-in` / `scale-out` animations

--- src/theme/tooltip.ts
+++ src/theme/tooltip.ts
@@ -1,24 +1,8 @@
 export default {
   slots: {
-    content: 'flex items-center gap-1 bg-white dark:bg-gray-900 text-gray-900 dark:text-white shadow rounded ring ring-gray-200 dark:ring-gray-800 h-6 px-2 py-1 text-xs select-none',
+    content: 'flex items-center gap-1 bg-white dark:bg-gray-900 text-gray-900 dark:text-white shadow rounded ring ring-gray-200 dark:ring-gray-800 h-6 px-2 py-1 text-xs select-none data-[state=delayed-open]:animate-[scale-in_100ms_ease-out]',
     arrow: 'fill-gray-200 dark:fill-gray-800',
     text: 'truncate',
     kbds: `hidden lg:inline-flex items-center shrink-0 gap-0.5 before:content-['·'] before:mr-0.5`
-  },
-  variants: {
-    side: {
-      top: {
-        content: 'data-[state=delayed-open]:animate-[slide-in-from-top-and-fade_200ms_ease-out]'
-      },
-      right: {
-        content: 'data-[state=delayed-open]:animate-[slide-in-from-right-and-fade_200ms_ease-out]'
-      },
-      bottom: {
-        content: 'data-[state=delayed-open]:animate-[slide-in-from-bottom-and-fade_200ms_ease-out]'
-      },
-      left: {
-        content: 'data-[state=delayed-open]:animate-[slide-in-from-left-and-fade_200ms_ease-out]'
-      }
-    }
   }
 }
