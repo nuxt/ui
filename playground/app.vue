df323c04de1cdd8636a1f93f1951171aec84f611
Moshe Tanzer <tanzermt@gmail.com>
2024-05-14 14:37:41 +0200

    remove play changes

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,15 @@
 <template>
-  <UAlert>
-    <template #actions>
-      <UInput />
-    </template>
-  </UAlert>
+  <UContainer class="min-h-screen flex items-center">
+    <UCard class="flex-1" :ui="{ background: 'bg-gray-50 dark:bg-gray-800/50', ring: 'ring-1 ring-gray-300 dark:ring-gray-700', divide: 'divide-y divide-gray-300 dark:divide-gray-700', header: { base: 'font-bold' } }">
+      <template #header>
+        Welcome to the playground!
+      </template>
+
+      <p class="text-gray-500 dark:text-gray-400">
+        Try your components here!
+      </p>
+    </UCard>
+  </UContainer>
 </template>
 
 <script setup>
