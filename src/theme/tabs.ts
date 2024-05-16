f1b9e27ff7d1107bbb3f2ee0b158a15750ffee9a
Benjamin Canac <canacb1@gmail.com>
2024-05-16 15:54:50 +0200

    chore(Tabs): improve trigger transition

--- src/theme/tabs.ts
+++ src/theme/tabs.ts
@@ -3,7 +3,7 @@ export default {
     root: 'flex items-center gap-2',
     list: 'relative flex rounded-lg bg-gray-50 dark:bg-gray-800 p-1 group',
     indicator: 'absolute transition-[translate,width] duration-200 bg-white dark:bg-gray-900 rounded-md shadow-sm',
-    trigger: 'relative inline-flex items-center justify-center gap-1.5 shrink-0 flex-1 text-gray-500 data-[state=active]:text-gray-900 dark:text-gray-400 dark:data-[state=active]:text-white px-3 py-1.5 text-sm font-medium rounded-md disabled:cursor-not-allowed disabled:opacity-75 transition-colors duration-200 ease-out focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 focus:outline-none',
+    trigger: 'relative inline-flex items-center justify-center gap-1.5 shrink-0 flex-1 text-gray-500 data-[state=active]:text-gray-900 dark:text-gray-400 dark:data-[state=active]:text-white px-3 py-1.5 text-sm font-medium rounded-md disabled:cursor-not-allowed disabled:opacity-75 transition-colors ease-out focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 focus:outline-none',
     content: 'focus:outline-none',
     leadingIcon: 'shrink-0 size-5',
     leadingAvatar: 'shrink-0',
