88dca6854aea432a6ab3d28411b9dc2dce3f5d67
Khaled Oghli <khalidoghli7@gmail.com>
2024-06-03 17:15:49 +0400

    fix(lint): fix linting issues

--- docs/components/content/examples/SlideoverExampleCard.vue
+++ docs/components/content/examples/SlideoverExampleCard.vue
@@ -7,11 +7,21 @@ const isOpen = ref(false)
     <UButton label="Open" @click="isOpen = true" />
 
     <USlideover v-model="isOpen">
-      <UCard class="flex flex-col flex-1"
-        :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+      <UCard
+        class="flex flex-col flex-1"
+        :ui="{ body: { base: 'flex-1' }, ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }"
+      >
         <template #header>
-          <UButton color="gray" variant="ghost" size="sm" icon="i-heroicons-x-mark-20-solid"
-            class="flex sm:hidden absolute end-5 top-5 z-10" @click="isOpen = false" square padded />
+          <UButton
+            color="gray"
+            variant="ghost"
+            size="sm"
+            icon="i-heroicons-x-mark-20-solid"
+            class="flex sm:hidden absolute end-5 top-5 z-10"
+            square
+            padded
+            @click="isOpen = false"
+          />
 
           <Placeholder class="h-8" />
         </template>
--- docs/components/content/examples/SlideoverExampleDisableTransition.vue
+++ docs/components/content/examples/SlideoverExampleDisableTransition.vue
@@ -8,8 +8,16 @@ const isOpen = ref(false)
 
     <USlideover v-model="isOpen" :transition="false">
       <div class="p-4 flex-1">
-        <UButton color="gray" variant="ghost" size="sm" icon="i-heroicons-x-mark-20-solid"
-          class="flex sm:hidden absolute end-5 top-5 z-10" @click="isOpen = false" square padded />
+        <UButton
+          color="gray"
+          variant="ghost"
+          size="sm"
+          icon="i-heroicons-x-mark-20-solid"
+          class="flex sm:hidden absolute end-5 top-5 z-10"
+          square
+          padded
+          @click="isOpen = false"
+        />
 
         <Placeholder class="h-full" />
       </div>
