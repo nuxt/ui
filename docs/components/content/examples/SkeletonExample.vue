0565998c675127fc78d18f98be022c47e23aa8d3
Liran Tal <liran.tal@gmail.com>
2024-07-02 11:45:14 +0000

    feat(Skeleton): add syntax support for light mode and dark mode by default

--- docs/components/content/examples/SkeletonExample.vue
+++ docs/components/content/examples/SkeletonExample.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="flex items-center space-x-4">
-    <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
+    <USkeleton
+      class="h-16 w-16"
+      :ui="{
+        rounded: 'rounded-full',
+        background: 'bg-gray-300 dark:bg-slate-300',
+      }"
+    />
     <div class="space-y-2">
-      <USkeleton class="h-4 w-[250px]" />
-      <USkeleton class="h-4 w-[200px]" />
+      <USkeleton class="h-4 w-[200px] bg-gray-300 dark:bg-slate-300" />
+      <USkeleton class="h-4 w-[150px] bg-gray-300 dark:bg-slate-300" />
     </div>
   </div>
 </template>
