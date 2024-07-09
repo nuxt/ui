5c12d428c4e39a5c28b5d0107c0091f8299bca50
Benjamin Canac <canacb1@gmail.com>
2024-07-09 17:17:53 +0200

    fix(SelectMenu): adapt input size

--- src/theme/select-menu.ts
+++ src/theme/select-menu.ts
@@ -7,7 +7,26 @@ export default (options: Required<ModuleOptions>) => {
     slots: {
       value: 'truncate',
       placeholder: 'truncate text-current/50',
-      input: 'text-sm px-2.5 py-1.5 placeholder-gray-400 dark:placeholder-gray-500 border-0 border-b border-gray-200 dark:border-gray-800 focus:outline-none'
+      input: 'placeholder-gray-400 dark:placeholder-gray-500 border-0 border-b border-gray-200 dark:border-gray-800 focus:outline-none'
+    },
+    variants: {
+      size: {
+        xs: {
+          input: 'text-xs px-2 py-1'
+        },
+        sm: {
+          input: 'text-xs px-2.5 py-1.5'
+        },
+        md: {
+          input: 'text-sm px-2.5 py-1.5'
+        },
+        lg: {
+          input: 'text-sm px-3 py-2'
+        },
+        xl: {
+          input: 'text-base px-3 py-2'
+        }
+      }
     }
   }, select(options))
 }
