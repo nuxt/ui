810ea32bf8c73563676fade175db9a8eb3f072a5
Benjamin Canac <canacb1@gmail.com>
2024-07-11 17:14:45 +0200

    docs(fetchComponentMeta): handle case where component doesn't exist

--- docs/app/composables/fetchComponentMeta.ts
+++ docs/app/composables/fetchComponentMeta.ts
@@ -13,9 +13,7 @@ export async function fetchComponentMeta(name: string): Promise<{ meta: Componen
     return state.value[name]
   }
 
-  // Store promise to avoid multiple calls
-
-  // add to nitro prerender
+  // Add to nitro prerender
   if (import.meta.server) {
     const event = useRequestEvent()
     event.node.res.setHeader(
@@ -23,8 +21,12 @@ export async function fetchComponentMeta(name: string): Promise<{ meta: Componen
       [event.node.res.getHeader('x-nitro-prerender'), `/api/component-meta/${name}.json`].filter(Boolean).join(',')
     )
   }
+
+  // Store promise to avoid multiple calls
   state.value[name] = $fetch(`/api/component-meta/${name}.json`).then((meta) => {
     state.value[name] = meta
+  }).catch(() => {
+    state.value[name] = {}
   })
 
   await state.value[name]
