c1ac3a9f9d5357bc3a7b125d8793d69d8d518bd3
Benjamin Canac <canacb1@gmail.com>
2024-07-22 12:52:28 +0200

    fix(Link): allow `ariaLabel` to be picked

--- src/runtime/utils/link.ts
+++ src/runtime/utils/link.ts
@@ -1,6 +1,6 @@
 import { reactivePick } from '@vueuse/core'
 import type { LinkProps } from '../types'
 
-export function pickLinkProps(link: LinkProps) {
-  return reactivePick(link, 'active', 'activeClass', 'ariaCurrentValue', 'as', 'disabled', 'exact', 'exactActiveClass', 'exactHash', 'exactQuery', 'external', 'href', 'inactiveClass', 'noPrefetch', 'noRel', 'prefetch', 'prefetchedClass', 'rel', 'replace', 'target', 'to', 'type')
+export function pickLinkProps(link: LinkProps & { ariaLabel?: string }) {
+  return reactivePick(link, 'active', 'activeClass', 'ariaCurrentValue', 'ariaLabel', 'as', 'disabled', 'exact', 'exactActiveClass', 'exactHash', 'exactQuery', 'external', 'href', 'inactiveClass', 'noPrefetch', 'noRel', 'prefetch', 'prefetchedClass', 'rel', 'replace', 'target', 'to', 'type')
 }
