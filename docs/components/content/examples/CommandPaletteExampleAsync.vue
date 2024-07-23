2443fd70ebc5232f2b1f8b560754270656f0e3fc
Benjamin Canac <canacb1@gmail.com>
2024-07-23 13:22:25 +0200

    docs: typecheck

--- docs/components/content/examples/CommandPaletteExampleAsync.vue
+++ docs/components/content/examples/CommandPaletteExampleAsync.vue
@@ -7,6 +7,7 @@ const groups = [{
       return []
     }
 
+    // @ts-ignore
     const users: any[] = await $fetch('https://jsonplaceholder.typicode.com/users', { params: { q } })
 
     return users.map(user => ({ id: user.id, label: user.name, suffix: user.email }))
