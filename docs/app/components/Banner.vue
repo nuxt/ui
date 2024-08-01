5366e228bba6ae3851e3932ac88e1dd37fa37a70
Benjamin Canac <canacb1@gmail.com>
2024-08-01 12:08:24 +0200

    docs: update banner title

--- docs/app/components/Banner.vue
+++ docs/app/components/Banner.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-const config = useRuntimeConfig().public
-</script>
-
 <template>
   <UBanner icon="i-heroicons-light-bulb" :actions="[{ label: 'Go to Nuxt UI v2', to: 'https://ui.nuxt.com', trailingIcon: 'i-heroicons-arrow-right-20-solid', class: 'rounded-full' }]" :close="false">
     <template #title>
-      You're looking at the documentation for <span class="font-semibold">Nuxt UI v{{ config.version }}</span>!
+      You're looking at the documentation for <span class="font-semibold">Nuxt UI v3</span>!
     </template>
   </UBanner>
 </template>
