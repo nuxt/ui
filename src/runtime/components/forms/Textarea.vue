23b970eeeb675d93d3a827fe10d16881b33446a1
Gerben Mulder <github.undergo381@passmail.net>
2024-09-28 19:49:06 +0200

    feat: add nullable modifier to input and textarea

--- src/runtime/components/forms/Textarea.vue
+++ src/runtime/components/forms/Textarea.vue
@@ -127,7 +127,7 @@ export default defineComponent({
       default: () => ({})
     },
     modelModifiers: {
-      type: Object as PropType<{ trim?: boolean, lazy?: boolean, number?: boolean }>,
+      type: Object as PropType<{ trim?: boolean, lazy?: boolean, number?: boolean, nullable?: boolean }>,
       default: () => ({})
     }
   },
@@ -137,7 +137,7 @@ export default defineComponent({
 
     const { emitFormBlur, emitFormInput, inputId, color, size, name } = useFormGroup(props, config)
 
-    const modelModifiers = ref(defu({}, props.modelModifiers, { trim: false, lazy: false, number: false }))
+    const modelModifiers = ref(defu({}, props.modelModifiers, { trim: false, lazy: false, number: false, nullable: false }))
 
     const textarea = ref<HTMLTextAreaElement | null>(null)
 
@@ -181,6 +181,10 @@ export default defineComponent({
 
       if (modelModifiers.value.number) {
         value = looseToNumber(value)
+      }
+
+      if (modelModifiers.value.nullable && value === '') {
+        value = null
       }
 
       emit('update:modelValue', value)
