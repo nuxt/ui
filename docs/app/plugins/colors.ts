a13fd1d5d6202aa18a0aec053cfd7fd61ca38d80
Benjamin Canac <canacb1@gmail.com>
2024-10-06 23:25:33 +0200

    docs(colors): fix plugin replace

--- docs/app/plugins/colors.ts
+++ docs/app/plugins/colors.ts
@@ -4,15 +4,15 @@ export default defineNuxtPlugin({
     const appConfig = useAppConfig()
 
     if (import.meta.client) {
-      const primary = window.localStorage.getItem('nuxt-ui-primary')
-      if (primary) {
-        appConfig.ui.colors.primary = primary
+      function updateColor(type: 'primary' | 'neutral') {
+        const color = localStorage.getItem(`nuxt-ui-${type}`)
+        if (color) {
+          appConfig.ui.colors[type] = color
+        }
       }
 
-      const neutral = window.localStorage.getItem('nuxt-ui-neutral')
-      if (neutral) {
-        appConfig.ui.colors.neutral = neutral
-      }
+      updateColor('primary')
+      updateColor('neutral')
     }
 
     if (import.meta.server) {
@@ -23,13 +23,21 @@ export default defineNuxtPlugin({
             let html = document.querySelector('style#nuxt-ui-colors').innerHTML;
 
             if (localStorage.getItem('nuxt-ui-primary')) {
-              html = html.replaceAll('${appConfig.ui.colors.primary}', localStorage.getItem('nuxt-ui-primary'))
+              const primaryColor = localStorage.getItem('nuxt-ui-primary');
+              html = html.replace(
+                /(--ui-color-primary-\\d{2,3}:\\s*var\\()--color-${appConfig.ui.colors.primary}-(\\d{2,3}\\))/g,
+                \`$1--color-\${primaryColor}-$2\`
+              );
             }
             if (localStorage.getItem('nuxt-ui-neutral')) {
-              html = html.replaceAll('${appConfig.ui.colors.neutral}', localStorage.getItem('nuxt-ui-neutral'))
+              const neutralColor = localStorage.getItem('nuxt-ui-neutral');
+              html = html.replace(
+                /(--ui-color-neutral-\\d{2,3}:\\s*var\\()--color-${appConfig.ui.colors.neutral}-(\\d{2,3}\\))/g,
+                \`$1--color-\${neutralColor}-$2\`
+              );
             }
 
-            document.querySelector('style#nuxt-ui-colors').innerHTML = html
+            document.querySelector('style#nuxt-ui-colors').innerHTML = html;
             `.replace(/\s+/g, ' '),
             type: 'text/javascript',
             tagPriority: -1
