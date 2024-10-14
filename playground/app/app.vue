4de61052872659bd1b771c1cb276868d7a2f9629
Romain Hamel <rom.hml@gmail.com>
2024-10-14 12:38:55 +0200

    chore: lint

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -66,9 +66,9 @@ defineShortcuts({
 
 <template>
   <div v-if="!$route.path.startsWith('/__nuxt_ui__')">
-  <UApp :toaster="appConfig.toaster">
-    <div class="h-screen w-screen overflow-hidden flex min-h-0 bg-[var(--ui-bg)]" vaul-drawer-wrapper>
-      <UNavigationMenu :items="items" orientation="vertical" class="border-r border-[var(--ui-border)] overflow-y-auto w-48 p-4" />
+    <UApp :toaster="appConfig.toaster">
+      <div class="h-screen w-screen overflow-hidden flex min-h-0 bg-[var(--ui-bg)]" vaul-drawer-wrapper>
+        <UNavigationMenu :items="items" orientation="vertical" class="border-r border-[var(--ui-border)] overflow-y-auto w-48 p-4" />
         <div class="flex-1 flex flex-col items-center justify-around overflow-y-auto w-full py-12 px-4">
           <NuxtPage />
         </div>
