49da2da6822e337a172e1d36a706053cf5d019c0
Daniel Roe <daniel@roe.dev>
2024-10-18 13:03:48 +0100

    chore: improve vue playground

--- playground-vue/vite.config.ts
+++ playground-vue/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImports from 'unplugin-auto-import/vite'
+import VueComponents from 'unplugin-vue-components'
 
 import { NuxtUIPlugin } from '@nuxt/ui/unplugin'
 
@@ -8,12 +9,11 @@ import { NuxtUIPlugin } from '@nuxt/ui/unplugin'
 export default defineConfig({
   server: {
     fs: {
-      allow: ['../playground', '..']
+      allow: ['..']
     }
   },
   plugins: [
     vue(),
-    AutoImports({ imports: ['vue'] }),
     NuxtUIPlugin.vite({
       ui: {
         colors: {
@@ -21,6 +21,15 @@ export default defineConfig({
           neutral: 'slate'
         }
       }
+    }),
+    // these are required as we share the component pages with the Nuxt playground
+    AutoImports({ imports: ['vue'] }),
+    VueComponents.vite({
+      dirs: ['../playground/app/components']
     })
-  ]
+  ],
+  optimizeDeps: {
+    // prevents reloading page when navigating between components
+    include: ['radix-vue/namespaced', 'vaul-vue', 'fast-deep-equal', 'embla-carousel-vue', 'embla-carousel-autoplay', 'embla-carousel-auto-scroll', 'embla-carousel-auto-height', 'embla-carousel-class-names', 'embla-carousel-fade', 'embla-carousel-wheel-gestures']
+  }
 })
