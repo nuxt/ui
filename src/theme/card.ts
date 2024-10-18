f1716dafbb1e2858fae923a898cc2fcb82b999f3
Georges KABBOUCHI <georges.kabbouchi@gmail.com>
2024-10-18 21:19:25 +0300

    feat(card): add CSS vars for bg and text

--- src/theme/card.ts
+++ src/theme/card.ts
@@ -1,6 +1,6 @@
 export default {
   slots: {
-    root: 'bg-[var(--ui-bg)] ring ring-[var(--ui-border)] divide-y divide-[var(--ui-border)] rounded-[calc(var(--ui-radius)*2)] shadow',
+    root: 'bg-[var(--ui-card-bg)] text-[var(--ui-card-text)] ring ring-[var(--ui-border)] divide-y divide-[var(--ui-border)] rounded-[calc(var(--ui-radius)*2)] shadow',
     header: 'p-4 sm:px-6',
     body: 'p-4 sm:p-6',
     footer: 'p-4 sm:px-6'
