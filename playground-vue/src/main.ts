01bdcba8d986b0d345c4ec3af1d41dfff1855e58
Benjamin Canac <canacb1@gmail.com>
2024-10-21 12:32:56 +0200

    playground-vue: load playground components

--- playground-vue/src/main.ts
+++ playground-vue/src/main.ts
@@ -5,6 +5,7 @@ import uiPlugin from '@nuxt/ui/vue-plugin'
 import App from './app.vue'
 
 const pages = import.meta.glob('../../playground/app/pages/**/*.vue')
+const components = import.meta.glob('../../playground/app/components/**/*.vue')
 
 const routes = Object.keys(pages).map((path) => {
   const name = path.match(/\.\.\/\.\.\/playground\/app\/pages(.*)\.vue$/)![1].toLowerCase()
@@ -20,6 +21,11 @@ const router = createRouter({
 })
 
 const app = createApp(App)
+
+Object.entries(components).forEach(([path, component]) => {
+  const name = path.split('/').pop()!.replace('.vue', '')
+  app.component(name, defineAsyncComponent(component as any))
+})
 
 app.use(router)
 app.use(uiPlugin)
