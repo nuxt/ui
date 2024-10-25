5ba374d8317e12bf279b6ad2d2befa2c5c8812b1
malik jouda <1999.malik.jouda@gmail.com>
2024-10-25 18:07:05 +0300

    fix(Tooltip): handle `Tooltip` in RTL mode

--- src/theme/tooltip.ts
+++ src/theme/tooltip.ts
@@ -3,7 +3,7 @@ export default {
     content: 'flex items-center gap-1 bg-[var(--ui-bg)] text-[var(--ui-text-highlighted)] shadow rounded-[var(--ui-radius)] ring ring-[var(--ui-border)] h-6 px-2 py-1 text-xs select-none data-[state=delayed-open]:animate-[scale-in_100ms_ease-out] data-[state=closed]:animate-[scale-out_100ms_ease-in]',
     arrow: 'fill-[var(--ui-border)]',
     text: 'truncate',
-    kbds: `hidden lg:inline-flex items-center shrink-0 gap-0.5 before:content-['·'] before:mr-0.5`,
+    kbds: `hidden lg:inline-flex items-center shrink-0 gap-0.5 before:content-['·'] before:me-0.5`,
     kbdsSize: 'sm'
   }
 }
