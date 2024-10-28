311b61cdbe74d9fef69bf4122db7ee50fae0609a
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2024-10-28 17:17:14 +0100

    fix(): allow string for any items

--- src/runtime/types/utils.ts
+++ src/runtime/types/utils.ts
@@ -28,7 +28,7 @@ export type PartialString<T> = {
 export type SelectItems<T> = T[] | T[][]
 export type SelectItemType<I extends SelectItems<unknown>> = I extends (infer U)[][] ? U : I extends (infer U)[] ? U : never
 export type SelectModelValue<T, V, M extends boolean = false, DV = T> = (T extends Record<string, any> ? V extends keyof T ? T[V] : DV : T) extends infer U ? M extends true ? U[] : U : never
-export type SelectItemKey<T> = (T extends Record<string, any> ? keyof T : never)
+export type SelectItemKey<T> = (T extends Record<string, any> ? keyof T : string)
 export type SelectModelValueEmits<T, V, M extends boolean = false, DV = T> = {
   'update:modelValue': [payload: SelectModelValue<T, V, M, DV>]
 }
