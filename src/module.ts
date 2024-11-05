23971efdb007701352ce58412db597cd95b9996b
CJBoy <78361904+cjboy76@users.noreply.github.com>
2024-11-05 23:08:54 +0800

    fix(module): missing types in `ui` config (#2467)

--- src/module.ts
+++ src/module.ts
@@ -21,7 +21,7 @@ type UI = {
   colors?: string[]
   strategy?: Strategy
   [key: string]: any
-} & DeepPartial<typeof config, string>
+} & DeepPartial<typeof config, string | number | boolean>
 
 declare module '@nuxt/schema' {
   interface AppConfigInput {
