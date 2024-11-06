60c574cd0184fa8235d8e4d510f14219509dde56
Benjamin Canac <canacb1@gmail.com>
2024-11-06 14:41:39 +0100

    docs(app): update banner icon

--- docs/app/components/Banner.vue
+++ docs/app/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <UBanner icon="i-lucide-wrench" :actions="[{ label: 'Go to Nuxt UI v2', to: 'https://ui.nuxt.com', trailingIcon: 'i-lucide-arrow-right' }]" :close="false">
+  <UBanner icon="i-lucide-construction" :actions="[{ label: 'Go to Nuxt UI v2', to: 'https://ui.nuxt.com', trailingIcon: 'i-lucide-arrow-right' }]" :close="false">
     <template #title>
       You're looking at the documentation for <span class="font-semibold">Nuxt UI v3</span>!
     </template>
