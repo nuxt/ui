008f380716562b6999ad92ecd0007f2ed1921d9a
Benjamin Canac <canacb1@gmail.com>
2024-11-08 11:46:21 +0100

    feat(locale): add `fr`

---
+++ src/runtime/locale/fr.ts
@@ -0,0 +1,38 @@
+export default {
+  name: 'Français',
+  ui: {
+    inputMenu: {
+      noMatch: 'Aucune donnée correspondante',
+      noData: 'Aucune donnée'
+    },
+    commandPalette: {
+      noMatch: 'Aucune donnée correspondante',
+      noData: 'Aucune donnée',
+      close: 'Fermer'
+    },
+    selectMenu: {
+      noMatch: 'Aucune donnée correspondante',
+      noData: 'Aucune donnée'
+    },
+    toast: {
+      close: 'Fermer'
+    },
+    carousel: {
+      prev: 'Précédent',
+      next: 'Suivant',
+      goto: 'Aller à {slide}'
+    },
+    modal: {
+      close: 'Fermer'
+    },
+    slideover: {
+      close: 'Fermer'
+    },
+    alert: {
+      close: 'Fermer'
+    },
+    table: {
+      noData: 'Aucune donnée'
+    }
+  }
+}
