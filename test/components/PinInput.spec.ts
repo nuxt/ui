444b4009763fcdc8f0b440723173954e30f02010
Benjamin Canac <canacb1@gmail.com>
2024-11-09 11:07:04 +0100

    test: update

--- test/components/PinInput.spec.ts
+++ test/components/PinInput.spec.ts
@@ -10,23 +10,22 @@ describe('PinInput', () => {
   it.each([
     // Props
     ['with as', { props: { as: 'div' } }],
-    ['with class', { props: { class: '' } }],
-    ['with ui', { props: { ui: {} } }],
+    ['with modelValue', { props: { modelValue: [] } }],
+    ['with defaultValue', { props: { defaultValue: ['1'] } }],
+    ['with type', { props: { type: 'text' } }],
+    ['with placeholder', { props: { placeholder: '*' } }],
+    ['with length', { props: { length: 6 } }],
+    ['with disabled', { props: { disabled: true } }],
+    ['with required', { props: { required: true } }],
+    ['with otp', { props: { otp: true } }],
+    ['with id', { props: { id: 'pin-input-id' } }],
+    ['with name', { props: { name: 'pin-input' } }],
     ...sizes.map((size: string) => [`with size ${size}`, { props: { size } }]),
     ...variants.map((variant: string) => [`with primary variant ${variant}`, { props: { variant } }]),
     ...variants.map((variant: string) => [`with neutral variant ${variant}`, { props: { variant, color: 'neutral' } }]),
-    ['with length', { props: { length: 6 } }],
     ['with highlight', { props: { highlight: true } }],
-    ['with type', { props: { type: 'text' } }],
-    ['with modelValue', { props: { modelValue: [] } }],
-    ['with disabled', { props: { disabled: true } }],
-    ['with otp', { props: { otp: true } }],
-    ['with required', { props: { required: true } }],
-    ['with placeholder', { props: { placeholder: '*' } }],
-    ['with dir', { props: { dir: 'ltr' } }],
-    ['with defaultValue', { props: { defaultValue: ['1'] } }],
-    ['with id', { props: { id: 'pin-input-id' } }],
-    ['with name', { props: { name: 'pin-input' } }]
+    ['with class', { props: { class: '' } }],
+    ['with ui', { props: { ui: {} } }]
   ])('renders %s correctly', async (nameOrHtml: string, options: { props?: PinInputProps }) => {
     const html = await ComponentRender(nameOrHtml, options, PinInput)
     expect(html).toMatchSnapshot()
