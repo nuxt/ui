71aedf10b67d427aa8059910c880c03fe7cf4216
Sandros94 <sandro.circi@digitoolmedia.com>
2024-11-09 13:25:47 +0100

    feat(locale): add support for Italian

--- src/runtime/locale/index.ts
+++ src/runtime/locale/index.ts
@@ -1,6 +1,7 @@
 export { default as de } from './de'
 export { default as en } from './en'
 export { default as fr } from './fr'
+export { default as it } from './it'
 export { default as ru } from './ru'
 export { default as zh_hans } from './zh_hans'
 export { default as zh_hant } from './zh_hant'
