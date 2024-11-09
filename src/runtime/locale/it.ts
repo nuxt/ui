4fbbb25f68b0b5ee76e50f2da776a74d54acc041
Sandro Circi <sandro.circi@digitoolmedia.com>
2024-11-09 13:50:03 +0100

    feat(locale): add support for Italian (#2583)

---
+++ src/runtime/locale/it.ts
@@ -0,0 +1,37 @@
+import { defineLocale } from '../composables/defineLocale'
+
+export default defineLocale('Italiano', {
+  inputMenu: {
+    noMatch: 'Nessun dato corrispondente',
+    noData: 'Nessun dato'
+  },
+  commandPalette: {
+    noMatch: 'Nessun dato corrispondente',
+    noData: 'Nessun dato',
+    close: 'Chiudi'
+  },
+  selectMenu: {
+    noMatch: 'Nessun dato corrispondente',
+    noData: 'Nessun dato'
+  },
+  toast: {
+    close: 'Chiudi'
+  },
+  carousel: {
+    prev: 'Precedente',
+    next: 'Successivo',
+    goto: 'Vai alla slide {slide}'
+  },
+  modal: {
+    close: 'Chiudi'
+  },
+  slideover: {
+    close: 'Chiudi'
+  },
+  alert: {
+    close: 'Chiudi'
+  },
+  table: {
+    noData: 'Nessuno dato'
+  }
+})
