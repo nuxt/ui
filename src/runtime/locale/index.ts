4889d30b448296de42e146dc5771738837c31f8c
Adam Kasper <adam@obvio.cz>
2024-11-10 18:17:32 +0100

    feat(locale): add support for Czech translation (#2593)

--- src/runtime/locale/index.ts
+++ src/runtime/locale/index.ts
@@ -1,4 +1,5 @@
 export { default as ar } from './ar'
+export { default as cs } from './cs'
 export { default as de } from './de'
 export { default as en } from './en'
 export { default as fr } from './fr'
