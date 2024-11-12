81170591119ee556a8fc504ed7599045cd5d80c3
Daniel Roe <daniel@roe.dev>
2024-11-12 22:57:55 +0000

    fix(unplugin): add declared types for `#build/app.config`

--- src/unplugin.ts
+++ src/unplugin.ts
@@ -24,7 +24,7 @@ import type { DeepPartial } from './runtime/types/utils'
 type NeutralColor = 'slate' | 'gray' | 'zinc' | 'neutral' | 'stone'
 type Color = Exclude<keyof typeof colors, 'inherit' | 'current' | 'transparent' | 'black' | 'white' | NeutralColor> | (string & {})
 
-type AppConfigUI = {
+export type AppConfigUI = {
   // TODO: add type hinting for colors from `options.theme.colors`
   colors?: Record<string, Color> & { neutral?: NeutralColor }
   icons?: Partial<typeof icons>
