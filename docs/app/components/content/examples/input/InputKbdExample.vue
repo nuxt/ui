1d95eb7246dd1a69b2ca0e624e6488338baa70f4
Benjamin Canac <canacb1@gmail.com>
2024-11-13 16:58:21 +0100

    docs(input): add more examples

---
+++ docs/app/components/content/examples/input/InputKbdExample.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const input = useTemplateRef('input')
+
+defineShortcuts({
+  '/': () => {
+    input.value?.inputRef?.focus()
+  }
+})
+</script>
+
+<template>
+  <UInput
+    ref="input"
+    icon="i-lucide-search"
+    placeholder="Search..."
+  >
+    <template #trailing>
+      <UKbd value="/" />
+    </template>
+  </UInput>
+</template>
