5af207d9032ba4cadffa4ce2cfebe78fba930c8e
Malik-Jouda <1999.malik.jouda@gmail.com>
2024-11-19 19:57:41 +0200

    fix(playground): fix responsive

--- playground-vue/src/app.vue
+++ playground-vue/src/app.vue
@@ -84,9 +84,9 @@ defineShortcuts({
   <UApp :toaster="(appConfig.toaster as any)">
     <div class="h-screen w-screen overflow-hidden flex min-h-0 bg-[var(--ui-bg)]" vaul-drawer-wrapper>
       <UNavigationMenu :items="items" orientation="vertical" class="hidden lg:flex border-e border-[var(--ui-border)] overflow-y-auto w-48 p-4" />
-      <UNavigationMenu :items="items" orientation="horizontal" class="lg:hidden border-b border-[var(--ui-border)] overflow-x-auto" />
+      <UNavigationMenu :items="items" orientation="horizontal" class="lg:hidden border-b border-[var(--ui-border)] [&>div]:min-w-min overflow-x-auto" />
 
-      <div class="fixed top-4 right-4 flex items-center gap-2">
+      <div class="fixed top-15 lg:top-3 right-4 flex items-center gap-2">
         <UButton
           :icon="mode === 'dark' ? 'i-lucide-moon' : 'i-lucide-sun'"
           color="neutral"
@@ -96,7 +96,7 @@ defineShortcuts({
         />
       </div>
 
-      <div class="flex-1 flex flex-col items-center justify-around overflow-y-auto w-full py-12 px-4">
+      <div class="flex-1 flex flex-col items-center justify-around overflow-y-auto w-full py-14 px-4">
         <Suspense>
           <RouterView />
         </Suspense>
