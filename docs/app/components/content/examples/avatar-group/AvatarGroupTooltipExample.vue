a9b9f38e34f65840154ed9e940fae36fc88ab7fc
Malik-Jouda <1999.malik.jouda@gmail.com>
2024-11-19 22:02:22 +0200

    fix(AvatarGroup): not working with `tooltip` around `avatar`

--- docs/app/components/content/examples/avatar-group/AvatarGroupTooltipExample.vue
+++ docs/app/components/content/examples/avatar-group/AvatarGroupTooltipExample.vue
@@ -1,24 +1,32 @@
 <template>
   <UAvatarGroup>
-    <UTooltip text="benjamincanac">
-      <UAvatar
-        src="https://github.com/benjamincanac.png"
-        alt="Benjamin Canac"
-      />
-    </UTooltip>
+    <div>
+      <UTooltip text="benjamincanac">
+        <UAvatar
+          src="https://github.com/benjamincanac.png"
+          alt="Benjamin Canac"
+        />
+      </UTooltip>
+    </div>
 
-    <UTooltip text="romhml">
-      <UAvatar
-        src="https://github.com/romhml.png"
-        alt="Romain Hamel"
-      />
-    </UTooltip>
+    <div>
+      <UTooltip text="romhml">
+        <UAvatar
+          src="https://github.com/romhml.png"
+          alt="Romain Hamel"
+        />
+      </UTooltip>
+    </div>
 
-    <UTooltip text="noook">
-      <UAvatar
-        src="https://github.com/noook.png"
-        alt="Neil Richter"
-      />
-    </UTooltip>
+    <div>
+      <UTooltip text="noook">
+        <UAvatar
+          src="https://github.com/noook.png"
+          alt="Neil Richter"
+        />
+      </UTooltip>
+    </div>
   </UAvatarGroup>
 </template>
+<script setup lang="ts">
+</script>
