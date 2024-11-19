7a02bfeba682bf0adfa37bfdfe2b1fad27d1934c
Malik-Jouda <48517781+Malik-Jouda@users.noreply.github.com>
2024-11-19 20:26:09 +0200

    playground: improve responsive (#2675)

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -85,9 +85,9 @@ defineShortcuts({
     <UApp :toaster="appConfig.toaster">
       <div class="h-screen w-screen overflow-hidden flex flex-col lg:flex-row min-h-0 bg-[var(--ui-bg)]" vaul-drawer-wrapper>
         <UNavigationMenu :items="items" orientation="vertical" class="hidden lg:flex border-e border-[var(--ui-border)] overflow-y-auto w-48 p-4" />
-        <UNavigationMenu :items="items" orientation="horizontal" class="lg:hidden border-b border-[var(--ui-border)] overflow-x-auto" />
+        <UNavigationMenu :items="items" orientation="horizontal" class="lg:hidden border-b border-[var(--ui-border)] [&>div]:min-w-min overflow-x-auto" />
 
-        <div class="fixed top-4 right-4 flex items-center gap-2">
+        <div class="fixed top-15 lg:top-3 right-4 flex items-center gap-2">
           <ClientOnly v-if="!colorMode?.forced">
             <UButton
               :icon="isDark ? 'i-lucide-moon' : 'i-lucide-sun'"
@@ -103,7 +103,7 @@ defineShortcuts({
           </ClientOnly>
         </div>
 
-        <div class="flex-1 flex flex-col items-center justify-around overflow-y-auto w-full py-12 px-4">
+        <div class="flex-1 flex flex-col items-center justify-around overflow-y-auto w-full py-14 px-4">
           <NuxtPage />
         </div>
 
