91af712e373886b25a1d7d071ab0bf48f4e08b7b
hywax <me@hywax.space>
2024-11-22 00:36:19 +0500

    fix(Calendar): use `dir`, `locale` from useLocale

--- src/runtime/components/Calendar.vue
+++ src/runtime/components/Calendar.vue
@@ -14,7 +14,7 @@ type CalendarVariants = VariantProps<typeof calendar>
 
 export type ModelValue<T extends boolean> = (T extends true ? RangeCalendarRootProps['modelValue'] : CalendarRootProps['modelValue']) | undefined
 
-export interface CalendarProps<T extends boolean> extends Omit<CalendarRootProps & RangeCalendarRootProps, 'modelValue' | 'defaultValue'> {
+export interface CalendarProps<T extends boolean> extends Omit<CalendarRootProps & RangeCalendarRootProps, 'modelValue' | 'defaultValue' | 'dir' | 'locale'> {
   /**
    * The element or component this component should render as.
    * @defaultValue 'div'
