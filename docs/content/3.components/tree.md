5e0aaf9bdf9384c9cd0b0b97b1eb22eefea45a07
hywax <me@hywax.space>
2024-11-25 19:19:02 +0500

    feat(Tree): implement component

---
+++ docs/content/3.components/tree.md
@@ -0,0 +1,32 @@
+---
+description:
+links: 
+  - label: Tree
+    icon: i-custom-radix-vue
+    to: https://www.radix-vue.com/components/tree.html
+  - label: GitHub
+    icon: i-simple-icons-github
+    to: https://github.com/nuxt/ui/tree/v3/src/runtime/components/Tree.vue
+---
+
+## Usage
+
+## Examples
+
+## API
+
+### Props
+
+:component-props
+
+### Slots
+
+:component-slots
+
+### Emits
+
+:component-emits
+
+## Theme
+
+:component-theme
---
+++ playground/app/pages/components/tree.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const items = ref([
+  {
+    label: 'Backlog',
+    value: 'backlog',
+    icon: 'i-lucide-circle-help',
+    children: [
+      {
+        label: 'Backlog 1',
+        value: 'backlog-1',
+        icon: 'i-lucide-circle-help'
+      },
+      {
+        label: 'Backlog 2',
+        value: 'backlog-2',
+        icon: 'i-lucide-circle-help'
+      }
+    ]
+  },
+  {
+    label: 'Todo',
+    value: 'todo',
+    icon: 'i-lucide-circle-plus'
+  },
+  {
+    label: 'In Progress',
+    value: 'in_progress',
+    icon: 'i-lucide-circle-arrow-up'
+  },
+  {
+    label: 'Done',
+    value: 'done',
+    icon: 'i-lucide-circle-check'
+  }
+])
+</script>
+
+<template>
+  <div>
+    <UTree :items="items" :get-key="(item) => item.label" />
+  </div>
+</template>
