91c1105a05d166d5725663784bce1ee1c446ce50
Anthony Fu <github@antfu.me>
2024-12-06 14:40:27 +0800

    chore: lint

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,9 +1,8 @@
 import { fileURLToPath } from 'node:url'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
-import { defaultExclude,  defineWorkspace} from 'vitest/config'
+import { defaultExclude, defineWorkspace, defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import ui from './src/vite'
-import { defineConfig } from 'vitest/config'
 import { glob } from 'tinyglobby'
 import { resolve } from 'pathe'
 
@@ -11,6 +10,7 @@ const components = await glob('./src/runtime/components/*.vue', { absolute: true
 const vueComponents = await glob('./src/runtime/vue/components/*.vue', { absolute: true })
 
 export default defineWorkspace([
+  // Nuxt Tests
   defineVitestConfig({
     test: {
       name: 'nuxt',
@@ -27,54 +27,53 @@ export default defineWorkspace([
       setupFiles: fileURLToPath(new URL('test/nuxt/setup.ts', import.meta.url))
     }
   }),
+  // Vue Tests
   defineConfig({
-  test: {
-    name: 'vue',
-    environment: 'happy-dom',
-    silent: true,
-    include: ['./test/components/**.spec.ts'],
-    setupFiles: ['./test/utils/setup.ts'],
-    resolveSnapshotPath(path, extension) {
-      return path.replace(/\/([^/]+)\.spec\.ts$/, `/__snapshots__/$1-vue.spec.ts${extension}`)
-    }
-  },
-  plugins: [
-    vue(),
-    ui({ dts: false }),
-    {
-      name: 'nuxt-ui-test:components',
-      enforce: 'pre',
-      resolveId(id) {
-        if (id === '@nuxt/test-utils/runtime') {
-          return resolve('./test/utils/mount')
-        }
+    test: {
+      name: 'vue',
+      environment: 'happy-dom',
+      silent: true,
+      include: ['./test/components/**.spec.ts'],
+      setupFiles: ['./test/utils/setup.ts'],
+      resolveSnapshotPath(path, extension) {
+        return path.replace(/\/([^/]+)\.spec\.ts$/, `/__snapshots__/$1-vue.spec.ts${extension}`)
       }
     },
-    {
-      name: 'nuxt-ui-test:components',
-      enforce: 'pre',
-      resolveId(id) {
-        if (id === '#components') {
-          return '#components'
+    plugins: [
+      vue(),
+      ui({ dts: false }),
+      {
+        name: 'nuxt-ui-test:components',
+        enforce: 'pre',
+        resolveId(id) {
+          if (id === '@nuxt/test-utils/runtime') {
+            return resolve('./test/utils/mount')
+          }
         }
       },
-      load(id) {
-        if (id === '#components' || id === '?#components') {
-          const resolvedComponents = [...vueComponents, ...components]
-          const renderedComponents = new Set<string>()
-          return resolvedComponents.map((file) => {
-            const componentName = file.split('/').pop()!.replace('.vue', '')
-            if (renderedComponents.has(componentName)) {
-              return ''
-            }
-            renderedComponents.add(componentName)
-            return `export { default as U${componentName} } from '${file}'`
-          }).join('\n')
+      {
+        name: 'nuxt-ui-test:components',
+        enforce: 'pre',
+        resolveId(id) {
+          if (id === '#components') {
+            return '#components'
+          }
+        },
+        load(id) {
+          if (id === '#components' || id === '?#components') {
+            const resolvedComponents = [...vueComponents, ...components]
+            const renderedComponents = new Set<string>()
+            return resolvedComponents.map((file) => {
+              const componentName = file.split('/').pop()!.replace('.vue', '')
+              if (renderedComponents.has(componentName)) {
+                return ''
+              }
+              renderedComponents.add(componentName)
+              return `export { default as U${componentName} } from '${file}'`
+            }).join('\n')
+          }
         }
       }
-    }
-  ]
-})
-
+    ]
+  })
 ])
-
