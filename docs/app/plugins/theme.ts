42f6bc8d94280a4653c53711f5fcd7ab9535e59b
Sébastien Chopin <atinux@gmail.com>
2025-02-07 17:49:36 +0100

    docs(theme): fix when using neutral color on pre-rendering

--- docs/app/plugins/theme.ts
+++ docs/app/plugins/theme.ts
@@ -48,12 +48,9 @@ export default defineNuxtPlugin({
             }
             if (localStorage.getItem('nuxt-ui-neutral')) {
               let neutralColor = localStorage.getItem('nuxt-ui-neutral');
-              if (neutralColor === 'neutral') {
-                neutralColor = 'old-neutral';
-              }
               html = html.replace(
                 /(--ui-color-neutral-\\d{2,3}:\\s*var\\()--color-${appConfig.ui.colors.neutral}-(\\d{2,3}\\))/g,
-                \`$1--color-\${neutralColor}-$2\`
+                \`$1--color-\${neutralColor === 'neutral' ? 'old-neutral' : neutralColor}-$2\`
               );
             }
 
