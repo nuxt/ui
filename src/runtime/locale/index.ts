cba4e8250e01a2c6979d805bb1ed6211b09b6aa2
B. Jonson <www.jbc.bd@gmail.com>
2025-02-15 23:45:54 +0600

    feat(locale): add Bengali (বাংলা) language (#3321)
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

--- src/runtime/locale/index.ts
+++ src/runtime/locale/index.ts
@@ -1,5 +1,6 @@
 export { default as ar } from './ar'
 export { default as az } from './az'
+export { default as bn } from './bn'
 export { default as cs } from './cs'
 export { default as da } from './da'
 export { default as de } from './de'
