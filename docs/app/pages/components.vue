4812c8a169a5a387e0a01e2824edacab089b9b12
Sébastien Chopin <atinux@gmail.com>
2025-02-20 15:14:37 +0100

    up

--- docs/app/pages/components.vue
+++ docs/app/pages/components.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-definePageMeta({
-  layout: 'docs'
-})
-
 useSeoMeta({
   titleTemplate: `%s - Nuxt UI`,
   title: 'Vue Components',
@@ -18,32 +14,66 @@ const { data: components } = await useAsyncData('components', () => {
   return queryCollection('content')
     .where('path', 'LIKE', '/components/%')
     .where('extension', '=', 'md')
-    .select('path', 'title', 'description', 'category')
+    .select('path', 'title', 'description', 'category', 'module')
     .all()
 })
 </script>
 
 <template>
-  <UContainer>
-    <UPage>
-      <UPageHeader title="Vue Components" description="Discover all the UI components available in both Nuxt UI and Nuxt UI Pro." />
-      <UPageBody>
-        <UPageGrid>
-          <UPageCard
-            v-for="component in components"
-            :key="component.path"
-            variant="naked"
-            :title="component.title"
-            :description="component.description"
-            :to="component.path"
-            reverse
-          >
-            <div class="group rounded-(--ui-radius) border border-(--ui-border) overflow-hidden aspect-[16/9]">
-              <NuxtImg :src="`${component.path}.png`" width="384" height="216" class="inline-block group-hover:scale-105 transition-transform" />
+  <UMain>
+    <UPageHero
+      :title="`Craft your UI with ${components!.length} components.`"
+      description="Build your Vue or Nuxt application faster with Nuxt UI and Nuxt UI Pro components. Powered by Tailwind CSS and  Reka UI, all the components are responsive & customizable."
+      class="relative"
+    >
+      <template #headline>
+        <UBadge label="Made with Tailwind CSS" size="lg" variant="outline" color="neutral" icon="i-logos-tailwindcss-icon" />
+      </template>
+      <template #links>
+        <UButton
+          to="/getting-started/installation/vue"
+          label="Start with Vue"
+          icon="i-logos-vue"
+          color="neutral"
+          variant="outline"
+          size="xl"
+        />
+        <UButton
+          to="/getting-started/installation/nuxt"
+          label="Start with Nuxt"
+          icon="i-logos-nuxt-icon"
+          color="neutral"
+          variant="outline"
+          size="xl"
+        />
+      </template>
+      <template #top>
+        <div class="absolute z-[-1] rounded-full bg-(--ui-primary) blur-[300px] size-60 sm:size-80 transform -translate-x-1/2 left-1/2 -translate-y-80" />
+        <StarsBg />
+      </template>
+    </UPageHero>
+    <UContainer>
+      <UPageGrid class="xl:grid-cols-4 mb-12">
+        <UPageCard
+          v-for="component in components"
+          :key="component.path"
+          variant="naked"
+          :title="component.title"
+          :description="component.description"
+          :to="component.path"
+          :ui="{ wrapper: 'order-last' }"
+        >
+          <template #title>
+            <div class="flex items-center justify-between">
+              <span>{{ component.title }}</span>
+              <UBadge v-if="component.module === 'ui-pro'" label="PRO" size="sm" variant="subtle" />
             </div>
-          </UPageCard>
-        </UPageGrid>
-      </UPageBody>
-    </UPage>
-  </UContainer>
+          </template>
+          <div class="group rounded-(--ui-radius) border border-(--ui-border) overflow-hidden aspect-[16/9] bg-(--ui-bg-muted)">
+            <UColorModeImage :light="`${component.path.replace('/components/', '/components/light/')}.png`" :dark="`${component.path.replace('/components/', '/components/dark/')}.png`" class="inline-block group-hover:scale-105 transition-transform" />
+          </div>
+        </UPageCard>
+      </UPageGrid>
+    </UContainer>
+  </UMain>
 </template>
