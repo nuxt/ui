1e59403b0229246c58a6c8412f5d11ba65153f09
Eugen Istoc <eugenistoc@gmail.com>
2025-02-24 11:42:14 -0500

    refactor: simplify ModalExample component by removing unused modal logic

--- playground/app/components/ModalExample.vue
+++ playground/app/components/ModalExample.vue
@@ -1,30 +1,13 @@
 <script setup lang="ts">
-const LazyModalExample = defineAsyncComponent(() => import('./ModalExample2.vue'))
-
-const overlay = useOverlay()
-const modalexample2Instance = overlay.create(LazyModalExample)
-
-const nestedCount = ref(0)
-
 defineProps<{
   count: number
 }>()
 
 const emit = defineEmits(['close'])
-
-function openModal() {
-  modalexample2Instance.open({ count: nestedCount.value })
-  nestedCount.value++
-}
 </script>
 
 <template>
   <UModal :title="`This modal was opened programmatically ${count} times`">
-    <template #body>
-      <UButton @click="openModal">
-        Open another Modal
-      </UButton>
-    </template>
     <template #footer>
       <UButton color="neutral" label="Close" @click="emit('close')" />
     </template>
