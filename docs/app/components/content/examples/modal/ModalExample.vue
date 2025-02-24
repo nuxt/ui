1325151141326f0d74f5881b39ecbeeeac6056f6
Eugen Istoc <eugenistoc@gmail.com>
2025-02-24 11:14:43 -0500

    docs: update modal example title and enhance installation notes for overlays

--- docs/app/components/content/examples/modal/ModalExample.vue
+++ docs/app/components/content/examples/modal/ModalExample.vue
@@ -7,7 +7,7 @@ const emit = defineEmits<{ close: [boolean] }>()
 </script>
 
 <template>
-  <UModal :close="{ onClick: () => emit('close', false) }" :title="`This modal was opened programmaticallyyyy ${count} times`">
+  <UModal :close="{ onClick: () => emit('close', false) }" :title="`This modal was opened programmatically ${count} times`">
     <template #footer>
       <div class="flex gap-2">
         <UButton color="neutral" label="Dismiss" @click="emit('close', false)" />
