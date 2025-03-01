211e053bc3358936273038468e55a2ce830c1b45
Sandros94 <sandro.circi@digitoolmedia.com>
2025-03-01 15:30:28 +0100

    fix(Stepper): slot type cast

--- src/runtime/components/Stepper.vue
+++ src/runtime/components/Stepper.vue
@@ -163,7 +163,7 @@ defineExpose({
 
     <div v-if="currentStep?.content || !!slots.content || (currentStep?.slot && !!slots[currentStep.slot as keyof StepperSlots<T>]) || (currentStep?.value && !!slots[currentStep.value as keyof StepperSlots<T>])" :class="ui.content({ class: props.ui?.description })">
       <slot
-        :name="!!slots[(currentStep?.slot ?? currentStep.value!) as keyof StepperSlots<T>] ? currentStep.slot ?? currentStep.value : 'content'"
+        :name="((!!slots[currentStep?.slot ?? currentStep.value!] ? currentStep.slot ?? currentStep.value : 'content') as keyof StepperSlots<T>)"
         :item="(currentStep as Extract<T, { slot: string }>)"
       >
         {{ currentStep?.content }}
