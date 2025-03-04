a8053be269ab2643ef743dfcccf176055e566767
Sébastien Chopin <atinux@gmail.com>
2025-03-04 16:33:38 +0100

    up

--- docs/app/pages/index.vue
+++ docs/app/pages/index.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
 import { joinURL } from 'ufo'
+// @ts-expect-error yaml is not typed
+import page from '.index.yml'
 
 const { url } = useSiteConfig()
 
-const title = 'The Intuitive Vue UI Library'
-const description = 'Create beautiful, responsive & accessible web apps quickly with Vue or Nuxt. Nuxt UI is an open-source UI library of 50+ customizable components built with Tailwind CSS and Reka UI.'
-
 useSeoMeta({
   titleTemplate: `%s - Nuxt UI`,
-  title,
-  description,
-  ogTitle: `${title} - Nuxt UI`,
-  ogDescription: description,
+  title: page.title,
+  description: page.description,
+  ogTitle: `${page.title} - Nuxt UI`,
+  ogDescription: page.description,
   ogImage: joinURL(url, '/og-image.png')
 })
 
@@ -33,25 +32,18 @@ const { data: components } = await useAsyncData('ui-components', () => {
       :ui="{ container: 'lg:py-0', description: 'text-balance' }"
     >
       <template #title>
-        The Intuitive <br class="hidden md:block"><span class="text-(--ui-primary)">Vue UI Library</span>
+        <MDC :value="page.hero.title" unwrap="p" />
       </template>
       <template #description>
-        Create beautiful, responsive & accessible web apps quickly with Vue or Nuxt. Nuxt UI is an open-source UI library of {{ components?.length }} customizable components built with Tailwind CSS and Reka UI.
+        {{ page.hero.description }}
       </template>
       <template #links>
-        <UButton to="/getting-started" size="xl">
-          Get Started
-        </UButton>
-        <UButton to="/components" size="xl" color="neutral" variant="outline" trailing-icon="i-lucide-arrow-right">
-          Explore Components
-        </UButton>
+        <UButton v-for="link of page.hero.links" :key="link.label" v-bind="link" size="xl" />
         <div class="w-full my-6">
           <USeparator class="w-1/2" type="dashed" />
         </div>
         <div class="flex flex-col gap-4">
-          <UPageFeature icon="i-logos-tailwindcss-icon" title="Styled with Tailwind CSS v4" description="Beautifully styled by default, overwrite any style you want." />
-          <UPageFeature icon="i-custom-reka-ui" title="Accessible with Reka UI" description="Robust accessibility out of the box." />
-          <UPageFeature icon="i-logos-typescript-icon" title="Type-safe with TypeScript" description="Auto-complete and type safety for all components." />
+          <UPageFeature v-for="feature of page.hero.features" :key="feature.title" v-bind="feature" />
         </div>
       </template>
       <div class="h-[344px] lg:h-auto lg:relative w-full lg:min-h-[calc(100vh-var(--ui-header-height)-1px)] overflow-hidden">
@@ -59,21 +51,21 @@ const { data: components } = await useAsyncData('ui-components', () => {
           pause-on-hover
           :overlay="false"
           :ui="{
-            root: '[--gap:--spacing(4)] [--duration:40s] border-(--ui-border) absolute w-full left-0 border-y lg:border-x lg:w-1/2 2xl:w-[320px] lg:flex-col',
+            root: '[--gap:--spacing(4)] [--duration:40s] border-(--ui-border-muted) absolute w-full left-0 border-y lg:border-x lg:w-[calc(50%-6px)] 2xl:w-[320px] lg:flex-col',
             content: 'lg:w-auto lg:h-full lg:flex-col lg:animate-[marquee-vertical_var(--duration)_linear_infinite] lg:rtl:animate-[marquee-vertical-rtl_var(--duration)_linear_infinite] lg:h-[fit-content]'
           }"
         >
           <ULink
             v-for="component of components?.slice(0, 10)"
             :key="component.path"
-            class="relative group aspect-video border-(--ui-border) w-[290px] xl:w-[330px] 2xl:w-[320px] 2xl:p-2 2xl:border-y"
+            class="relative group aspect-video border-(--ui-border-muted) w-[290px] xl:w-[330px] 2xl:w-[320px] 2xl:p-2 2xl:border-y"
             :to="component.path"
           >
             <UColorModeImage
 
               :light="`${component.path.replace('/components/', '/components/light/')}.png`"
               :dark="`${component.path.replace('/components/', '/components/dark/')}.png`"
-              class="hover:scale-105 lg:hover:scale-110 transition-transform aspect-video w-full border-x lg:border-x-0 lg:border-y border-(--ui-border) 2xl:border-y-0"
+              class="hover:scale-105 lg:hover:scale-110 transition-transform aspect-video w-full border-x lg:border-x-0 lg:border-y border-(--ui-border-muted) 2xl:border-y-0"
             />
             <UBadge color="neutral" variant="outline" size="md" :label="component.title" class="hidden lg:block absolute mx-auto top-4 left-6 xl:left-4 group-hover:opacity-100 opacity-0 transition-opacity duration-300 pointer-events-none" />
           </ULink>
@@ -83,26 +75,62 @@ const { data: components } = await useAsyncData('ui-components', () => {
           reverse
           :overlay="false"
           :ui="{
-            root: '[--gap:--spacing(4)] [--duration:40s] border-(--ui-border) absolute w-full mt-[180px] left-0 border-y lg:mt-auto lg:left-auto lg:border-r lg:border-y-0 2xl:border-x lg:w-1/2 2xl:w-[320px] lg:right-0 lg:flex-col',
+            root: '[--gap:--spacing(4)] [--duration:40s] border-(--ui-border-muted) absolute w-full mt-[180px] left-0 border-y lg:mt-auto lg:left-auto lg:border-y-0 lg:border-x lg:w-[calc(50%-6px)] 2xl:w-[320px] lg:right-0 lg:flex-col',
             content: 'lg:w-auto lg:h-full lg:flex-col lg:animate-[marquee-vertical_var(--duration)_linear_infinite] lg:rtl:animate-[marquee-vertical-rtl_var(--duration)_linear_infinite] lg:h-[fit-content] lg:[animation-direction:reverse]'
           }"
         >
           <ULink
             v-for="component of components?.slice(10, 20)"
             :key="component.path"
-            class="relative group aspect-video border-(--ui-border) w-[290px] xl:w-[330px] 2xl:w-[320px] 2xl:p-2 2xl:border-y"
+            class="relative group aspect-video border-(--ui-border-muted) w-[290px] xl:w-[330px] 2xl:w-[320px] 2xl:p-2 2xl:border-y"
             :to="component.path"
           >
             <UColorModeImage
 
               :light="`${component.path.replace('/components/', '/components/light/')}.png`"
               :dark="`${component.path.replace('/components/', '/components/dark/')}.png`"
-              class="hover:scale-105 lg:hover:scale-110 transition-transform aspect-video w-full border-x lg:border-x-0 lg:border-y border-(--ui-border) 2xl:border-y-0"
+              class="hover:scale-105 lg:hover:scale-110 transition-transform aspect-video w-full border-x lg:border-x-0 lg:border-y border-(--ui-border-muted) 2xl:border-y-0"
             />
             <UBadge color="neutral" variant="outline" size="md" :label="component.title" class="hidden lg:block absolute mx-auto top-4 left-6 xl:left-4 group-hover:opacity-100 opacity-0 transition-opacity duration-300 pointer-events-none" />
           </ULink>
         </UPageMarquee>
       </div>
     </UPageHero>
+    <UPageSection :ui="{ container: 'py-6 sm:py-12 lg:py-12' }">
+      <ul class="grid grid-cols-1 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 gap-y-6 lg:gap-x-8 lg:gap-y-8 xl:gap-y-10">
+        <li
+          v-for="feature in page?.features"
+          :key="feature.title"
+          class="flex items-start gap-x-3 relative group"
+          :class="{ 'opacity-75': feature.soon }"
+        >
+          <NuxtLink v-if="feature.to" :to="feature.to" class="absolute inset-0 z-10" />
+          <div class="relative p-3">
+            <svg class="absolute inset-0" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line x1="6.5" x2="6.5" y2="44" stroke="var(--ui-border)" />
+              <line x1="38.5" x2="38.5" y2="44" stroke="var(--ui-border)" />
+              <line y1="5.5" x2="44" y2="5.5" stroke="var(--ui-border)" />
+              <line y1="37.5" x2="44" y2="37.5" stroke="var(--ui-border)" />
+              <circle cx="6.53613" cy="5.45508" r="1.5" fill="var(--ui-border-accented)" />
+              <circle cx="38.5957" cy="5.45508" r="1.5" fill="var(--ui-border-accented)" />
+              <circle cx="6.53711" cy="37.4551" r="1.5" fill="var(--ui-border-accented)" />
+              <circle cx="38.5957" cy="37.4551" r="1.5" fill="var(--ui-border-accented)" />
+            </svg>
+            <UIcon :name="feature.icon" class="size-5 flex-shrink-0" />
+          </div>
+          <div class="flex flex-col">
+            <h2 class="font-medium text-(--ui-text-highlighted) inline-flex items-center gap-x-1">
+              {{ feature.title }} <UBadge v-if="feature.soon" color="neutral" variant="subtle" size="sm" class="rounded-full">
+                Soon
+              </UBadge>
+              <UIcon v-if="feature.to" name="i-lucide-arrow-right" class="size-4 flex-shrink-0 opacity-0 group-hover:opacity-100 transition-all duration-200 -translate-x-1 group-hover:translate-x-0" />
+            </h2>
+            <p class="text-sm text-(--ui-text-muted)">
+              {{ feature.description }}
+            </p>
+          </div>
+        </li>
+      </ul>
+    </UPageSection>
   </UMain>
 </template>
