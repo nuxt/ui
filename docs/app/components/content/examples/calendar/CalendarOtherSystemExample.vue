22f6e919acdf8bb16b0e4a18552d259854a6b524
Hessam A. Cheraghi <62061093+HessamCheraghi@users.noreply.github.com>
2025-03-04 13:11:00 +0330

    docs(calendar): other calendar systems (#3447)

---
+++ docs/app/components/content/examples/calendar/CalendarOtherSystemExample.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { CalendarDate, HebrewCalendar } from '@internationalized/date'
+
+const hebrewDate = shallowRef(new CalendarDate(new HebrewCalendar(), 5781, 1, 1))
+</script>
+
+<template>
+  <UCalendar v-model="hebrewDate" />
+</template>
