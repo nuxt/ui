d38db811c3a421de6eefd04565ac866c2d655ddc
Emils Gulbis <emils.gulbis@gmail.com>
2025-03-07 11:53:54 +0200

    feat(SelectMenu): snapshot update and select-menu theme file update

--- src/theme/select-menu.ts
+++ src/theme/select-menu.ts
@@ -5,27 +5,9 @@ import select from './select'
 export default (options: Required<ModuleOptions>) => {
   return defu({
     slots: {
-      input: 'border-b border-[var(--ui-border)]',
+      input: 'border-b border-(--ui-border)',
+      focusScope: 'flex flex-col min-h-0',
       clearIcon: 'shrink-0 text-[var(--ui-text-dimmed)]'
-    },
-    variants: {
-      size: {
-        xs: {
-          clearIcon: 'size-3'
-        },
-        sm: {
-          clearIcon: 'size-3.5'
-        },
-        md: {
-          clearIcon: 'size-4'
-        },
-        lg: {
-          clearIcon: 'size-4'
-        },
-        xl: {
-          clearIcon: 'size-4.5'
-        }
-      }
     }
   }, select(options))
 }
