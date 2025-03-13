aebdde3ea0b0099ed989423a4fb4fc29168630b2
Mickael Chanrion <michkkael@gmail.com>
2025-03-13 09:45:59 +0100

    feat(locale): add Catalan language

---
+++ src/runtime/locale/ca.ts
@@ -0,0 +1,56 @@
+import type { Messages } from '../types'
+import { defineLocale } from '../composables/defineLocale'
+
+export default defineLocale<Messages>({
+  name: 'Català',
+  code: 'ca',
+  messages: {
+    inputMenu: {
+      noMatch: 'No hi ha dades coincidents',
+      noData: 'Sense dades',
+      create: 'Crear "{label}"'
+    },
+    calendar: {
+      prevYear: 'Any anterior',
+      nextYear: 'Any següent',
+      prevMonth: 'Mes anterior',
+      nextMonth: 'Mes següent'
+    },
+    inputNumber: {
+      increment: 'Incrementar',
+      decrement: 'Decrementar'
+    },
+    commandPalette: {
+      placeholder: 'Escriu una ordre o cerca...',
+      noMatch: 'No hi ha dades coincidents',
+      noData: 'Sense dades',
+      close: 'Tancar'
+    },
+    selectMenu: {
+      noMatch: 'No hi ha dades coincidents',
+      noData: 'Sense dades',
+      create: 'Crear "{label}"',
+      search: 'Cerca...'
+    },
+    toast: {
+      close: 'Tancar'
+    },
+    carousel: {
+      prev: 'Anterior',
+      next: 'Següent',
+      goto: 'Anar a la diapositiva {slide}'
+    },
+    modal: {
+      close: 'Tancar'
+    },
+    slideover: {
+      close: 'Tancar'
+    },
+    alert: {
+      close: 'Tancar'
+    },
+    table: {
+      noData: 'Sense dades'
+    }
+  }
+})
--- src/runtime/locale/index.ts
+++ src/runtime/locale/index.ts
@@ -1,6 +1,7 @@
 export { default as ar } from './ar'
 export { default as az } from './az'
 export { default as bn } from './bn'
+export { default as ca } from './ca'
 export { default as cs } from './cs'
 export { default as da } from './da'
 export { default as de } from './de'
