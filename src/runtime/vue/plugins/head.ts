fa6a749cb5b38b62d45db904055d61c94fbc00f4
harlan <harlan@harlanzw.com>
2025-03-18 00:14:42 +1100

    fix: avoid replacing unhead instance

--- src/runtime/vue/plugins/head.ts
+++ src/runtime/vue/plugins/head.ts
@@ -3,6 +3,11 @@ import type { Plugin } from 'vue'
 
 export default {
   install(app) {
+    // check for existing head instance to avoid replacement
+    // bit hacky but we can't use injectHead() here
+    if (app._context.provides.usehead) {
+      return
+    }
     const head = createHead()
     app.use(head)
   }
