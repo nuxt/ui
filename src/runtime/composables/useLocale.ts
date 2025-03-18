e0f51837d08f222590857bf40dec77e1ff0040c1
Benjamin Canac <canacb1@gmail.com>
2025-03-18 11:59:04 +0100

    fix(useLocale): use `Symbol.for` for uniqueness

--- src/runtime/composables/useLocale.ts
+++ src/runtime/composables/useLocale.ts
@@ -5,7 +5,7 @@ import type { Locale, Messages } from '../types/locale'
 import { buildLocaleContext } from '../utils/locale'
 import en from '../locale/en'
 
-export const localeContextInjectionKey: InjectionKey<Ref<Locale<unknown> | undefined>> = Symbol('nuxt-ui.locale-context')
+export const localeContextInjectionKey: InjectionKey<Ref<Locale<unknown> | undefined>> = Symbol.for('nuxt-ui.locale-context')
 
 const _useLocale = (localeOverrides?: Ref<Locale<Messages> | undefined>) => {
   const locale = localeOverrides || toRef(inject<Locale<Messages>>(localeContextInjectionKey))
