8d63be1e319d424c89582c45ca67d70adc8ee15c
Romain Hamel <rom.hml@gmail.com>
2025-03-19 12:17:52 +0100

    chore(Form): improve TSDoc (#3619)

--- src/runtime/components/FormField.vue
+++ src/runtime/components/FormField.vue
@@ -31,7 +31,12 @@ export interface FormFieldProps {
    */
   size?: FormFieldVariants['size']
   required?: boolean
+  /** If true, validation on input will be active immediately instead of waiting for a blur event. */
   eagerValidation?: boolean
+  /**
+   * Delay in milliseconds before validating the form on input events.
+   * @defaultValue `300`
+   */
   validateOnInputDelay?: number
   class?: any
   ui?: Partial<typeof formField.slots>
