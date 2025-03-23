2e730470c2c10ad40c3ebb3c4fcd6e5e5d3fb6c7
Benjamin Canac <canacb1@gmail.com>
2025-03-23 14:53:58 +0100

    fix(defineLocale/defineShortcuts): remove `@__NO_SIDE_EFFECTS__`

--- src/runtime/composables/defineLocale.ts
+++ src/runtime/composables/defineLocale.ts
@@ -8,7 +8,6 @@ interface DefineLocaleOptions<M> {
   messages: M
 }
 
-/* @__NO_SIDE_EFFECTS__ */
 export function defineLocale<M>(options: DefineLocaleOptions<M>): Locale<M> {
   return defu<DefineLocaleOptions<M>, [{ dir: Direction }]>(options, { dir: 'ltr' })
 }
