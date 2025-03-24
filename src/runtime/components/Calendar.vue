25449f278c7f318db4ad5636e3710068b26f5485
HugoRCD <hugo.richard@epitech.eu>
2025-03-24 18:33:13 +0100

    up

--- src/runtime/components/Calendar.vue
+++ src/runtime/components/Calendar.vue
@@ -35,7 +35,7 @@ export interface CalendarProps<R extends boolean, M extends boolean> extends Omi
   nextYearIcon?: string
   /**
    * Configure the next year button.
-   * @defaultValue { size: 'md', color: 'neutral', variant: 'ghost' }
+   * `{ size: 'md', color: 'neutral', variant: 'ghost' }`{lang="ts-type"}
    */
   nextYear?: ButtonProps
   /**
@@ -46,7 +46,7 @@ export interface CalendarProps<R extends boolean, M extends boolean> extends Omi
   nextMonthIcon?: string
   /**
    * Configure the next month button.
-   * @defaultValue { size: 'md', color: 'neutral', variant: 'ghost' }
+   * `{ size: 'md', color: 'neutral', variant: 'ghost' }`{lang="ts-type"}
    */
   nextMonth?: ButtonProps
   /**
@@ -57,7 +57,7 @@ export interface CalendarProps<R extends boolean, M extends boolean> extends Omi
   prevYearIcon?: string
   /**
    * Configure the prev year button.
-   * @defaultValue { size: 'md', color: 'neutral', variant: 'ghost' }
+   * `{ size: 'md', color: 'neutral', variant: 'ghost' }`{lang="ts-type"}
    */
   prevYear?: ButtonProps
   /**
@@ -68,7 +68,7 @@ export interface CalendarProps<R extends boolean, M extends boolean> extends Omi
   prevMonthIcon?: string
   /**
    * Configure the prev month button.
-   * @defaultValue { size: 'md', color: 'neutral', variant: 'ghost' }
+   * `{ size: 'md', color: 'neutral', variant: 'ghost' }`{lang="ts-type"}
    */
   prevMonth?: ButtonProps
   /**
