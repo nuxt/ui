8f2e4ef9159d1dd3402d990ad279210f04588623
HugoRCD <hugo.richard@epitech.eu>
2025-03-25 10:23:20 +0100

    up

--- docs/app/components/content/examples/table/TableColumnPinningExample.vue
+++ docs/app/components/content/examples/table/TableColumnPinningExample.vue
@@ -15,31 +15,31 @@ type Payment = {
 }
 
 const data = ref<Payment[]>([{
-  id: '46000000000000000000000000000000000000000',
+  id: '4600',
   date: '2024-03-11T15:30:00',
   status: 'paid',
   email: 'james.anderson@example.com',
   amount: 594000
 }, {
-  id: '45990000000000000000000000000000000000000',
+  id: '4599',
   date: '2024-03-11T10:10:00',
   status: 'failed',
   email: 'mia.white@example.com',
   amount: 276000
 }, {
-  id: '45980000000000000000000000000000000000000',
+  id: '4598',
   date: '2024-03-11T08:50:00',
   status: 'refunded',
   email: 'william.brown@example.com',
   amount: 315000
 }, {
-  id: '45970000000000000000000000000000000000000',
+  id: '4597',
   date: '2024-03-10T19:45:00',
   status: 'paid',
   email: 'emma.davis@example.com',
   amount: 5290000
 }, {
-  id: '45960000000000000000000000000000000000000',
+  id: '4596',
   date: '2024-03-10T15:55:00',
   status: 'paid',
   email: 'ethan.harris@example.com',
--- docs/app/pages/index.vue
+++ docs/app/pages/index.vue
@@ -106,6 +106,9 @@ useIntersectionObserver(contributorsRef, ([entry]) => {
               :light="`${component.path.replace('/components/', '/components/light/')}.png`"
               :dark="`${component.path.replace('/components/', '/components/dark/')}.png`"
               :alt="`${component.title} preview`"
+              width="290"
+              height="163"
+              format="webp"
               class="hover:scale-105 lg:hover:scale-110 transition-transform aspect-video w-full border-x lg:border-x-0 lg:border-y border-(--ui-border) 2xl:border-y-0"
               loading="lazy"
             />
@@ -223,26 +226,32 @@ useIntersectionObserver(contributorsRef, ([entry]) => {
       class="border-b border-(--ui-border)"
     >
       <template #features>
-        <NuxtLink to="https://npm.chart.dev/@nuxt/ui" target="_blank" class="min-w-0">
-          <p class="text-4xl font-semibold text-(--ui-text-highlighted) truncate">
-            {{ format(module?.stats?.downloads ?? 0) }}+
-          </p>
-          <p class="text-(--ui-text-muted) text-sm truncate">monthly downloads</p>
-        </NuxtLink>
+        <li>
+          <NuxtLink to="https://npm.chart.dev/@nuxt/ui" target="_blank" class="min-w-0">
+            <p class="text-4xl font-semibold text-(--ui-text-highlighted) truncate">
+              {{ format(module?.stats?.downloads ?? 0) }}+
+            </p>
+            <p class="text-(--ui-text-muted) text-sm truncate">monthly downloads</p>
+          </NuxtLink>
+        </li>
 
-        <NuxtLink to="https://github.com/nuxt/ui" target="_blank" class="min-w-0">
-          <p class="text-4xl font-semibold text-(--ui-text-highlighted) truncate">
-            {{ format(module?.stats?.stars ?? 0) }}+
-          </p>
-          <p class="text-(--ui-text-muted) text-sm truncate">GitHub stars</p>
-        </NuxtLink>
+        <li>
+          <NuxtLink to="https://github.com/nuxt/ui" target="_blank" class="min-w-0">
+            <p class="text-4xl font-semibold text-(--ui-text-highlighted) truncate">
+              {{ format(module?.stats?.stars ?? 0) }}+
+            </p>
+            <p class="text-(--ui-text-muted) text-sm truncate">GitHub stars</p>
+          </NuxtLink>
+        </li>
 
-        <NuxtLink to="https://github.com/nuxt/ui/graphs/contributors" target="_blank" class="min-w-0">
-          <p class="text-4xl font-semibold text-(--ui-text-highlighted) truncate">
-            175+
-          </p>
-          <p class="text-(--ui-text-muted) text-sm truncate">Contributors</p>
-        </NuxtLink>
+        <li>
+          <NuxtLink to="https://github.com/nuxt/ui/graphs/contributors" target="_blank" class="min-w-0">
+            <p class="text-4xl font-semibold text-(--ui-text-highlighted) truncate">
+              175+
+            </p>
+            <p class="text-(--ui-text-muted) text-sm truncate">Contributors</p>
+          </NuxtLink>
+        </li>
       </template>
 
       <div ref="contributorsRef" class="p-4 sm:px-6 md:px-8 lg:px-12 xl:px-14 overflow-hidden flex relative">
