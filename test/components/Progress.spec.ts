f7f0521834b4c20e4683018810569c5c5386e95a
Charfeddine Hamrouni <charfeddine@MacBook-Pro-de-Charfeddine.local>
2025-03-29 15:22:33 +0100

    fix(Progress): add class in tests

--- test/components/Progress.spec.ts
+++ test/components/Progress.spec.ts
@@ -7,6 +7,7 @@ describe('Progress', () => {
   const sizes = Object.keys(theme.variants.size) as any
   const orientations = Object.keys(theme.variants.orientation) as any
   const animations = Object.keys(theme.variants.animation) as any
+  const variants = Object.keys(theme.variants.variant) as any
   const max = ['Waiting...', 'Cloning...', 'Migrating...', 'Deploying...', 'Done!']
 
   it.each([
@@ -14,11 +15,13 @@ describe('Progress', () => {
     ['with modelValue', { props: { modelValue: 50 } }],
     ['with status', { props: { modelValue: 50, status: true } }],
     ['with status inverted', { props: { modelValue: 50, status: true, inverted: true } }],
+    ['with status position and circular variant', { props: { modelValue: 50, status: true, variant: 'circular' } }],
     ['with max', { props: { modelValue: 2, status: true, max } }],
     ['with max inverted', { props: { modelValue: 2, status: true, inverted: true, max } }],
     ...sizes.map((size: string) => [`with size ${size}`, { props: { size } }]),
     ...orientations.map((orientation: string) => [`with orientation ${orientation}`, { props: { orientation } }]),
     ...animations.map((animation: string) => [`with animation ${animation}`, { props: { animation } }]),
+    ...variants.map((variant: string) => [`with variant ${variant}`, { props: { variant } }]),
     ['with color neutral', { props: { color: 'neutral', modelValue: 50 } }],
     ['with as', { props: { as: 'section' } }],
     ['with class', { props: { class: 'w-48' } }],
