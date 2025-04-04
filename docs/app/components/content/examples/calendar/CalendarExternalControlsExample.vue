2ad46de00120dd5fd722cb369ef07bdfbdec5136
Evan Schleret <evan@schleret.ch>
2025-04-04 16:51:42 +0200

    docs(#3764): add the  in Calendar page

---
+++ docs/app/components/content/examples/calendar/CalendarExternalControlsExample.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { CalendarDate } from '@internationalized/date'
+
+const date = ref(new CalendarDate(2025, 4, 2))
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-8">
+    <div class="flex gap-x-4">
+      <UButton @click="() => date = date.subtract({ months: 1 })">
+        previous
+      </UButton>
+
+      <UButton @click="() => date = date.add({ months: 1 })">
+        next
+      </UButton>
+    </div>
+
+    <UCalendar v-model="date" :month-controls="false" :year-controls="false" />
+  </div>
+</template>
