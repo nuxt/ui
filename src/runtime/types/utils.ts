421e692f2f6e5d7d1b9a6e6f84a53e54cb0a0c27
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-04-11 00:51:37 +0200

    type: fix dynamic slots fallback

--- src/runtime/types/utils.ts
+++ src/runtime/types/utils.ts
@@ -21,12 +21,14 @@ export type DynamicSlots<
   D extends object = {}
 > = {
   [
-  K in T['slot'] as K extends string
+  K in NonNullable<T['slot']> as K extends string
     ? S extends string
       ? (K | `${K}-${S}`)
       : K
     : never
   ]?: (props: { item: Extract<T, { slot: K extends `${infer Base}-${S}` ? Base : K }> } & D) => any
+} & {
+  [key: string]: (props: { item: T } & D) => any
 }
 
 export type GetObjectField<MaybeObject, Key extends string> = MaybeObject extends Record<string, any>
