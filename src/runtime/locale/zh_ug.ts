383917fd32fbca3dc3478e84cf9f0000f14f0693
ElshadBeg <elshad@126.com>
2025-04-14 01:51:17 +0800

    feat(locale): add Uyghur language

---
+++ src/runtime/locale/zh_ug.ts
@@ -0,0 +1,57 @@
+import type { Messages } from '../types'
+import { defineLocale } from '../composables/defineLocale'
+
+export default defineLocale<Messages>({
+  name: 'ئۇيغۇرچە',
+  code: 'zh-UG',
+  dir: 'rtl',
+  messages: {
+    inputMenu: {
+      noMatch: 'ماس كېلىدىغان سانلىق مەلۇمات يوق',
+      noData: 'سانلىق مەلۇمات يوق',
+      create: '"{label}" نى قۇرۇش'
+    },
+    calendar: {
+      prevYear: 'ئالدىنقى يىل',
+      nextYear: 'كېلەر يىل',
+      prevMonth: 'ئالدىنقى ئاي',
+      nextMonth: 'كېلەر ئاي'
+    },
+    inputNumber: {
+      increment: 'كۆپەيتىش',
+      decrement: 'ئازايتىش'
+    },
+    commandPalette: {
+      placeholder: 'بۇيرۇق كىرگۈزۈڭ ياكى ئىزدەڭ...',
+      noMatch: 'ماس كېلىدىغان سانلىق مەلۇمات يوق',
+      noData: 'سانلىق مەلۇمات يوق',
+      close: 'تاقاش'
+    },
+    selectMenu: {
+      noMatch: 'ماس كېلىدىغان سانلىق مەلۇمات يوق',
+      noData: 'سانلىق مەلۇمات يوق',
+      create: '"{label}" نى قۇرۇش',
+      search: 'ئىزدەش...'
+    },
+    toast: {
+      close: 'تاقاش'
+    },
+    carousel: {
+      prev: 'ئالدىنقى بەت',
+      next: 'كېيىنكى بەت',
+      goto: '{slide}-بەتكە ئاتلاش'
+    },
+    modal: {
+      close: 'تاقاش'
+    },
+    slideover: {
+      close: 'تاقاش'
+    },
+    alert: {
+      close: 'تاقاش'
+    },
+    table: {
+      noData: 'سانلىق مەلۇمات يوق'
+    }
+  }
+})
