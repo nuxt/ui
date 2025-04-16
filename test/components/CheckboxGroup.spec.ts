27c3046b2409d4e5498e06d5635812ce23903279
rich-tribeweb <rich@tribeweb.co.uk>
2025-04-16 01:58:23 +0100

    chore: fixed my initial mistakes in test file - still failing 4 tests

--- test/components/CheckboxGroup.spec.ts
+++ test/components/CheckboxGroup.spec.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, test } from 'vitest'
 import CheckboxGroup, { type CheckboxGroupProps, type CheckboxGroupSlots } from '../../src/runtime/components/CheckboxGroup.vue'
 import ComponentRender from '../component-render'
-import theme from '#build/ui/radio-group'
+import theme from '#build/ui/checkbox-group'
 import { flushPromises, mount } from '@vue/test-utils'
 import { renderForm } from '../utils/form'
 import type { FormInputEvents } from '~/src/module'
@@ -21,8 +21,8 @@ describe('CheckboxGroup', () => {
 
   it.each([
     ['with items', { props }],
-    ['with modelValue', { props: { ...props, modelValue: '["1"]' } }],
-    ['with defaultValue', { props: { ...props, defaultValue: '["1"]' } }],
+    ['with modelValue', { props: { ...props, modelValue: ['1'] } }],
+    ['with defaultValue', { props: { ...props, defaultValue: ['1'] } }],
     ['with valueKey', { props: { ...props, valueKey: 'label' } }],
     ['with labelKey', { props: { ...props, labelKey: 'value' } }],
     ['with descriptionKey', { props: { ...props, descriptionKey: 'value' } }],
@@ -30,9 +30,9 @@ describe('CheckboxGroup', () => {
     ['with description', { props: { items: items.map((opt, count) => ({ ...opt, description: `Description ${count}` })) } }],
     ['with required', { props: { ...props, legend: 'Legend', required: true } }],
     ...sizes.map((size: string) => [`with size ${size}`, { props: { ...props, size } }]),
-    ...variants.map((variant: string) => [`with primary variant ${variant}`, { props: { ...props, variant, defaultValue: '["1"]' } }]),
-    ...variants.map((variant: string) => [`with neutral variant ${variant}`, { props: { ...props, variant, color: 'neutral', defaultValue: '["1"]' } }]),
-    ...variants.map((variant: string) => [`with horizontal variant ${variant}`, { props: { ...props, variant, orientation: 'horizontal', defaultValue: '["1"]' } }]),
+    ...variants.map((variant: string) => [`with primary variant ${variant}`, { props: { ...props, variant, defaultValue: ['1'] } }]),
+    ...variants.map((variant: string) => [`with neutral variant ${variant}`, { props: { ...props, variant, color: 'neutral', defaultValue: ['1'] } }]),
+    ...variants.map((variant: string) => [`with horizontal variant ${variant}`, { props: { ...props, variant, orientation: 'horizontal', defaultValue: ['1'] } }]),
     ...indicators.map((indicator: string) => [`with indicator ${indicator}`, { props: { ...props, indicator } }]),
     ['with ariaLabel', { props, attrs: { 'aria-label': 'Aria label' } }],
     ['with as', { props: { ...props, as: 'section' } }],
@@ -42,26 +42,23 @@ describe('CheckboxGroup', () => {
     ['with legend slot', { props, slots: { label: () => 'Legend slot' } }],
     ['with label slot', { props, slots: { label: () => 'Label slot' } }],
     ['with description slot', { props, slots: { label: () => 'Description slot' } }]
-  ])('renders %s correctly', async (nameOrHtml: string, options: { props?: RadioGroupProps, slots?: Partial<RadioGroupSlots> }) => {
-    const html = await ComponentRender(nameOrHtml, options, RadioGroup)
+  ])('renders %s correctly', async (nameOrHtml: string, options: { props?: CheckboxGroupProps, slots?: Partial<CheckboxGroupSlots> }) => {
+    const html = await ComponentRender(nameOrHtml, options, CheckboxGroup)
     expect(html).toMatchSnapshot()
   })
 
   describe('emits', () => {
     test('update:modelValue event', async () => {
-      const wrapper = mount(RadioGroup, { props: { items: ['Option 1', 'Option 2'] } })
-      const input = wrapper.findComponent({ name: 'RadioGroupRoot' })
-      await input.setValue('Option 1')
-
+      const wrapper = mount(CheckboxGroup, { props: { items: ['Option 1', 'Option 2'] } })
+      const input = wrapper.findComponent({ name: 'CheckboxGroupRoot' })
+      await input.setValue(['Option 1'])
       expect(wrapper.emitted()).toMatchObject({ 'update:modelValue': [['Option 1']] })
     })
 
     test('change event', async () => {
-      const wrapper = mount(RadioGroup, { props: { items: ['Option 1', 'Option 2'] } })
-
-      const input = wrapper.findComponent({ name: 'RadioGroupRoot' })
-      await input.setValue('Option 1')
-
+      const wrapper = mount(CheckboxGroup, { props: { items: ['Option 1', 'Option 2'] } })
+      const input = wrapper.findComponent({ name: 'CheckboxGroupRoot' })
+      await input.setValue(['Option 1'])
       expect(wrapper.emitted()).toMatchObject({ change: [[{ type: 'change' }]] })
     })
   })
@@ -82,12 +79,12 @@ describe('CheckboxGroup', () => {
           items: ['Option 1', 'Option 2']
         },
         slotTemplate: `
-        <UFormField name="value" label="Radio group">
-          <URadioGroup id="input" v-model="state.value" :items="items" />
+        <UFormField name="value" label="Checkbox group">
+          <UCheckboxGroup id="input" v-model="state.value" :items="items" />
         </UFormField>
         `
       })
-      const input = wrapper.findComponent({ name: 'RadioGroupRoot' })
+      const input = wrapper.findComponent({ name: 'CheckboxGroupRoot' })
       return {
         wrapper,
         input
@@ -97,11 +94,11 @@ describe('CheckboxGroup', () => {
     test('validate on change works', async () => {
       const { input, wrapper } = await createForm(['change'])
 
-      input.setValue('Option 1')
+      input.setValue(['Option 1'])
       await flushPromises()
       expect(wrapper.text()).toContain('Error message')
 
-      input.setValue('Option 2')
+      input.setValue(['Option 2'])
       await flushPromises()
       expect(wrapper.text()).not.toContain('Error message')
     })
@@ -109,11 +106,11 @@ describe('CheckboxGroup', () => {
     test('validate on input works', async () => {
       const { input, wrapper } = await createForm(['input'])
 
-      input.setValue('Option 1')
+      input.setValue(['Option 1'])
       await flushPromises()
       expect(wrapper.text()).toContain('Error message')
 
-      input.setValue('Option 2')
+      input.setValue(['Option 2'])
       await flushPromises()
       expect(wrapper.text()).not.toContain('Error message')
     })
