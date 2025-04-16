e4d871987449683e60e6097a4400182287673a5f
rich-tribeweb <rich@tribeweb.co.uk>
2025-04-16 01:49:55 +0100

    feat: share and reuse styling between both components

--- src/theme/checkbox-group.ts
+++ src/theme/checkbox-group.ts
@@ -1,176 +1,68 @@
+import { defu } from 'defu'
 import type { ModuleOptions } from '../module'
+import checkbox from './checkbox'
 
-export default (options: Required<ModuleOptions>) => ({
-  slots: {
-    root: 'relative',
-    fieldset: 'flex',
-    legend: 'mb-1 block font-medium text-(--ui-text)',
-    item: 'flex items-start',
-    icon: 'shrink-0 size-full',
-    base: 'shrink-0 flex items-center justify-center rounded-(--ui-radius) text-(--ui-bg) ring ring-inset ring-(--ui-border-accented) focus-visible:outline-2 focus-visible:outline-offset-2',
-    indicator: 'flex items-center justify-center size-full rounded-(--ui-radius) after:bg-(--ui-bg)',
-    container: 'flex items-center',
-    wrapper: 'w-full',
-    label: 'block font-medium text-(--ui-text)',
-    description: 'text-(--ui-text-muted) text-wrap'
-  },
-  variants: {
-    color: {
-      ...Object.fromEntries((options.theme.colors || []).map((color: string) => [color, {
-        base: `focus-visible:outline-(--ui-${color})`,
-        indicator: `bg-(--ui-${color})`
-      }])),
-      neutral: {
-        base: 'focus-visible:outline-(--ui-border-inverted)',
-        indicator: 'bg-(--ui-bg-inverted)'
-      }
+export default (options: Required<ModuleOptions>) => {
+  return defu({
+    slots: {
+      root: 'relative',
+      fieldset: 'relative flex',
+      legend: 'mb-1 block font-medium text-(--ui-text)',
+      wrapper: 'w-full'
     },
-    variant: {
-      list: {
+    variants: {
+      orientation: {
+        horizontal: {
+          fieldset: 'flex-row',
+          wrapper: 'me-2'
+        },
+        vertical: {
+          fieldset: 'flex-col'
+        }
       },
-      card: {
-        item: 'items-center border border-(--ui-border-muted) rounded-lg'
+      size: {
+        xs: {
+          fieldset: 'gap-0.5',
+          legend: 'text-xs'
+        },
+        sm: {
+          fieldset: 'gap-0.5',
+          legend: 'text-xs'
+        },
+        md: {
+          fieldset: 'gap-1',
+          legend: 'text-sm'
+        },
+        lg: {
+          fieldset: 'gap-1',
+          legend: 'text-sm'
+        },
+        xl: {
+          fieldset: 'gap-1.5',
+          legend: 'text-base'
+        }
       },
-      table: {
-        item: 'border border-(--ui-border-muted)'
+      required: {
+        true: {
+          legend: 'after:content-[\'*\'] after:ms-0.5 after:text-(--ui-error)'
+        }
       }
     },
-    orientation: {
-      horizontal: {
-        fieldset: 'flex-row',
-        wrapper: 'me-2'
+    compoundVariants: [
+      {
+        orientation: 'horizontal',
+        variant: 'table',
+        class: {
+          fieldset: 'gap-0 -space-x-px'
+        }
       },
-      vertical: {
-        fieldset: 'flex-col'
+      {
+        orientation: 'vertical',
+        variant: 'table',
+        class: {
+          fieldset: 'gap-0 -space-y-px'
+        }
       }
-    },
-    indicator: {
-      start: {
-        item: 'flex-row',
-        base: 'me-2'
-      },
-      end: {
-        item: 'flex-row-reverse',
-        base: 'ms-2'
-      },
-      hidden: {
-        base: 'sr-only',
-        wrapper: 'text-center'
-      }
-    },
-    size: {
-      xs: {
-        fieldset: 'gap-0.5',
-        legend: 'text-xs',
-        base: 'size-3',
-        item: 'text-xs',
-        container: 'h-4',
-        indicator: 'after:size-1'
-      },
-      sm: {
-        fieldset: 'gap-0.5',
-        legend: 'text-xs',
-        base: 'size-3.5',
-        item: 'text-xs',
-        container: 'h-4',
-        indicator: 'after:size-1'
-      },
-      md: {
-        fieldset: 'gap-1',
-        legend: 'text-sm',
-        base: 'size-4',
-        item: 'text-sm',
-        container: 'h-5',
-        indicator: 'after:size-1.5'
-      },
-      lg: {
-        fieldset: 'gap-1',
-        legend: 'text-sm',
-        base: 'size-4.5',
-        item: 'text-sm',
-        container: 'h-5',
-        indicator: 'after:size-1.5'
-      },
-      xl: {
-        fieldset: 'gap-1.5',
-        legend: 'text-base',
-        base: 'size-5',
-        item: 'text-base',
-        container: 'h-6',
-        indicator: 'after:size-2'
-      }
-    },
-    disabled: {
-      true: {
-        base: 'cursor-not-allowed opacity-75',
-        label: 'cursor-not-allowed opacity-75'
-      }
-    },
-    required: {
-      true: {
-        legend: 'after:content-[\'*\'] after:ms-0.5 after:text-(--ui-error)'
-      }
-    },
-    checked: {
-      true: ''
-    }
-  },
-  compoundVariants: [
-    { size: 'xs', variant: ['card', 'table'], class: { item: 'p-2.5' } },
-    { size: 'sm', variant: ['card', 'table'], class: { item: 'p-3' } },
-    { size: 'md', variant: ['card', 'table'], class: { item: 'p-3.5' } },
-    { size: 'lg', variant: ['card', 'table'], class: { item: 'p-4' } },
-    { size: 'xl', variant: ['card', 'table'], class: { item: 'p-4.5' } },
-    {
-      orientation: 'horizontal',
-      variant: 'table',
-      class: {
-        item: 'first-of-type:rounded-l-lg last-of-type:rounded-r-lg',
-        fieldset: 'gap-0 -space-x-px'
-      }
-    },
-    {
-      orientation: 'vertical',
-      variant: 'table',
-      class: {
-        item: 'first-of-type:rounded-t-lg last-of-type:rounded-b-lg',
-        fieldset: 'gap-0 -space-y-px'
-      }
-    },
-    ...(options.theme.colors || []).map((color: string) => ({
-      color,
-      variant: 'card',
-      class: {
-        item: `has-data-[state=checked]:border-(--ui-${color})`
-      }
-    })),
-    {
-      color: 'neutral',
-      variant: 'card',
-      class: {
-        item: 'has-data-[state=checked]:border-(--ui-border-elevated)'
-      }
-    },
-    ...(options.theme.colors || []).map((color: string) => ({
-      color,
-      variant: 'table',
-      class: {
-        item: `has-data-[state=checked]:bg-(--ui-${color})/10 has-data-[state=checked]:border-(--ui-${color})/50 has-data-[state=checked]:z-[1]`
-      }
-    })),
-    {
-      color: 'neutral',
-      variant: 'table',
-      class: {
-        item: 'has-data-[state=checked]:bg-(--ui-bg-elevated) has-data-[state=checked]:border-(--ui-border-inverted)/25 has-data-[state=checked]:z-[1]'
-      }
-    }
-  ],
-  defaultVariants: {
-    size: 'md',
-    color: 'primary',
-    variant: 'list',
-    orientation: 'vertical',
-    indicator: 'start'
-  }
-})
+    ]
+  }, checkbox(options))
+}
--- src/theme/checkbox.ts
+++ src/theme/checkbox.ts
@@ -2,11 +2,12 @@ import type { ModuleOptions } from '../module'
 
 export default (options: Required<ModuleOptions>) => ({
   slots: {
-    root: 'relative flex items-start',
-    base: 'shrink-0 flex items-center justify-center rounded-(--ui-radius) text-(--ui-bg) ring ring-inset ring-(--ui-border-accented) focus-visible:outline-2 focus-visible:outline-offset-2',
+    item: 'flex items-start',
     container: 'flex items-center',
-    wrapper: 'ms-2',
+    base: 'shrink-0 flex items-center justify-center rounded-(--ui-radius) text-(--ui-bg) ring ring-inset ring-(--ui-border-accented) focus-visible:outline-2 focus-visible:outline-offset-2',
+    indicator: 'flex items-center justify-center size-full rounded-(--ui-radius) after:bg-(--ui-bg)',
     icon: 'shrink-0 size-full',
+    wrapper: 'ms-2 w-full',
     label: 'block font-medium text-(--ui-text)',
     description: 'text-(--ui-text-muted)'
   },
@@ -15,33 +16,70 @@ export default (options: Required<ModuleOptions>) => ({
       ...Object.fromEntries((options.theme.colors || []).map((color: string) => [color, `focus-visible:outline-(--ui-${color})`])),
       neutral: 'focus-visible:outline-(--ui-border-inverted)'
     },
+    variant: {
+      list: {
+      },
+      card: {
+        item: 'items-center border border-(--ui-border-muted) rounded-lg'
+      },
+      table: {
+        item: 'border border-(--ui-border-muted)'
+      }
+    },
+    orientation: {
+      horizontal: {
+        wrapper: 'me-2'
+      },
+      vertical: {
+      }
+    },
+    indicator: {
+      start: {
+        item: 'flex-row',
+        base: 'me-2'
+      },
+      end: {
+        item: 'flex-row-reverse',
+        base: 'ms-2'
+      },
+      hidden: {
+        base: 'sr-only',
+        wrapper: 'text-center'
+      }
+    },
     size: {
       xs: {
         base: 'size-3',
+        item: 'text-xs',
         container: 'h-4',
-        wrapper: 'text-xs'
+        indicator: 'after:size-1'
       },
       sm: {
         base: 'size-3.5',
+        item: 'text-xs',
         container: 'h-4',
-        wrapper: 'text-xs'
+        indicator: 'after:size-1'
       },
       md: {
         base: 'size-4',
+        item: 'text-sm',
         container: 'h-5',
-        wrapper: 'text-sm'
+        indicator: 'after:size-1.5'
       },
       lg: {
         base: 'size-4.5',
+        item: 'text-sm',
         container: 'h-5',
-        wrapper: 'text-sm'
+        indicator: 'after:size-1.5'
       },
       xl: {
         base: 'size-5',
+        item: 'text-base',
         container: 'h-6',
-        wrapper: 'text-base'
+        indicator: 'after:size-2'
       }
     },
+
     required: {
       true: {
         label: 'after:content-[\'*\'] after:ms-0.5 after:text-(--ui-error)'
@@ -58,17 +96,69 @@ export default (options: Required<ModuleOptions>) => ({
       true: ''
     }
   },
-  compoundVariants: [...(options.theme.colors || []).map((color: string) => ({
-    color,
-    checked: true,
-    class: `ring-2 ring-(--ui-${color}) bg-(--ui-${color})`
-  })), {
-    color: 'neutral',
-    checked: true,
-    class: 'ring-2 ring-(--ui-border-inverted) bg-(--ui-bg-inverted)'
-  }],
+
+  compoundVariants: [
+    { size: 'xs', variant: ['card', 'table'], class: { item: 'p-2.5' } },
+    { size: 'sm', variant: ['card', 'table'], class: { item: 'p-3' } },
+    { size: 'md', variant: ['card', 'table'], class: { item: 'p-3.5' } },
+    { size: 'lg', variant: ['card', 'table'], class: { item: 'p-4' } },
+    { size: 'xl', variant: ['card', 'table'], class: { item: 'p-4.5' } },
+    {
+      orientation: 'horizontal',
+      variant: 'table',
+      class: {
+        item: 'first-of-type:rounded-l-lg last-of-type:rounded-r-lg'
+      }
+    },
+    {
+      orientation: 'vertical',
+      variant: 'table',
+      class: {
+        item: 'first-of-type:rounded-t-lg last-of-type:rounded-b-lg'
+      }
+    },
+    ...(options.theme.colors || []).map((color: string) => ({
+      color,
+      variant: 'card',
+      class: {
+        item: `has-data-[state=checked]:border-(--ui-${color})`
+      }
+    })),
+    {
+      color: 'neutral',
+      variant: 'card',
+      class: {
+        item: 'has-data-[state=checked]:border-(--ui-border-elevated)'
+      }
+    },
+    ...(options.theme.colors || []).map((color: string) => ({
+      color,
+      variant: 'table',
+      class: {
+        item: `has-data-[state=checked]:bg-(--ui-${color})/10 has-data-[state=checked]:border-(--ui-${color})/50 has-data-[state=checked]:z-[1]`
+      }
+    })),
+    {
+      color: 'neutral',
+      variant: 'table',
+      class: {
+        item: 'has-data-[state=checked]:bg-(--ui-bg-elevated) has-data-[state=checked]:border-(--ui-border-inverted)/25 has-data-[state=checked]:z-[1]'
+      }
+    },
+    ...(options.theme.colors || []).map((color: string) => ({
+      color,
+      checked: true,
+      class: `ring-2 ring-(--ui-${color}) bg-(--ui-${color})`
+    })), {
+      color: 'neutral',
+      checked: true,
+      class: 'ring-2 ring-(--ui-border-inverted) bg-(--ui-bg-inverted)'
+    }
+  ],
   defaultVariants: {
     size: 'md',
-    color: 'primary'
+    color: 'primary',
+    variant: 'list',
+    indicator: 'start'
   }
 })
