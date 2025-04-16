cf7f46dcdadd8c7e298ee860f6cace2a84d4bcf3
rich-tribeweb <rich@tribeweb.co.uk>
2025-04-16 01:52:04 +0100

    chore: update playground with variant chooser

--- playground/app/pages/components/checkbox.vue
+++ playground/app/pages/components/checkbox.vue
@@ -2,27 +2,37 @@
 import theme from '#build/ui/checkbox'
 
 const sizes = Object.keys(theme.variants.size) as Array<keyof typeof theme.variants.size>
+const variants = Object.keys(theme.variants.variant)
+const variant = ref('list' as const)
 
 const checked = ref(true)
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-4">
+    <USelect v-model="variant" :items="variants" />
     <div class="flex flex-col gap-4">
-      <UCheckbox v-model="checked" label="Primary" />
-      <UCheckbox label="Neutral" color="neutral" :default-value="true" />
-      <UCheckbox label="Error" color="error" :model-value="true" />
-      <UCheckbox label="Icon" icon="i-lucide-heart" :model-value="true" />
-      <UCheckbox label="Default value" :default-value="true" />
-      <UCheckbox label="Indeterminate" default-value="indeterminate" />
-      <UCheckbox label="Required" required />
-      <UCheckbox label="Disabled" disabled />
+      <UCheckbox v-model="checked" label="Primary" :variant="variant" />
+      <UCheckbox label="Neutral" color="neutral" :variant="variant" :default-value="true" />
+      <UCheckbox label="Error" color="error" :variant="variant" :model-value="true" />
+      <UCheckbox label="Icon" icon="i-lucide-heart" :variant="variant" :model-value="true" />
+      <UCheckbox label="Default value" :variant="variant" :default-value="true" />
+      <UCheckbox label="Indeterminate" :variant="variant" default-value="indeterminate" />
+      <UCheckbox label="Required" :variant="variant" required />
+      <UCheckbox label="Disabled" :variant="variant" disabled />
     </div>
     <div class="flex items-center gap-4 me-[-11px]">
-      <UCheckbox v-for="size in sizes" :key="size" label="Check me" :size="size" />
+      <UCheckbox v-for="size in sizes" :key="size" label="Check me" :size="size" :variant="variant" />
     </div>
     <div class="flex items-center gap-4 me-[-96px]">
-      <UCheckbox v-for="size in sizes" :key="size" label="Check me" description="This is a description" :size="size" />
+      <UCheckbox
+        v-for="size in sizes"
+        :key="size"
+        label="Check me"
+        description="This is a description"
+        :size="size"
+        :variant="variant"
+      />
     </div>
   </div>
 </template>
