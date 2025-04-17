61318dffefa9ac6eb8bde12acb04a190f986aaba
Benjamin Canac <canacb1@gmail.com>
2025-04-17 11:22:08 +0200

    fix(select-menu): use `defuFn` instead of string interpolation

--- src/theme/select-menu.ts
+++ src/theme/select-menu.ts
@@ -1,14 +1,13 @@
-import { defu } from 'defu'
+import { defuFn } from 'defu'
 import type { ModuleOptions } from '../module'
 import select from './select'
 
 export default (options: Required<ModuleOptions>) => {
-  const selectTheme = select(options)
-  return defu({
+  return defuFn({
     slots: {
       input: 'border-b border-(--ui-border)',
       focusScope: 'flex flex-col min-h-0',
-      content: `${selectTheme.slots.content} origin-(--reka-combobox-content-transform-origin)`
+      content: (content: string) => [content, 'origin-(--reka-combobox-content-transform-origin)']
     }
-  }, selectTheme)
+  }, select(options))
 }
