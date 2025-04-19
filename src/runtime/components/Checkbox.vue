30d769f109baa64deec011aedd62df0a69f1ccde
rich-tribeweb <rich@tribeweb.co.uk>
2025-04-19 23:52:07 +0100

    chore: refactor using vueUse-createReusableTemplate to reduce repetition

--- src/runtime/components/Checkbox.vue
+++ src/runtime/components/Checkbox.vue
@@ -60,12 +60,14 @@ export interface CheckboxSlots {
 <script setup lang="ts">
 import { computed, useId } from 'vue'
 import { CheckboxRoot, CheckboxIndicator, Label, useForwardProps, Primitive } from 'reka-ui'
-import { reactivePick } from '@vueuse/core'
+import { reactivePick, createReusableTemplate } from '@vueuse/core'
 import { useAppConfig } from '#imports'
 import { useFormField } from '../composables/useFormField'
 import { useCheckboxGroup } from '../composables/useCheckboxGroup'
 import { tv } from '../utils/tv'
 import UIcon from './Icon.vue'
+
+const [DefineTemplate, ReuseTemplate] = createReusableTemplate()
 
 defineOptions({ inheritAttrs: false })
 
@@ -106,43 +108,7 @@ function onUpdate(value: any) {
 
 <!-- eslint-disable vue/no-template-shadow -->
 <template>
-  <template v-if="!orientation">
-    <Primitive :as="as" :class="ui.root({ class: [props.class, props.ui?.root] })">
-      <component :is="variant === 'list' ? 'div' : Label" :class="ui.item({ class: props.ui?.item })">
-        <div :class="ui.container({ class: props.ui?.container })">
-          <CheckboxRoot
-            :id="id"
-            v-bind="{ ...rootProps, ...$attrs, ...ariaAttrs }"
-            v-model="modelValue"
-            :name="name"
-            :disabled="disabled"
-            :class="ui.base({ class: props.ui?.base })"
-            @update:model-value="onUpdate"
-          >
-            <template #default="{ modelValue }">
-              <CheckboxIndicator as-child :class="ui.indicator({ class: props.ui?.indicator })">
-                <UIcon v-if="modelValue === 'indeterminate'" :name="indeterminateIcon || appConfig.ui.icons.minus" :class="ui.icon({ class: props.ui?.icon })" />
-                <UIcon v-else :name="icon || appConfig.ui.icons.check" :class="ui.icon({ class: props.ui?.icon })" />
-              </CheckboxIndicator>
-            </template>
-          </CheckboxRoot>
-        </div>
-        <div :class="ui.wrapper({ class: props.ui?.wrapper })">
-          <component :is="variant === 'list' ? Label : 'p'" :class="ui.label({ class: props.ui?.label })" :for="id">
-            <slot name="label" :label="label">
-              {{ label }}
-            </slot>
-          </component>
-          <p v-if="description || !!slots.description" :class="ui.description({ class: props.ui?.description })">
-            <slot name="description" :description="description">
-              {{ description }}
-            </slot>
-          </p>
-        </div>
-      </component>
-    </Primitive>
-  </template>
-  <template v-else>
+  <DefineTemplate>
     <component :is="variant === 'list' ? 'div' : Label" :class="ui.item({ class: props.ui?.item })">
       <div :class="ui.container({ class: props.ui?.container })">
         <CheckboxRoot
@@ -175,5 +141,13 @@ function onUpdate(value: any) {
         </p>
       </div>
     </component>
+  </DefineTemplate>
+  <template v-if="!orientation">
+    <Primitive :as="as" :class="ui.root({ class: [props.class, props.ui?.root] })">
+      <ReuseTemplate />
+    </Primitive>
+  </template>
+  <template v-else>
+    <ReuseTemplate />
   </template>
 </template>
