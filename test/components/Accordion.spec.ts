1197806d6a6ba13be3a55c96746fd3bc10a3e2ea
J-michalek <jaku.michalek@gmail.com>
2025-05-06 18:58:42 +0200

    feat(accordion): add ui field in items

--- test/components/Accordion.spec.ts
+++ test/components/Accordion.spec.ts
@@ -56,7 +56,16 @@ describe('Accordion', () => {
     ['with content slot', { props: { ...props, modelValue: '1' }, slots: { content: () => 'Content slot' } }],
     ['with body slot', { props: { ...props, modelValue: '1' }, slots: { body: () => 'Body slot' } }],
     ['with custom slot', { props: { ...props, modelValue: '5' }, slots: { custom: () => 'Custom slot' } }],
-    ['with custom body slot', { props: { ...props, modelValue: '5' }, slots: { 'custom-body': () => 'Custom body slot' } }]
+    ['with custom body slot', { props: { ...props, modelValue: '5' }, slots: { 'custom-body': () => 'Custom body slot' } }],
+    ['with ui prop on items', { props: { items: [{ ...items[0], ui: {
+      header: 'item-custom-header-class',
+      trigger: 'item-custom-trigger-class',
+      leadingIcon: 'item-custom-leading-icon-class',
+      label: 'item-custom-label-class',
+      trailingIcon: 'item-custom-trailing-icon-class',
+      content: 'item-custom-content-class',
+      body: 'item-custom-body-class'
+    } }, ...items.slice(1)] } }]
   ])('renders %s correctly', async (nameOrHtml: string, options: { props?: AccordionProps, slots?: Partial<AccordionSlots & { custom: () => 'Custom slot' } & { 'custom-body': () => 'Custom body slot' }> }) => {
     const html = await ComponentRender(nameOrHtml, options, Accordion)
     expect(html).toMatchSnapshot()
