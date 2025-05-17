6b4765942838dfed5cc360508cee414058d42d7a
zikju <zq@zq.lt>
2025-05-17 13:04:10 +0300

    feat(locale): add Lithuanian language

--- docs/app/components/content/SupportedLanguages.vue
+++ docs/app/components/content/SupportedLanguages.vue
@@ -25,6 +25,7 @@ function getEmojiFlag(locale: string): string {
     kk: 'kz', // Kazakh -> Kazakhstan
     km: 'kh', // Khmer -> Cambodia
     ko: 'kr', // Korean -> South Korea
+    lt: 'lt', // Lithuanian -> Lithuania
     ms: 'my', // Malay -> Malaysia
     nb: 'no', // Norwegian Bokmål -> Norway
     sl: 'si', // Slovenian -> Slovenia
