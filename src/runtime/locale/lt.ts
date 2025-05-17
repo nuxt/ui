77ad25d823265f1491fc06721d1221b9b7921822
zikju <zq@zq.lt>
2025-05-17 13:04:10 +0300

    feat(locale): add Lithuanian language

---
+++ src/runtime/locale/lt.ts
@@ -0,0 +1,56 @@
+import type { Messages } from '../types'
+import { defineLocale } from '../composables/defineLocale'
+
+export default defineLocale<Messages>({
+  name: 'Lietuvių',
+  code: 'lt',
+  messages: {
+    inputMenu: {
+      noMatch: 'Nėra atitinkančių duomenų',
+      noData: 'Nėra duomenų',
+      create: 'Sukurti „{label}“'
+    },
+    calendar: {
+      prevYear: 'Ankstesni metai',
+      nextYear: 'Kiti metai',
+      prevMonth: 'Ankstesnis mėnuo',
+      nextMonth: 'Kitas mėnuo'
+    },
+    inputNumber: {
+      increment: 'Padidinti',
+      decrement: 'Sumažinti'
+    },
+    commandPalette: {
+      placeholder: 'Įveskite komandą arba ieškokite...',
+      noMatch: 'Nėra atitinkančių duomenų',
+      noData: 'Nėra duomenų',
+      close: 'Uždaryti'
+    },
+    selectMenu: {
+      noMatch: 'Nėra atitinkančių duomenų',
+      noData: 'Nėra duomenų',
+      create: 'Sukurti „{label}“',
+      search: 'Ieškoti...'
+    },
+    toast: {
+      close: 'Uždaryti'
+    },
+    carousel: {
+      prev: 'Atgal',
+      next: 'Pirmyn',
+      goto: 'Eiti į skaidrę {slide}'
+    },
+    modal: {
+      close: 'Uždaryti'
+    },
+    slideover: {
+      close: 'Uždaryti'
+    },
+    alert: {
+      close: 'Uždaryti'
+    },
+    table: {
+      noData: 'Nėra duomenų'
+    }
+  }
+})
