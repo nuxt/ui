b41330e345d7ca2693671ba5d963f8e2077f8ef6
Valentin Chmara <valentinchmara@gmail.com>
2025-06-06 15:34:21 +0200

    refactor: design

--- playground/app/pages/components/file-upload.vue
+++ playground/app/pages/components/file-upload.vue
@@ -12,12 +12,12 @@ const testFile = ref<UploadWithStatus[]>([{
 
 <template>
   <div class="grid grid-cols-12 *:px-1 *:py-8 *:not-first:-ms-px *:not-first:-mt-px sm:*:px-8 xl:*:px-12">
-    <UFileUpload v-model="files" size="xs" upload-icon="i-lucide-image" class="col-span-12 sm:col-span-6 lg:col-span-6">
+    <UFileUpload v-model="files" multiple size="xs" upload-icon="i-lucide-image" class="col-span-12 sm:col-span-6 lg:col-span-6">
       <UButton variant="outline" color="neutral" icon="i-lucide-upload" size="xs" label="Select image" />
     </UFileUpload>
     <UFileUpload v-model="testFile" size="sm" layout="grid" class="col-span-12 sm:col-span-6 lg:col-span-6" />
     <UFileUpload size="md" preview-placement="outside" class="col-span-12 sm:col-span-6 lg:col-span-6" />
     <UFileUpload size="lg" disabled class="col-span-12 sm:col-span-6 lg:col-span-6" />
-    <UFileUpload size="xl" :multiple="true" layout="grid" preview-placement="outside" class="col-span-12 sm:col-span-6 lg:col-span-6" />
+    <UFileUpload size="xl" multiple layout="grid" class="col-span-12 sm:col-span-6 lg:col-span-6" />
   </div>
 </template>
--- src/theme/file-upload.ts
+++ src/theme/file-upload.ts
@@ -1,13 +1,13 @@
 export default {
   slots: {
     root: 'group/item relative flex flex-col gap-2 items-center',
-    base: 'w-full relative flex flex-col items-center overflow-hidden p-4 transition-colors bg-default shadow-sm rounded-md divide-y divide-default overflow-y-auto border border-dashed border-accented',
+    base: 'w-full relative flex flex-col items-center overflow-hidden p-4 transition-colors bg-default shadow-sm rounded-md overflow-y-auto border border-dashed border-accented',
     dragging: 'bg-accented/20',
     empty: 'flex flex-col items-center justify-center gap-2',
     label: 'font-semibold text-highlighted text-center px-2 line-clamp-1',
     uploadIcon: 'pointer-events-none',
     files: 'space-y-2 w-full',
-    filesActions: 'flex items-center justify-between w-full px-1 py-2',
+    filesActions: 'w-full flex items-center justify-between px-1 py-2',
     file: 'text-default rounded-md relative',
     fileContent: 'flex items-center gap-3',
     fileLeadingAvatar: 'shrink-0',
@@ -25,7 +25,6 @@ export default {
         empty: 'min-h-16',
         label: 'text-xs',
         uploadIcon: 'size-4',
-        files: 'w-full',
         file: 'p-1 text-xs gap-1',
         fileLeadingAvatarSize: 'xs'
       },
@@ -33,7 +32,6 @@ export default {
         empty: 'h-20',
         label: 'text-xs',
         uploadIcon: 'size-4',
-        files: 'w-full',
         file: 'p-1.5 text-xs gap-1.5',
         fileLeadingAvatarSize: 'sm'
       },
@@ -41,7 +39,6 @@ export default {
         empty: 'h-24',
         label: 'text-sm',
         uploadIcon: 'size-5',
-        files: 'w-full',
         file: 'p-1.5 text-sm gap-1.5',
         fileLeadingAvatarSize: 'md'
       },
@@ -49,7 +46,6 @@ export default {
         empty: 'h-26',
         label: 'text-sm',
         uploadIcon: 'size-5',
-        files: 'w-full',
         file: 'p-2 text-sm gap-2',
         fileLeadingAvatarSize: 'lg'
       },
@@ -57,7 +53,6 @@ export default {
         empty: 'h-32',
         label: 'text-base',
         uploadIcon: 'size-6',
-        files: 'w-full',
         file: 'p-2 text-base gap-2',
         fileLeadingAvatarSize: 'xl'
       }
@@ -68,12 +63,15 @@ export default {
     },
     layout: {
       list: {
+        base: 'divide-y divide-default',
         files: 'space-y-2',
         file: 'flex justify-between items-center gap-2 p-2 border border-accented pe-3',
         fileContent: 'flex items-center gap-3',
         fileTrailing: 'flex items-start'
       },
-      grid: ''
+      grid: {
+        base: 'gap-2'
+      }
     },
     disabled: {
       true: {
@@ -116,7 +114,7 @@ export default {
         file: 'text-default relative',
         fileContent: 'relative rounded-md aspect-square flex items-center justify-center bg-elevated',
         fileTrailing: 'absolute -top-1 -right-1',
-        fileImage: 'size-full object-cover'
+        fileImage: 'object-cover absolute inset-0 w-full h-full rounded-[inherit]'
       }
     },
     {
@@ -127,13 +125,13 @@ export default {
         file: 'text-default rounded-md min-h-26 relative bg-elevated',
         fileContent: 'absolute inset-0 flex items-center justify-center',
         fileTrailing: 'absolute -top-2 -right-2',
-        fileImage: 'mx-auto max-h-full object-contain'
+        fileImage: 'mx-auto max-w-full max-h-full object-contain'
       }
     },
     {
       layout: 'grid',
       class: {
-        fileRemoveButton: 'rounded-full'
+        fileRemoveButton: 'rounded-full shadow-sm'
       }
     },
     {
