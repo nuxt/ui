6aca751861713ac901a92f00c1a2edf4c8b2e53c
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-07-25 18:02:05 +0200

    docs(llms): edit links to directly point to the raw content

--- docs/server/plugins/llms.ts
+++ docs/server/plugins/llms.ts
@@ -5,4 +5,21 @@ export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:llms:generate:document', async (_: H3Event, doc: PageCollectionItemBase) => {
     transformMDC(doc as any)
   })
+
+  nitroApp.hooks.hook('llms:generate', (_, { sections }) => {
+    sections.map((section) => {
+      if (section.title === 'Documentation Sets') return section
+      return {
+        ...section,
+        links: section.links.map(link => ({
+          ...link,
+          href: transformRawLink(link.href)
+        }))
+      }
+    })
+  })
 })
+
+function transformRawLink(href: string) {
+  return `${href.replace(/^https:\/\/ui.nuxt.com/, 'https://ui.nuxt.com/raw')}.md`
+}
