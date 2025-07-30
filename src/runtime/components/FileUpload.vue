09ceae342425376ffd0f9da3708de68f0d1a5a0a
Jakub <jakub.michalek@freelo.io>
2025-07-30 06:23:36 +0200

    fix(FileUpload): prevent scrolling on keyup

--- src/runtime/components/FileUpload.vue
+++ src/runtime/components/FileUpload.vue
@@ -329,6 +329,7 @@ defineExpose({
         :class="ui.base({ class: props.ui?.base })"
         :tabindex="interactive && !disabled ? 0 : -1"
         @click="interactive && !disabled && open()"
+        @keydown.prevent
         @keyup.enter.space="interactive && !disabled && open()"
       >
         <ReuseFilesTemplate v-if="position === 'inside'" />
