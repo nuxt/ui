f5c740d31f11bff9ea97f57f4f421cb5d5a49af8
Hugo Richard <hugo.richard@epitech.eu>
2025-08-11 11:51:42 +0100

    up

--- docs/server/api/chat.ts
+++ docs/server/api/chat.ts
@@ -1,13 +1,13 @@
-// import { streamText } from 'ai'
+import { streamText, convertToModelMessages } from 'ai'
+import { gateway } from '@ai-sdk/gateway'
 
 export default defineEventHandler(async (event) => {
   const { messages } = await readBody(event)
 
-  return messages
-  // return streamText({
-  //   model: workersAI('@cf/meta/llama-3.2-3b-instruct'),
-  //   maxTokens: 10000,
-  //   system: 'You are a helpful assistant that can answer questions and help.',
-  //   messages
-  // }).toDataStreamResponse()
+  return streamText({
+    model: gateway('openai/gpt-4o-mini'),
+    maxOutputTokens: 10000,
+    system: 'You are a helpful assistant that can answer questions and help.',
+    messages: convertToModelMessages(messages)
+  }).toUIMessageStreamResponse()
 })
