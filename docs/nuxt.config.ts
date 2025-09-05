b5d49f75f5683d589ac62b8a312b612c55363099
Benjamin Canac <canacb1@gmail.com>
2025-09-05 10:44:52 +0200

    docs(nuxt.config): improve redirect

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -80,7 +80,7 @@ export default defineNuxtConfig({
     '/docs/getting-started/icons': { redirect: '/docs/getting-started/icons/nuxt', prerender: false },
     '/docs/getting-started/color-mode': { redirect: '/docs/getting-started/color-mode/nuxt', prerender: false },
     '/docs/getting-started/i18n': { redirect: '/docs/getting-started/i18n/nuxt', prerender: false },
-    '/docs/getting-started/typography': { redirect: '/docs/typography', prerender: false },
+    '/docs/getting-started/typography': { redirect: { to: '/docs/typography', statusCode: 301 }, prerender: false },
     // v4 redirects - renamed components
     '/docs/components/button-group': { redirect: { to: '/docs/components/field-group', statusCode: 301 }, prerender: false },
     '/docs/components/page-accordion': { redirect: { to: '/docs/components/accordion', statusCode: 301 }, prerender: false },
