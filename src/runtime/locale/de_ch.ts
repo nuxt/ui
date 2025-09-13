45bac67887f5db3a61fda4a6acb2a08c74e3f43f
Sam <samuel.sieg@crafted.ch>
2025-09-13 12:09:39 +0200

    Fix quotes

--- src/runtime/locale/de_ch.ts
+++ src/runtime/locale/de_ch.ts
@@ -1,131 +1,131 @@
-import type { Messages } from "../types";
-import { defineLocale } from "../composables/defineLocale";
+import type { Messages } from '../types'
+import { defineLocale } from '../composables/defineLocale'
 
 export default defineLocale<Messages>({
-  name: "Schweizerdeutsch",
-  code: "de-CH",
+  name: 'Schweizerdeutsch',
+  code: 'de-CH',
   messages: {
     alert: {
-      close: "Schliessen",
+      close: 'Schliessen'
     },
     authForm: {
-      hidePassword: "Passwort verbergen",
-      showPassword: "Passwort anzeigen",
-      submit: "Weiter",
+      hidePassword: 'Passwort verbergen',
+      showPassword: 'Passwort anzeigen',
+      submit: 'Weiter'
     },
     banner: {
-      close: "Schliessen",
+      close: 'Schliessen'
     },
     calendar: {
-      nextMonth: "Nächster Monat",
-      nextYear: "Nächstes Jahr",
-      prevMonth: "Vorheriger Monat",
-      prevYear: "Vorheriges Jahr",
+      nextMonth: 'Nächster Monat',
+      nextYear: 'Nächstes Jahr',
+      prevMonth: 'Vorheriger Monat',
+      prevYear: 'Vorheriges Jahr'
     },
     carousel: {
-      dots: "Folie zur Anzeige auswählen",
-      goto: "Gehe zu {slide}",
-      next: "Weiter",
-      prev: "Zurück",
+      dots: 'Folie zur Anzeige auswählen',
+      goto: 'Gehe zu {slide}',
+      next: 'Weiter',
+      prev: 'Zurück'
     },
     chatPrompt: {
-      placeholder: "Hier schreiben Sie Ihre Nachricht...",
+      placeholder: 'Hier schreiben Sie Ihre Nachricht...'
     },
     chatPromptSubmit: {
-      label: "Senden",
+      label: 'Senden'
     },
     colorMode: {
-      dark: "Dunkel",
-      light: "Hell",
-      switchToDark: "Zum dunklen Modus wechseln",
-      switchToLight: "Zum hellen Modus wechseln",
-      system: "System",
+      dark: 'Dunkel',
+      light: 'Hell',
+      switchToDark: 'Zum dunklen Modus wechseln',
+      switchToLight: 'Zum hellen Modus wechseln',
+      system: 'System'
     },
     commandPalette: {
-      back: "Zurück",
-      close: "Schliessen",
-      noData: "Keine Daten",
-      noMatch: "Nichts gefunden",
-      placeholder: "Geben Sie einen Befehl ein oder suchen Sie...",
+      back: 'Zurück',
+      close: 'Schliessen',
+      noData: 'Keine Daten',
+      noMatch: 'Nichts gefunden',
+      placeholder: 'Geben Sie einen Befehl ein oder suchen Sie...'
     },
     contentSearch: {
-      links: "Links",
-      theme: "Thema",
+      links: 'Links',
+      theme: 'Thema'
     },
     contentSearchButton: {
-      label: "Suchen...",
+      label: 'Suchen...'
     },
     contentToc: {
-      title: "Auf dieser Seite",
+      title: 'Auf dieser Seite'
     },
     dashboardSearch: {
-      theme: "Thema",
+      theme: 'Thema'
     },
     dashboardSearchButton: {
-      label: "Suchen...",
+      label: 'Suchen...'
     },
     dashboardSidebarCollapse: {
-      collapse: "Seitenleiste einklappen",
-      expand: "Seitenleiste erweitern",
+      collapse: 'Seitenleiste einklappen',
+      expand: 'Seitenleiste erweitern'
     },
     dashboardSidebarToggle: {
-      close: "Seitenleiste schliessen",
-      open: "Seitenleiste öffnen",
+      close: 'Seitenleiste schliessen',
+      open: 'Seitenleiste öffnen'
     },
     error: {
-      clear: "Zurück zur Startseite",
+      clear: 'Zurück zur Startseite'
     },
     fileUpload: {
-      removeFile: "{filename} entfernen",
+      removeFile: '{filename} entfernen'
     },
     header: {
-      close: "Menü schliessen",
-      open: "Menü öffnen",
+      close: 'Menü schliessen',
+      open: 'Menü öffnen'
     },
     inputMenu: {
       create: '"{label}" erstellen',
-      noData: "Keine Daten",
-      noMatch: "Nichts gefunden",
+      noData: 'Keine Daten',
+      noMatch: 'Nichts gefunden'
     },
     inputNumber: {
-      decrement: "Verringern",
-      increment: "Erhöhen",
+      decrement: 'Verringern',
+      increment: 'Erhöhen'
     },
     modal: {
-      close: "Schliessen",
+      close: 'Schliessen'
     },
     pricingTable: {
-      caption: "Preisplanvergleich",
+      caption: 'Preisplanvergleich'
     },
     prose: {
       codeCollapse: {
-        closeText: "Reduzieren",
-        name: "Code",
-        openText: "Erweitern",
+        closeText: 'Reduzieren',
+        name: 'Code',
+        openText: 'Erweitern'
       },
       collapsible: {
-        closeText: "Ausblenden",
-        name: "Eigenschaften",
-        openText: "Anzeigen",
+        closeText: 'Ausblenden',
+        name: 'Eigenschaften',
+        openText: 'Anzeigen'
       },
       pre: {
-        copy: "Code in die Zwischenablage kopieren",
-      },
+        copy: 'Code in die Zwischenablage kopieren'
+      }
     },
     selectMenu: {
       create: '"{label}" erstellen',
-      noData: "Keine Daten",
-      noMatch: "Nichts gefunden",
-      search: "Suchen...",
+      noData: 'Keine Daten',
+      noMatch: 'Nichts gefunden',
+      search: 'Suchen...'
     },
     slideover: {
-      close: "Schliessen",
+      close: 'Schliessen'
     },
     table: {
-      noData: "Keine Daten",
+      noData: 'Keine Daten'
     },
     toast: {
-      close: "Schliessen",
-    },
-  },
-});
+      close: 'Schliessen'
+    }
+  }
+})
