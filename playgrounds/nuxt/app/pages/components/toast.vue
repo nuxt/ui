e5ab996a5be0fadf1fef682120e7f7aa4219b983
Benjamin Canac <canacb1@gmail.com>
2025-10-09 15:42:15 +0200

    up

--- playgrounds/nuxt/app/pages/components/toast.vue
+++ playgrounds/nuxt/app/pages/components/toast.vue
@@ -129,6 +129,7 @@ function removeToast() {
     <UCheckbox v-model="appConfig.toaster.expand" label="Expand" />
     <USelect v-model="appConfig.toaster.position" :items="positions" placeholder="Position" />
     <UInput v-model="appConfig.toaster.duration" label="Duration" type="number" />
+    <UInput v-model="appConfig.toaster.max" label="Max" type="number" />
   </Navbar>
 
   <div class="flex items-center gap-2">
