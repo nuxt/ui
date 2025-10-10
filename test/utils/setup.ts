2dd8911e63099b4031a9d5e44cc2d142ec0625df
J-michalek <jaku.michalek@gmail.com>
2025-10-10 18:01:33 +0200

    chore(vitest-axe): disable region rule
    This rule demands the component to be a part of the
    whole page which would require us to attach the mounted
    component to the body, which I don't find necessary.

--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -1,6 +1,7 @@
 import * as matchers from 'vitest-axe/matchers'
 
 import { expect } from 'vitest'
+import { configureAxe } from 'vitest-axe'
 
 // @ts-expect-error incomplete implementation
 window.IntersectionObserver = class IntersectionObserver {
@@ -10,5 +11,15 @@ window.IntersectionObserver = class IntersectionObserver {
   unobserve() {}
   disconnect() {}
 }
+
+configureAxe({
+  globalOptions: {
+    rules: [{
+      // Disable region rule as it doesn't work well with components rendered in isolation.
+      id: 'region',
+      enabled: false
+    }]
+  }
+})
 
 expect.extend(matchers)
