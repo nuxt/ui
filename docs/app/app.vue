a34192b30b96639ad00b327af9628963bc15c351
rdjanuar <rdjanuar008@gmail.com>
2025-10-13 10:38:13 +0700

    up

--- docs/app/app.vue
+++ docs/app/app.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { withoutTrailingSlash } from 'ufo'
 import colors from 'tailwindcss/colors'
+import { Analytics } from '@vercel/analytics/nuxt'
 import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 
 const route = useRoute()
