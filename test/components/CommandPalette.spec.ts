fb3c5dbf904ab8d0f2083a461d54ec3d8756f761
Benjamin Canac <canacb1@gmail.com>
2025-10-21 14:07:09 +0200

    test: put back with searchTerm

--- test/components/CommandPalette.spec.ts
+++ test/components/CommandPalette.spec.ts
@@ -106,6 +106,8 @@ describe('CommandPalette', () => {
     ['without groups', {}],
     ['with modelValue', { props: { ...props, modelValue: groups[2]?.items[0] } }],
     ['with defaultValue', { props: { ...props, defaultValue: groups[2]?.items[0] } }],
+    ['with searchTerm', { props: { ...props, searchTerm: 'f' } }],
+    ['with searchTerm and preserveGroupOrder', { props: { ...props, searchTerm: 'f', preserveGroupOrder: true } }],
     ['with labelKey', { props: { ...props, labelKey: 'icon' } }],
     ['with descriptionKey', { props: { groups: groupsWithDescription, descriptionKey: 'label' } }],
     ['with placeholder', { props: { ...props, placeholder: 'Search...' } }],
