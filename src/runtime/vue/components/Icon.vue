2de25797e97cdaf9ebd35550293eb0a361fd7236
Dany <alwe.dev@gmail.com>
2025-10-30 17:18:04 +0100

    feat(Icon): add global options on Vue-only side

--- src/runtime/vue/components/Icon.vue
+++ src/runtime/vue/components/Icon.vue
@@ -1,18 +1,39 @@
-<script lang="ts">
-import type { IconProps as NuxtIconProps } from '../../types'
-
-export interface IconProps {
-  name: NuxtIconProps['name']
-}
-</script>
-
 <script setup lang="ts">
+import type { IconProps } from '../../types'
+import type { IconifyRenderMode } from '@iconify/vue'
+import { computed } from 'vue'
 import { Icon as IconifyIcon } from '@iconify/vue'
+import { useAppConfig } from '#imports'
+import type { RuntimeOptions } from '@nuxt/icon'
 
-defineProps<IconProps>()
+const props = defineProps<IconProps>()
+
+const appConfig = useAppConfig()
+
+function resolveCustomizeFn(
+  customize: RuntimeOptions['customize'] | boolean | null | undefined,
+  globalCustomize: RuntimeOptions['customize'] | undefined
+): RuntimeOptions['customize'] | undefined {
+  if (customize === false) return undefined
+  if (customize === true || customize === null) return globalCustomize
+  return customize
+}
+
+const resolvedMode = computed(() => {
+  const mode = props.mode || appConfig.icon?.mode
+  if (mode === 'css') return 'style'
+  return mode as IconifyRenderMode
+})
 </script>
 
 <template>
-  <IconifyIcon v-if="typeof name === 'string'" :icon="name.replace(/^i-/, '')" />
+  <IconifyIcon
+    v-if="typeof name === 'string'"
+    :icon="name.replace(/^i-/, '')"
+    :mode="resolvedMode"
+    :width="size || appConfig.icon?.size"
+    :height="size || appConfig.icon?.size"
+    :customise="resolveCustomizeFn(props.customize, appConfig.icon?.customize)"
+  />
   <component :is="name" v-else />
 </template>
