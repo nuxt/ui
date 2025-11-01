0fa447a0a0afb27a6bf6a68fea84163c0acf9a4a
hywax <a.hywax@gmail.com>
2025-11-01 16:50:03 +0500

    fix(ModelModifiers): update type constraints for string and number properties

--- src/runtime/types/input.ts
+++ src/runtime/types/input.ts
@@ -1,8 +1,8 @@
 export interface ModelModifiers<T = any> {
-  string?: boolean
-  number?: boolean
+  string?: T extends string ? boolean : never
+  number?: T extends number ? boolean : never
   trim?: T extends string ? boolean : never
   lazy?: boolean
   nullable?: T extends null ? boolean : never
-  optional?: T extends undefined ? boolean : never
+  optional?: boolean
 }
