fa460eb3aaf89aec3e34d6a601cf7414b52e51c1
hywax <a.hywax@gmail.com>
2025-11-01 18:06:53 +0500

    refactor(InputTime): remove console.log statement from TimeFieldInput

--- src/runtime/components/InputTime.vue
+++ src/runtime/components/InputTime.vue
@@ -146,7 +146,6 @@ defineExpose({
         :class="segment.part !== 'literal' ? ui.segment({ class: props.ui?.segment }) : ''"
       >
         {{ segment.value.trim() }}
-        {{ console.log(segment) }}
       </TimeFieldInput>
 
       <slot :ui="ui" />
