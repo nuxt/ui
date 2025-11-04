06ef69684915e0be9311aada4484a757581cbf3d
Sabih S <antpalmolive@gmail.com>
2025-11-04 00:54:20 -0500

    docs: fix active tabs explanation

---
+++ docs/app/components/content/examples/tabs/TabsModelValueSimpleExample.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { TabsItem } from '@nuxt/ui'
+
+const items: TabsItem[] = [
+  {
+    label: 'Account',
+    value: 'account'
+  },
+  {
+    label: 'Password',
+    value: 'password'
+  }
+]
+</script>
+
+<template>
+  <UTabs defaultValue="account" :content="false" :items="items" class="w-full" />
+</template>
