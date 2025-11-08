9ba1e6a66d78bf3388edfb3fd5df702d969cbb20
Benjamin Canac <canacb1@gmail.com>
2025-11-08 14:51:59 +0100

    improve playground

--- playgrounds/nuxt/app/pages/components/input-date.vue
+++ playgrounds/nuxt/app/pages/components/input-date.vue
@@ -2,12 +2,6 @@
 import { CalendarDate } from '@internationalized/date'
 import theme from '#build/ui/input-date'
 
-const singleValue = shallowRef(new CalendarDate(2022, 1, 10))
-const rangeValue = shallowRef({
-  start: new CalendarDate(2022, 1, 10),
-  end: new CalendarDate(2022, 1, 20)
-})
-
 const colors = Object.keys(theme.variants.color)
 const variants = Object.keys(theme.variants.variant)
 const sizes = Object.keys(theme.variants.size)
@@ -18,7 +12,11 @@ const attrs = reactive({
   size: [theme.defaultVariants.size]
 })
 
-const range = ref(false)
+const value = shallowRef(new CalendarDate(2022, 1, 10))
+const range = shallowRef({
+  start: new CalendarDate(2022, 1, 10),
+  end: new CalendarDate(2022, 1, 20)
+})
 </script>
 
 <template>
@@ -26,11 +24,21 @@ const range = ref(false)
     <USelect v-model="attrs.color" :items="colors" multiple />
     <USelect v-model="attrs.variant" :items="variants" multiple />
     <USelect v-model="attrs.size" :items="sizes" multiple />
-    <USwitch v-model="range" label="Range" />
   </Navbar>
 
   <Matrix v-slot="props" :attrs="attrs">
-    <UInputDate v-if="range" v-model="rangeValue" range v-bind="props" />
-    <UInputDate v-else v-model="singleValue" v-bind="props" />
+    <UInputDate v-model="value" autofocus v-bind="props" />
+    <UInputDate :default-value="new CalendarDate(2022, 1, 10)" v-bind="props" />
+    <UInputDate v-model="range" range v-bind="props" />
+    <UInputDate :default-value="{ start: new CalendarDate(2022, 1, 10), end: new CalendarDate(2022, 1, 20) }" range v-bind="props" />
+    <UInputDate highlight v-bind="props" />
+    <UInputDate disabled v-bind="props" />
+    <UInputDate required v-bind="props" />
+    <UInputDate icon="i-lucide-clock" v-bind="props" />
+    <UInputDate icon="i-lucide-clock" trailing v-bind="props" />
+    <UInputDate :avatar="{ src: 'https://github.com/benjamincanac.png' }" icon="i-lucide-clock" trailing v-bind="props" />
+    <UInputDate loading v-bind="props" />
+    <UInputDate loading trailing v-bind="props" />
+    <UInputDate loading icon="i-lucide-clock" trailing-icon="i-lucide-chevron-down" v-bind="props" />
   </Matrix>
 </template>
