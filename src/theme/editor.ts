3c8453016170062afafeb3bd663dcb9f6123d861
Benjamin Canac <canacb1@gmail.com>
2025-11-11 12:30:05 +0100

    fix(editor): clean theme

--- src/theme/editor.ts
+++ src/theme/editor.ts
@@ -1,45 +1,44 @@
 import type { NuxtOptions } from '@nuxt/schema'
 
-export default (_options: Required<NuxtOptions['ui']>) => ({
+export default (options: Required<NuxtOptions['ui']>) => ({
   slots: {
     root: '',
     content: 'size-full flex flex-col flex-1',
     base: [
-      'flex-1 w-full outline-none *:first:mt-0 *:last:mb-0 px-4 sm:px-6 lg:px-8',
+      'flex-1 w-full outline-none *:my-5 *:first:mt-0 *:last:mb-0 px-4 sm:px-6 lg:px-8',
       // Placeholder
       '[&_:is(p,h1,h2,h3,h4).is-empty]:before:content-[attr(data-placeholder)] [&_:is(p,h1,h2,h3,h4).is-empty]:before:text-muted [&_:is(p,h1,h2,h3,h4).is-empty]:before:float-left [&_:is(p,h1,h2,h3,h4).is-empty]:before:h-0 [&_:is(p,h1,h2,h3,h4).is-empty]:before:pointer-events-none',
       '[&_li_.is-empty]:before:content-none',
       // Paragraph
-      '[&_p]:my-5 [&_p]:leading-7 [&_p]:text-pretty',
+      '[&_p]:leading-7',
       // Links
-      '[&_a]:text-primary [&_a]:border-b [&_a]:border-transparent [&_a]:hover:border-primary [&_a]:font-medium [&_a]:transition-colors',
-      '[&_a_code]:border-dashed [&_a:hover_code]:border-primary [&_a:hover_code]:text-primary [&_a_code]:transition-colors',
+      '[&_a]:text-primary [&_a]:border-b [&_a]:border-transparent [&_a]:hover:border-primary [&_a]:font-medium',
+      options.theme?.transitions && '[&_a]:transition-colors',
       // Mentions
       '[&_.mention]:text-primary [&_.mention]:font-medium',
       // Headings - shared styles
       '[&_:is(h1,h2,h3,h4)]:text-highlighted [&_:is(h1,h2,h3,h4)]:font-bold',
-      '[&_:is(h2,h3)_a_code]:font-bold [&_:is(h2,h3)_a_code]:border-dashed [&_:is(h2,h3):hover_a_code]:border-primary [&_:is(h2,h3):hover_a_code]:text-primary [&_:is(h2,h3)_a_code]:transition-colors',
       // Headings - unique styles
-      '[&_h1]:text-3xl [&_h1]:mb-8',
-      '[&_h2]:text-2xl [&_h2]:mb-6 [&_h2_a_code]:text-xl/7',
-      '[&_h3]:text-xl [&_h3]:mt-8 [&_h3]:mb-3 [&_h3_a_code]:text-lg/6',
-      '[&_h4]:text-lg [&_h4]:mt-6 [&_h4]:mb-2',
+      '[&_h1]:text-3xl',
+      '[&_h2]:text-2xl',
+      '[&_h3]:text-xl',
+      '[&_h4]:text-lg',
       // Blockquote & HR
-      '[&_blockquote]:border-s-4 [&_blockquote]:border-accented [&_blockquote]:ps-4 [&_blockquote]:italic [&_blockquote_pre]:my-0',
-      '[&_[data-type=horizontalRule]]:my-10 [&_[data-type=horizontalRule]]:py-2',
+      '[&_blockquote]:border-s-4 [&_blockquote]:border-accented [&_blockquote]:ps-4 [&_blockquote]:italic',
+      '[&_[data-type=horizontalRule]]:my-8 [&_[data-type=horizontalRule]]:py-2',
       '[&_hr]:border-t [&_hr]:border-default',
       // Code blocks
-      '[&_pre]:my-5 [&_pre]:font-mono [&_pre]:text-sm/6 [&_pre]:border [&_pre]:border-muted [&_pre]:bg-muted [&_pre]:rounded-md [&_pre]:px-4 [&_pre]:py-3 [&_pre]:whitespace-pre-wrap [&_pre]:break-words [&_pre]:overflow-x-auto',
+      '[&_pre]:text-sm/6 [&_pre]:border [&_pre]:border-muted [&_pre]:bg-muted [&_pre]:rounded-md [&_pre]:px-4 [&_pre]:py-3 [&_pre]:whitespace-pre-wrap [&_pre]:break-words [&_pre]:overflow-x-auto',
       '[&_pre_code]:p-0 [&_pre_code]:text-inherit [&_pre_code]:font-inherit [&_pre_code]:rounded-none [&_pre_code]:inline [&_pre_code]:border-0 [&_pre_code]:bg-transparent',
       // Inline code
       '[&_code]:px-1.5 [&_code]:py-0.5 [&_code]:text-sm [&_code]:font-mono [&_code]:font-medium [&_code]:rounded-md [&_code]:inline-block [&_code]:border [&_code]:border-muted [&_code]:text-highlighted [&_code]:bg-muted',
       // Lists
-      '[&_:is(ul,ol)]:ps-6 [&_:is(ul,ol)]:my-5',
+      '[&_:is(ul,ol)]:ps-6',
       '[&_ul]:list-disc [&_ul]:marker:text-(--ui-border-accented)',
       '[&_ol]:list-decimal [&_ol]:marker:text-muted',
-      '[&_li]:my-1.5 [&_li]:ps-1.5 [&_li]:leading-7 [&_li_p]:my-0 [&_li_ul]:my-0',
+      '[&_li]:my-1.5 [&_li]:ps-1.5',
       // Images
-      '[&_img]:my-5 [&_img]:rounded-md [&_img]:block [&_img]:max-w-full [&_img.ProseMirror-selectednode]:outline-2 [&_img.ProseMirror-selectednode]:outline-primary'
+      '[&_img]:rounded-md [&_img]:block [&_img]:max-w-full [&_img.ProseMirror-selectednode]:outline-2 [&_img.ProseMirror-selectednode]:outline-primary'
     ]
   }
 })
