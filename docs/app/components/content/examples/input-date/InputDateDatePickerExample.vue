1c33fa225a6eaebd0d710f95facdd63c5be5ad0e
hywax <a.hywax@gmail.com>
2025-11-12 23:49:00 +0500

    docs(InputDate): add examples for DatePicker and DateRangePicker components

---
+++ docs/app/components/content/examples/input-date/InputDateDatePickerExample.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { CalendarDate } from '@internationalized/date'
+
+const inputDateRef = useTemplateRef('inputDateRef')
+
+const modelValue = shallowRef(new CalendarDate(2022, 1, 10))
+</script>
+
+<template>
+  <UInputDate ref="inputDateRef" v-model="modelValue">
+    <template #trailing>
+      <UPopover :reference="inputDateRef?.inputsRef[3]?.$el">
+        <UButton
+          color="neutral"
+          variant="link"
+          size="sm"
+          icon="i-lucide-calendar"
+          aria-label="Select a date"
+          class="px-0"
+        />
+
+        <template #content>
+          <UCalendar v-model="modelValue" class="p-2" />
+        </template>
+      </UPopover>
+    </template>
+  </UInputDate>
+</template>
