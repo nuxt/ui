6fa004b351383f2002e9a841fb0e3c49674d9817
Benjamin Canac <canacb1@gmail.com>
2025-11-13 18:10:42 +0100

    chore(theme): update placeholder

--- src/theme/editor.ts
+++ src/theme/editor.ts
@@ -7,7 +7,7 @@ export default (options: Required<NuxtOptions['ui']>) => ({
     base: [
       'flex-1 w-full outline-none *:my-5 *:first:mt-0 *:last:mb-0 sm:px-8',
       // Placeholder
-      '[&_:is(p,h1,h2,h3,h4).is-empty]:before:content-[attr(data-placeholder)] [&_:is(p,h1,h2,h3,h4).is-empty]:before:text-muted [&_:is(p,h1,h2,h3,h4).is-empty]:before:float-left [&_:is(p,h1,h2,h3,h4).is-empty]:before:h-0 [&_:is(p,h1,h2,h3,h4).is-empty]:before:pointer-events-none',
+      '[&_:is(p,h1,h2,h3,h4).is-empty]:before:content-[attr(data-placeholder)] [&_:is(p,h1,h2,h3,h4).is-empty]:before:text-dimmed [&_:is(p,h1,h2,h3,h4).is-empty]:before:float-left [&_:is(p,h1,h2,h3,h4).is-empty]:before:h-0 [&_:is(p,h1,h2,h3,h4).is-empty]:before:pointer-events-none',
       '[&_li_.is-empty]:before:content-none',
       // Paragraph
       '[&_p]:leading-7',
