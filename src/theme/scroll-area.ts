5b2739b0be14ef2e0759be1d52d5865e2ded88ab
Benjamin Canac <canacb1@gmail.com>
2025-11-13 16:44:17 +0100

    chore(theme): improve

--- src/theme/scroll-area.ts
+++ src/theme/scroll-area.ts
@@ -1,19 +1,19 @@
 export default {
   slots: {
     root: 'relative',
-    viewport: 'relative gap-3 p-3',
+    viewport: 'relative flex gap-4 p-4',
     item: ''
   },
   variants: {
     orientation: {
       vertical: {
         root: 'overflow-y-auto overflow-x-hidden',
-        viewport: 'columns-xs inline-flex flex-col',
+        viewport: 'columns-xs flex-col',
         item: ''
       },
       horizontal: {
         root: 'overflow-x-auto overflow-y-hidden',
-        viewport: 'inline-flex flex-row',
+        viewport: 'flex-row',
         item: 'w-max'
       }
     }
