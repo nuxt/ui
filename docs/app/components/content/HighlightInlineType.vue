c9ac4a9b43cf2374803e3632a9294f9f0da65f8c
Farnabaz <farnabaz@gmail.com>
2025-11-18 16:50:23 +0100

    docs: update UI components and prevent hydration

--- docs/app/components/content/HighlightInlineType.vue
+++ docs/app/components/content/HighlightInlineType.vue
@@ -23,9 +23,14 @@ const type = computed(() => {
   return type
 })
 
-const { data: ast } = await useAsyncData(`hightlight-inline-code-${hash(type.value).slice(0, 10)}`, () => parseMarkdown(`\`${type.value}\`{lang="ts-type"}`))
+const ast = ref<any>(null)
+
+onMounted(async () => {
+  ast.value = await parseMarkdown(`\`${type.value}\`{lang="ts-type"}`)
+})
 </script>
 
 <template>
   <MDCRenderer v-if="ast" :body="ast.body" :data="ast.data" />
+  <ProseCode v-else>{{ type }}</ProseCode>
 </template>
