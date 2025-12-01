e455fa7e0a702e18cfcb6c5eb9a7de457b19671b
Benjamin Canac <canacb1@gmail.com>
2025-12-01 17:24:53 +0100

    up

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -16,6 +16,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxtjs/plausible',
     '@vueuse/nuxt',
+    '@vercel/analytics/nuxt/module',
     'nuxt-component-meta',
     'nuxt-og-image',
     'motion-v/nuxt',
