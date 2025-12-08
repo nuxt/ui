3860c473cea6b2fe500667f62fe9b05ce400a328
Benjamin Canac <canacb1@gmail.com>
2025-12-08 09:57:49 +0100

    up

--- docs/app/app.vue
+++ docs/app/app.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { withoutTrailingSlash } from 'ufo'
 import colors from 'tailwindcss/colors'
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
 
 const route = useRoute()
 const appConfig = useAppConfig()
@@ -50,6 +52,9 @@ provide('navigation', rootNavigation)
 <template>
   <UApp :toaster="appConfig.toaster">
     <NuxtLoadingIndicator color="var(--ui-primary)" :height="2" />
+
+    <Analytics :debug="false" />
+    <SpeedInsights :debug="false" />
 
     <div :class="[route.path.startsWith('/docs/') && 'root']">
       <template v-if="!route.path.startsWith('/examples')">
