a9fc648f59233c9c0b3b56d7bb187a33905761a3
Benjamin Canac <canacb1@gmail.com>
2025-12-15 10:45:15 +0100

    up

--- playground/server/api/blob.put.ts
+++ playground/server/api/blob.put.ts
@@ -1,5 +1,5 @@
 export default eventHandler(async (event) => {
-  return hubBlob().handleUpload(event, {
+  return blob.handleUpload(event, {
     formKey: 'files', // read file or files form the `formKey` field of request body (body should be a `FormData` object)
     multiple: true, // when `true`, the `formKey` field will be an array of `Blob` objects
     ensure: {
