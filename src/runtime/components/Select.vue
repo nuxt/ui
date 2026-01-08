e184fbf10a869ac4f1f96f78917da9ff8e2e3c5e
Youri van Mill <y.vanmill@nobears.nl>
2026-01-08 15:07:41 +0100

    move ref

--- src/runtime/components/Select.vue
+++ src/runtime/components/Select.vue
@@ -329,7 +329,7 @@ defineExpose({
       <SelectContent data-slot="content" :class="ui.content({ class: props.ui?.content })" v-bind="contentProps">
         <slot name="content-top" />
 
-        <div role="presentation" data-slot="viewport" :class="ui.viewport({ class: props.ui?.viewport })" ref="viewportRef">
+        <div ref="viewportRef" role="presentation" data-slot="viewport" :class="ui.viewport({ class: props.ui?.viewport })">
           <SelectGroup v-for="(group, groupIndex) in groups" :key="`group-${groupIndex}`" data-slot="group" :class="ui.group({ class: props.ui?.group })">
             <template v-for="(item, index) in group" :key="`group-${groupIndex}-${index}`">
               <SelectLabel v-if="isSelectItem(item) && item.type === 'label'" data-slot="label" :class="ui.label({ class: [props.ui?.label, item.ui?.label, item.class] })">
