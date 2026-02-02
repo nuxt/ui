6573aab722d0684d68acd592915958a25a494b2c
Benjamin Canac <canacb1@gmail.com>
2026-02-02 17:57:28 +0100

    put back empty page

---
+++ docs/app/pages/roadmap.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const title = 'Roadmap'
+const description = 'Discover our Volta board for @nuxt/ui development status.'
+
+useSeoMeta({
+  titleTemplate: '%s - Nuxt UI',
+  title,
+  description,
+  ogTitle: `${title} - Nuxt UI`,
+  ogDescription: description
+})
+
+defineOgImageComponent('Docs', {
+  headline: 'Community'
+})
+</script>
+
+<template>
+  <div class="h-[calc(100vh-var(--ui-header-height))]" />
+</template>
