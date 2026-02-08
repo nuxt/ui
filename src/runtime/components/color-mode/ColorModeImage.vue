6c66d99ecf2c69bfac1828d54a91f28121436676
Varun Chawla <varun_6april@hotmail.com>
2026-02-07 23:09:11 -0800

    fix: add baseURL support to ColorModeImage for public paths
    Fixes #5916
    
    ColorModeImage component now properly adds the app.baseURL to public
    folder paths (starting with '/'), ensuring images work correctly in
    production with custom base URLs. This matches the behavior of other
    image components like ProseImg.

--- src/runtime/components/color-mode/ColorModeImage.vue
+++ src/runtime/components/color-mode/ColorModeImage.vue
@@ -8,14 +8,37 @@ export interface ColorModeImageProps extends /** @vue-ignore */ Omit<ImgHTMLAttr
 </script>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { withTrailingSlash, withLeadingSlash, joinURL } from 'ufo'
+import { useRuntimeConfig } from '#imports'
 import ImageComponent from '#build/ui-image-component'
 
 defineOptions({ inheritAttrs: false })
 
-defineProps<ColorModeImageProps>()
+const props = defineProps<ColorModeImageProps>()
+
+const refinedLight = computed(() => {
+  if (props.light?.startsWith('/') && !props.light.startsWith('//')) {
+    const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
+    if (_base !== '/' && !props.light.startsWith(_base)) {
+      return joinURL(_base, props.light)
+    }
+  }
+  return props.light
+})
+
+const refinedDark = computed(() => {
+  if (props.dark?.startsWith('/') && !props.dark.startsWith('//')) {
+    const _base = withLeadingSlash(withTrailingSlash(useRuntimeConfig().app.baseURL))
+    if (_base !== '/' && !props.dark.startsWith(_base)) {
+      return joinURL(_base, props.dark)
+    }
+  }
+  return props.dark
+})
 </script>
 
 <template>
-  <component :is="ImageComponent" :src="light" class="dark:hidden" v-bind="$attrs" />
-  <component :is="ImageComponent" :src="dark" class="hidden dark:block" v-bind="$attrs" />
+  <component :is="ImageComponent" :src="refinedLight" class="dark:hidden" v-bind="$attrs" />
+  <component :is="ImageComponent" :src="refinedDark" class="hidden dark:block" v-bind="$attrs" />
 </template>
