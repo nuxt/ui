15f29162e4192b44ca28ab0d768589999cb5b771
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-02-18 13:08:53 +0100

    fix: remove `trim` from type evaluation and improve `number`

--- src/runtime/types/input.ts
+++ src/runtime/types/input.ts
@@ -1,18 +1,17 @@
 export interface ModelModifiers {
   nullable?: boolean
   number?: boolean
-  trim?: boolean
   optional?: boolean
+  trim?: boolean
   lazy?: boolean
 }
 
-export type ApplyModifiers<T, M extends ModelModifiers = ModelModifiers> = _Trim<_Number<_Optional<_Nullable<T, M>, M>, M>, M>
+export type ApplyModifiers<T, M extends ModelModifiers = ModelModifiers> = _Number<_Optional<_Nullable<T, M>, M>, M>
 
 type _Nullable<T, M> = M extends { nullable: true } ? T | null : T
 type _Optional<T, M> = M extends { optional: true } ? T | undefined : T
-type _Number<T, M> = M extends { number: true } ? T | number : T
-type _Trim<T, M> = M extends { trim: true }
+type _Number<T, M> = M extends { number: true }
   ? T extends string
-    ? T
-    : never
+    ? T | number
+    : T
   : T
