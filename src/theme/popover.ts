02485dea46b844011f36e9c178cd3a4b1d57a0f9
Can Stand <canstand@outlook.com>
2026-02-25 01:09:32 +0000

    fix: new inline fill and stroke variable

--- src/theme/popover.ts
+++ src/theme/popover.ts
@@ -1,6 +1,6 @@
 export default {
   slots: {
     content: 'bg-default shadow-lg rounded-md ring ring-default data-[state=open]:animate-[scale-in_100ms_ease-out] data-[state=closed]:animate-[scale-out_100ms_ease-in] origin-(--reka-popover-content-transform-origin) focus:outline-none pointer-events-auto',
-    arrow: 'fill-(--ui-bg) stroke-default'
+    arrow: 'fill-bg stroke-default'
   }
 }
