9384a61374e8bd65c993a71a2f8346ca809641b0
Sandro Circi <sandro.circi@digitoolmedia.com>
2026-02-27 17:07:36 +0100

    fix(NavigationMenu): `close` key is always present

--- src/runtime/components/NavigationMenu.vue
+++ src/runtime/components/NavigationMenu.vue
@@ -215,7 +215,7 @@ export type NavigationMenuSlots<
   'item-leading'?: SlotProps<T>
   'item-label'?: (props: { item: T, index: number, active: boolean }) => VNode[]
   'item-trailing'?: SlotProps<T>
-  'item-content'?: (props: { item: T, index: number, active: boolean, ui: NavigationMenu['ui'], close?: () => void }) => VNode[]
+  'item-content'?: (props: { item: T, index: number, active: boolean, ui: NavigationMenu['ui'], close: (() => void) | undefined }) => VNode[]
   'list-leading'?: (props?: {}) => VNode[]
   'list-trailing'?: (props?: {}) => VNode[]
 }
