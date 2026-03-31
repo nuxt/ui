f2ef87e655cc6afdec58f77630cf0a23cfe7ae51
lorado <eugen.sacharow@gmx.de>
2026-03-31 16:01:24 +0200

    Update focus logic in `InputMenu` and `SelectMenu` to use `filteredItems` for item checking

--- src/runtime/components/InputMenu.vue
+++ src/runtime/components/InputMenu.vue
@@ -543,7 +543,7 @@ function onInputEnter(e: Event) {
     return
   }
   // In case we have items but our focus is not within the combobox, just bring the focus back
-  if (props.items && props.items.length > 0) {
+  if (filteredItems.value && filteredItems.value.length > 0) {
     comboboxRootRef.value?.highlightFirstItem?.()
     e.preventDefault()
     e.stopPropagation()
