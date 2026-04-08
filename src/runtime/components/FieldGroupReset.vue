a89a3fef0a486df27f71e13ee8e3bdcc5ff8c8a0
Mateusz Narowski <mateusz.narowski@mndev.eu>
2026-04-08 11:09:09 +0200

    docs(FieldGroupReset): add inline comment explaining purpose of component

--- src/runtime/components/FieldGroupReset.vue
+++ src/runtime/components/FieldGroupReset.vue
@@ -1,4 +1,9 @@
 <script setup lang="ts">
+/*
+ * Internal component used to reset the FieldGroup injection context at portal
+ * boundaries, preventing nested components from inheriting `size` and
+ * `orientation` styles from a parent UFieldGroup.
+ */
 import { computed, provide } from 'vue'
 import { fieldGroupInjectionKey } from '../composables/useFieldGroup'
 
