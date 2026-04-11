f721801fcf5696fb28006c725cfa9f2fba7bc791
Dany <alwe.dev@gmail.com>
2026-04-11 14:54:40 +0200

    update tests

--- test/components/Select.spec.ts
+++ test/components/Select.spec.ts
@@ -37,12 +37,20 @@ describe('Select', () => {
 
   const itemsWithDescription = [...items.map(item => ({ ...item, description: 'Description' }))]
 
+  const itemsWithBadge = [
+    ...items.map((item, i) => ({
+      ...item,
+      badge: i % 2 === 0 ? 'Badge' : { color: 'primary', variant: 'solid', size: 'sm', label: 'Badge' }
+    }))
+  ]
+
   const props = { open: true, portal: false, items }
 
   renderEach(Select, [
     // Props
     ['with items', { props }],
     ['with items with description', { props: { ...props, items: itemsWithDescription } }],
+    ['with items with badge', { props: { ...props, items: itemsWithBadge } }],
     ['with modelValue', { props: { ...props, modelValue: items[0]?.value } }],
     ['with defaultValue', { props: { ...props, defaultValue: items[0]?.value } }],
     ['with valueKey', { props: { ...props, valueKey: 'label', defaultValue: 'Backlog' } }],
