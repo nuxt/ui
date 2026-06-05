b8ee80b663d9a0076a2db0a2f3ef54ae9db7e893
Benjamin Canac <canacb1@gmail.com>
2026-06-05 11:57:23 +0200

    docs(composables): improve examples

---
+++ docs/app/components/content/examples/use-scroll-shadow/UseScrollShadowExample.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const el = useTemplateRef('el')
+
+const { style } = useScrollShadow(el)
+</script>
+
+<template>
+  <div class="max-w-sm bg-elevated/50 rounded-lg">
+    <div
+      ref="el"
+      :style="style"
+      class="h-72 p-4 space-y-4 overflow-y-auto"
+    >
+      <p v-for="i in 6" :key="i">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor quam. Morbi accumsan cursus enim, sed ultricies sapien.
+      </p>
+    </div>
+  </div>
+</template>
---
+++ docs/app/components/content/examples/use-toast/UseToastExample.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const toast = useToast()
+
+function showToast() {
+  toast.add({
+    title: 'New message',
+    description: 'You have a new message from Benjamin.',
+    actions: [{
+      icon: 'i-lucide-reply',
+      label: 'Reply',
+      color: 'neutral',
+      variant: 'outline',
+      onClick: () => {
+        toast.add({ title: 'Reply sent', color: 'success' })
+      }
+    }]
+  })
+}
+</script>
+
+<template>
+  <UButton label="Show toast" @click="showToast" />
+</template>
