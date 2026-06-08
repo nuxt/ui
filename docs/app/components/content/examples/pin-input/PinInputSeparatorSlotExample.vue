89b30e8ebe2b3b3e637ba823d1c35bc2c2d32a68
Ali Maher <ali.maher.2906@gmail.com>
2026-06-08 16:29:04 +0300

    feat(PinInput): add `separator` prop (#6392)
    Co-authored-by: Ali Maher <ali@ali-pc.ali-pc>
    Co-authored-by: Benjamin Canac <canacb1@gmail.com>

---
+++ docs/app/components/content/examples/pin-input/PinInputSeparatorSlotExample.vue
@@ -0,0 +1,7 @@
+<template>
+  <UPinInput :length="6" :separator="3" placeholder="○">
+    <template #separator>
+      <UIcon name="i-lucide-minus" class="size-4" />
+    </template>
+  </UPinInput>
+</template>
