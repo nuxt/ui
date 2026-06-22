d73f9641cae7a2102add79367e87255eca1c93f9
Benjamin Canac <canacb1@gmail.com>
2026-06-22 16:28:27 +0200

    fix(LocaleSelect): update mapping

--- src/runtime/components/locale/LocaleSelect.vue
+++ src/runtime/components/locale/LocaleSelect.vue
@@ -36,10 +36,12 @@ function getEmojiFlag(locale: string): string {
     el: 'gr', // Greek -> Greece
     en: 'us', // English -> United States (default)
     et: 'ee', // Estonian -> Estonia
+    eu: 'es', // Basque -> Spain
     gl: 'es', // Galician -> Spain
     he: 'il', // Hebrew -> Israel
     hi: 'in', // Hindi -> India
     hy: 'am', // Armenian -> Armenia
+    is: 'is', // Icelandic -> Iceland
     ja: 'jp', // Japanese -> Japan
     ka: 'ge', // Georgian -> Georgia
     kk: 'kz', // Kazakh -> Kazakhstan
@@ -47,6 +49,7 @@ function getEmojiFlag(locale: string): string {
     ko: 'kr', // Korean -> South Korea
     ky: 'kg', // Kyrgyz -> Kyrgyzstan
     lb: 'lu', // Luxembourgish -> Luxembourg
+    lo: 'la', // Lao -> Laos
     ms: 'my', // Malay -> Malaysia
     nb: 'no', // Norwegian Bokmål -> Norway
     sl: 'si', // Slovenian -> Slovenia
