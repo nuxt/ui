f866a271073061764a1441278acc1fd266c2cf4e
Benjamin Canac <canacb1@gmail.com>
2026-06-26 17:20:07 +0200

    up

--- test/components/Separator.spec.ts
+++ test/components/Separator.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import { axe } from 'vitest-axe'
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import { renderEach } from '../component-render'
