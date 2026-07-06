d7284f3a3f284fc4be3dc9f09b24a91bbf1e0d14
Benjamin Canac <canacb1@gmail.com>
2026-07-06 11:40:44 +0200

    docs: use `shiki-transformer-icon-highlight` package (#6684)

--- docs/app/mdc.config.ts
+++ docs/app/mdc.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@nuxtjs/mdc/config'
 import type { ShikiTransformer } from '@shikijs/types'
 import { transformerColorHighlight } from 'shiki-transformer-color-highlight'
-import { transformerIconHighlight } from './utils/shiki-transformer-icon-highlight'
+import { transformerIconHighlight } from 'shiki-transformer-icon-highlight'
 
 export default defineConfig({
   shiki: {
