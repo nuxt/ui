cb94298b80f611bdad01342dd851c91fdfe1f8ee
Benjamin Canac <canacb1@gmail.com>
2026-09-09 13:50:09 +0200

    docs(community): link the submitter again

--- docs/app/pages/community.vue
+++ docs/app/pages/community.vue
@@ -65,11 +65,18 @@ if (import.meta.server) {
               </template>
 
               <template v-if="item.user" #footer>
-                <UUser
-                  :name="item.user.name"
-                  :avatar="item.user.avatar"
-                  size="xs"
-                  class="rounded-full border border-default ps-1 pe-3 py-1"
+                <UButton
+                  :label="item.user.name"
+                  :avatar="{
+                    ...item.user.avatar,
+                    alt: `${item.user.name} avatar`
+                  }"
+                  :to="item.user.to"
+                  target="_blank"
+                  size="sm"
+                  color="neutral"
+                  variant="outline"
+                  class="bg-transparent z-1 relative"
                 />
               </template>
             </UPageCard>
